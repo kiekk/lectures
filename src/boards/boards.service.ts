583a51585075c50f1c3a5a544ae68eedcab7d1cf
shyoon <dbstnsgh2@naver.com>
2021-09-12 16:32:49 +0900

    use model board

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import {Board} from "./board.model";
 
 @Injectable()
 export class BoardsService {
-    private boards = []
+    private boards: Board[] = []
 
-    getAllBoards() {
+                    // 타입 추론이 되기 때문에 타입을 지정하지 않아도 Board[]로 인식
+    getAllBoards()/*: Board[] */ {
         return this.boards
     }
 }
