c58cba9822abbbb14368d9d765de92155e51ee60
shyoon <dbstnsgh2@naver.com>
2021-09-12 17:17:31 +0900

    add handler deleteBoard()

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from '../dto/create-board.dto';
 
@@ -19,5 +19,10 @@ export class BoardsController {
   @Get('/:id')
   getBoardById(@Param('id') id: string) {
     return this.boardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string) {
+    this.boardsService.deleteBoard(id);
   }
 }
