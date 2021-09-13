bdd22580f1ced357c108d693b3748b539e2f5309
shyoon <dbstnsgh2@naver.com>
2021-09-14 08:07:14 +0900

    create jwt.strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -18,6 +19,9 @@ import { PassportModule } from '@nestjs/passport';
     TypeOrmModule.forFeature([UserRepository]),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  // auth module에서 사용할수 있게 등록
+  providers: [AuthService, JwtStrategy],
+  // 다른 module에서 사용할수 있게 등록
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
