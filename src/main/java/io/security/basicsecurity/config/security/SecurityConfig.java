9acb7a66044953df40c03e2767852227fb5e7238
shyoon <kiekk@i-commtech.com>
2022-04-11 15:19:06 +0900

    fix error - authorizeHttpRequests -> authorizeRequests
    error message: permitAll only works with HttpSecurity.authorizeRequests()

--- src/main/java/io/security/basicsecurity/config/security/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/config/security/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests()
+                .authorizeRequests()
                 .anyRequest().authenticated();
         http
                 .formLogin()
