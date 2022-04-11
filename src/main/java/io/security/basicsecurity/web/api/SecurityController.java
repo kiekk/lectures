ec6c27082e6c94d469de750bbf63b487a1682815
shyoon <kiekk@i-commtech.com>
2022-04-11 15:23:14 +0900

    add request mapping - "login"

--- src/main/java/io/security/basicsecurity/web/api/SecurityController.java
+++ src/main/java/io/security/basicsecurity/web/api/SecurityController.java
@@ -10,4 +10,9 @@ public class SecurityController {
     public String index() {
         return "home";
     }
+
+    @GetMapping("login")
+    public String login() {
+        return "login";
+    }
 }
