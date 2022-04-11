c953048dbcd104a02f639389b348e170a5884945
shyoon <kiekk@i-commtech.com>
2022-04-11 15:14:05 +0900

    setting config - formLogin

--- src/main/java/io/security/basicsecurity/config/security/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/config/security/SecurityConfig.java
@@ -1,10 +1,12 @@
 package io.security.basicsecurity.config.security;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import sun.misc.Contended;
 
+@Slf4j
 @Contended
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -13,9 +15,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests()
-                .anyRequest()
-                .authenticated();
+                .anyRequest().authenticated();
         http
-                .formLogin();
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login")
+                .usernameParameter("userId")
+                .passwordParameter("password")
+                .loginProcessingUrl("/login-processing")
+                .successHandler((request, response, authentication) -> {
+                    log.debug("authentication : {}", authentication.getName());
+                    response.sendRedirect("/");
+                })
+                .failureHandler((request, response, exception) -> {
+                    log.debug("exception : {}", exception.getMessage());
+                    response.sendRedirect("/");
+                })
+                .permitAll();
     }
 }
