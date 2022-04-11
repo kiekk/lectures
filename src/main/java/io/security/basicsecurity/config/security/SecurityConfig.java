f906a0140c3c0f84a90f21a085a7c8beed219098
shyoon <kiekk@i-commtech.com>
2022-04-11 16:58:36 +0900

    setting config - remember-me

--- src/main/java/io/security/basicsecurity/config/security/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/config/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import sun.misc.Contended;
 
 import javax.servlet.http.HttpSession;
@@ -13,28 +14,34 @@ import javax.servlet.http.HttpSession;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .anyRequest().authenticated();
         http
-                .formLogin()
-                .loginPage("/login")
-                .defaultSuccessUrl("/")
-                .failureUrl("/login")
-                .usernameParameter("userId")
-                .passwordParameter("password")
-                .loginProcessingUrl("/login-processing")
-                .successHandler((request, response, authentication) -> {
-                    log.debug("authentication : {}", authentication.getName());
-                    response.sendRedirect("/");
-                })
-                .failureHandler((request, response, exception) -> {
-                    log.debug("exception : {}", exception.getMessage());
-                    response.sendRedirect("/");
-                })
-                .permitAll();
+                .formLogin();
+//                .loginPage("/login")
+//                .defaultSuccessUrl("/")
+//                .failureUrl("/login")
+//                .usernameParameter("userId")
+//                .passwordParameter("password")
+//                .loginProcessingUrl("/login-processing")
+//                .successHandler((request, response, authentication) -> {
+//                    log.debug("authentication : {}", authentication.getName());
+//                    response.sendRedirect("/");
+//                })
+//                .failureHandler((request, response, exception) -> {
+//                    log.debug("exception : {}", exception.getMessage());
+//                    response.sendRedirect("/");
+//                })
+//                .permitAll();
         http
                 .logout()
                 .logoutUrl("/logout")
@@ -45,5 +52,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 })
                 .logoutSuccessHandler((request, response, authentication) -> response.sendRedirect("/login"))
                 .deleteCookies("remember-me");
+        http
+                .rememberMe()
+                .rememberMeParameter("remember")
+                .tokenValiditySeconds(60 * 60)
+                .userDetailsService(userDetailsService);
     }
 }
