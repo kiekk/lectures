52e7eb658f1722db100dc93ed028b1414debe55b
kiekk <dbstnsgh2@naver.com>
2022-04-12 15:31:13 +0900

    update config - set multiple security config

--- src/main/java/io/security/basicsecurity/config/security/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/config/security/SecurityConfig.java
@@ -2,95 +2,40 @@ package io.security.basicsecurity.config.security;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.RequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
 
 @Slf4j
+@Order(0)
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsService userDetailsService;
-
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
-
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("{noop}1234").roles("USER");
-        auth.inMemoryAuthentication().withUser("sys").password("{noop}1234").roles("SYS");
-        auth.inMemoryAuthentication().withUser("admin").password("{noop}1234").roles("ADMIN", "SYS", "USER");
-
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/admin/**")
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
+}
+
+@Order(1)
+@Configuration
+class SecurityConfig2 extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/login").permitAll()
-                .antMatchers("/user/**").hasAnyRole("USER")
-                .antMatchers("/admin/pay").hasAnyRole("ADMIN")
-                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
-                .anyRequest().authenticated();
-        http
-                .formLogin()
-                .successHandler((request, response, authentication) -> {
-                    RequestCache requestCache = new HttpSessionRequestCache();
-                    SavedRequest savedRequest = requestCache.getRequest(request, response);
-                    String redirectUrl = savedRequest.getRedirectUrl();
-                    response.sendRedirect(redirectUrl);
-                });
-//                .loginPage("/login")
-//                .defaultSuccessUrl("/")
-//                .failureUrl("/login")
-//                .usernameParameter("userId")
-//                .passwordParameter("password")
-//                .loginProcessingUrl("/login-processing")
-//                .successHandler((request, response, authentication) -> {
-//                    log.debug("authentication : {}", authentication.getName());
-//                    response.sendRedirect("/");
-//                })
-//                .failureHandler((request, response, exception) -> {
-//                    log.debug("exception : {}", exception.getMessage());
-//                    response.sendRedirect("/");
-//                })
-//                .permitAll();
-//        http
-//                .logout()
-//                .logoutUrl("/logout")
-//                .logoutSuccessUrl("/login")
-//                .addLogoutHandler((request, response, authentication) -> {
-//                    HttpSession session = request.getSession();
-//                    session.invalidate();
-//                })
-//                .logoutSuccessHandler((request, response, authentication) -> response.sendRedirect("/login"))
-//                .deleteCookies("remember-me");
-//        http
-//                .rememberMe()
-//                .rememberMeParameter("remember")
-//                .tokenValiditySeconds(60 * 60)
-//                .userDetailsService(userDetailsService);
-//        http
-//                .sessionManagement()
-//                .maximumSessions(1)
-//                .maxSessionsPreventsLogin(true);   // true: 동시 로그인 차단, false: 이전 세션 만료
-        http
-                .exceptionHandling()
-                .authenticationEntryPoint(((request, response, authException) -> {
-                    log.debug("authenticationEntryPoint");
-                    response.sendRedirect("/login");
-                }))
-                .accessDeniedHandler((request, response, accessDeniedException) -> {
-                    log.debug("accessDeniedHandler");
-                    response.sendRedirect("/denied");
-                });
-
+                .anyRequest()
+                .permitAll()
+                .and()
+                .formLogin();
     }
 }
