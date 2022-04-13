6cf58256fa3873e07c867ca153f3e58380e96711
kiekk <kiekk@i-commtech.com>
2022-04-13 17:19:36 +0900

    add @Getter to AccountContext

--- src/main/java/io/security/corespringsecurity/security/service/AccountContext.java
+++ src/main/java/io/security/corespringsecurity/security/service/AccountContext.java
@@ -1,11 +1,13 @@
 package io.security.corespringsecurity.security.service;
 
 import io.security.corespringsecurity.domain.Account;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import java.util.Collection;
 
+@Getter
 public class AccountContext extends User {
 
     private final Account account;
