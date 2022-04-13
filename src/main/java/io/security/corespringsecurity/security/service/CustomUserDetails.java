1050db9aa16b4779650a946b4208d7e730bfd14c
kiekk <kiekk@i-commtech.com>
2022-04-13 17:05:13 +0900

    add security service - CustomUserDetails

---
+++ src/main/java/io/security/corespringsecurity/security/service/CustomUserDetails.java
@@ -0,0 +1,36 @@
+package io.security.corespringsecurity.security.service;
+
+import io.security.corespringsecurity.domain.Account;
+import io.security.corespringsecurity.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetails(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Account account = userRepository.findByUsername(username);
+
+        if (null == account) {
+            throw new UsernameNotFoundException("UsernameNotFoundException");
+        }
+
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new AccountContext(account, roles);
+    }
+}
