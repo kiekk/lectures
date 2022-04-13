2d910f44241cc322ea1436d44c58e7d6005725d2
kiekk <kiekk@i-commtech.com>
2022-04-13 16:48:32 +0900

    add service - UserService

---
+++ src/main/java/io/security/corespringsecurity/service/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package io.security.corespringsecurity.service.impl;
+
+import io.security.corespringsecurity.domain.Account;
+import io.security.corespringsecurity.repository.UserRepository;
+import io.security.corespringsecurity.service.UserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    @Override
+    public void createUser(Account account) {
+        userRepository.save(account);
+    }
+}
