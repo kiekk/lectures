dc2edc6bb8b718df154c9c5b00123bff6912665f
kiekk <dbstnsgh2@naver.com>
2022-04-17 16:48:09 +0900

    add controller

---
+++ src/main/java/io/security/corespringsecurity/controller/admin/RoleController.java
@@ -0,0 +1,74 @@
+package io.security.corespringsecurity.controller.admin;
+
+import io.security.corespringsecurity.domain.dto.RoleDto;
+import io.security.corespringsecurity.domain.entity.Role;
+import io.security.corespringsecurity.service.RoleService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin/roles")
+public class RoleController {
+
+    private final RoleService roleService;
+
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @GetMapping("")
+    public String getRoles(Model model) throws Exception {
+
+        List<Role> roles = roleService.getRoles();
+        model.addAttribute("roles", roles);
+
+        return "admin/role/list";
+    }
+
+    @GetMapping("register")
+    public String viewRoles(Model model) throws Exception {
+
+        RoleDto role = new RoleDto();
+        model.addAttribute("role", role);
+
+        return "admin/role/detail";
+    }
+
+    @PostMapping("")
+    public String createRole(RoleDto roleDto) throws Exception {
+
+        ModelMapper modelMapper = new ModelMapper();
+        Role role = modelMapper.map(roleDto, Role.class);
+        roleService.createRole(role);
+
+        return "redirect:/admin/roles";
+    }
+
+    @GetMapping("{id}")
+    public String getRole(@PathVariable String id, Model model) throws Exception {
+
+        Role role = roleService.getRole(Long.parseLong(id));
+
+        ModelMapper modelMapper = new ModelMapper();
+        RoleDto roleDto = modelMapper.map(role, RoleDto.class);
+        model.addAttribute("role", roleDto);
+
+        return "admin/role/detail";
+    }
+
+    @GetMapping("delete/{id}")
+    public String removeResources(@PathVariable String id, Model model) throws Exception {
+
+        Role role = roleService.getRole(Long.parseLong(id));
+        roleService.deleteRole(Long.parseLong(id));
+
+        return "redirect:/admin/resources";
+    }
+}
