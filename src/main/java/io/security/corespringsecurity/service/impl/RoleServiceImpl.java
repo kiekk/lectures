62d6ff0f3ce4a3efac12d3793609ef40a7c3ef53
kiekk <dbstnsgh2@naver.com>
2022-04-17 15:55:04 +0900

    update @Transactional import

--- src/main/java/io/security/corespringsecurity/service/impl/RoleServiceImpl.java
+++ src/main/java/io/security/corespringsecurity/service/impl/RoleServiceImpl.java
@@ -5,8 +5,8 @@ import io.security.corespringsecurity.repository.RoleRepository;
 import io.security.corespringsecurity.service.RoleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Slf4j
