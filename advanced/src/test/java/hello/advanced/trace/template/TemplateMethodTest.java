291abb513d45b5667b386960545a3a6c49ebb24a
soonho <dbstnsgh2@naver.com>
2022-06-06 21:04:47 +0900

    temmplate-method-pattern: 기존 로그 추적기 v3 코드 - 중복 코드 확인 (로그 출력 코드 중복)

---
+++ advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,37 @@
+package hello.advanced.trace.template;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    void templateMethodV0() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행");
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+}
+/*
+logic1()과 logic2()는 비즈니스 로직1, 비즈니스 로직 2 부분인 핵심 기능을 제외하고는 코드가 전부 동일합니다.
+ */
