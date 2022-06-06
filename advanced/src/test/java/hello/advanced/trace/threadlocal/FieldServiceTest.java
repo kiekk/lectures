af6eee13d1cd5893cfdc9f47aa2fa7ad38356845
soonho <dbstnsgh2@naver.com>
2022-06-06 20:22:17 +0900

    log-trace: 동시성 문제가 발생하는 코드

--- advanced/src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
+++ advanced/src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
@@ -28,7 +28,8 @@ public class FieldServiceTest {
         threadB.setName("thread-B");
 
         threadA.start();
-        sleep(2000); // 동시성 문제 발생 X
+//        sleep(2000); // 동시성 문제 발생 X
+        sleep(100); // 동시성 문제 발생 O
         threadB.start();
 
         sleep(2000); // 메인 쓰레드 종료 대기
