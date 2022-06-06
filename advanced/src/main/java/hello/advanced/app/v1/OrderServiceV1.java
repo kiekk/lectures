8433fff35240cbadf16dc9bee63159b652d22867
soonho <dbstnsgh2@naver.com>
2022-06-06 17:40:53 +0900

    advanced: OrderRepository, OrderService 로그 추적기 적용

--- advanced/src/main/java/hello/advanced/app/v1/OrderServiceV1.java
+++ advanced/src/main/java/hello/advanced/app/v1/OrderServiceV1.java
@@ -1,5 +1,7 @@
 package hello.advanced.app.v1;
 
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,10 +9,19 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OrderServiceV1 {
 
+    private final HelloTraceV1 trace;
     private final OrderRepositoryV1 orderRepositoryV1;
 
     public void orderItem(String itemId) {
-        orderRepositoryV1.save(itemId);
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            orderRepositoryV1.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
     }
 
 }
