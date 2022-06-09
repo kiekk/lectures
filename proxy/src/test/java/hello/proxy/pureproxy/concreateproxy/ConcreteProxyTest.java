970713d9304cf73e4458ae82a375bd5c7f18b00d
soonho <dbstnsgh2@naver.com>
2022-06-10 07:08:42 +0900

    decorator-pattern: 클래스 기반 프록시 도입 - 2. add TimeProxy

--- proxy/src/test/java/hello/proxy/pureproxy/concreateproxy/ConcreteProxyTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/concreateproxy/ConcreteProxyTest.java
@@ -2,6 +2,7 @@ package hello.proxy.pureproxy.concreateproxy;
 
 import hello.proxy.pureproxy.concreateproxy.code.ConcreteClient;
 import hello.proxy.pureproxy.concreateproxy.code.ConcreteLogic;
+import hello.proxy.pureproxy.concreateproxy.code.TimeProxy;
 import org.junit.jupiter.api.Test;
 
 public class ConcreteProxyTest {
@@ -10,6 +11,15 @@ public class ConcreteProxyTest {
     void noProxy() {
         ConcreteLogic concreteLogic = new ConcreteLogic();
         ConcreteClient client = new ConcreteClient(concreteLogic);
+
+        client.execute();
+    }
+
+    @Test
+    void addProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient client = new ConcreteClient(timeProxy);
 
         client.execute();
     }
