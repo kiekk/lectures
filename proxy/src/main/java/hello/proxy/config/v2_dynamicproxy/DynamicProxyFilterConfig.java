7bd52e286f372367aae6b8648faad58fbf76bdae
soonho <dbstnsgh2@naver.com>
2022-06-10 22:06:09 +0900

    reflection: jdk-dynamic-proxy 로그 추적기 프로젝트에 적용 - 2 (특정 메소드만 로그 출력)

--- proxy/src/main/java/hello/proxy/config/v2_dynamicproxy/DynamicProxyFilterConfig.java
+++ proxy/src/main/java/hello/proxy/config/v2_dynamicproxy/DynamicProxyFilterConfig.java
@@ -1,7 +1,7 @@
 package hello.proxy.config.v2_dynamicproxy;
 
 import hello.proxy.app.v1.*;
-import hello.proxy.config.v2_dynamicproxy.handler.LogTraceBasicHandler;
+import hello.proxy.config.v2_dynamicproxy.handler.LogTraceFilterHandler;
 import hello.proxy.trace.logtrace.LogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +9,9 @@ import org.springframework.context.annotation.Configuration;
 import java.lang.reflect.Proxy;
 
 @Configuration
-public class DynamicProxyBasicConfig {
+public class DynamicProxyFilterConfig {
+
+    private static final String[] PATTERNS = {"request*", "order*", "save*"};
 
     @Bean
     public OrderRepositoryV1 orderRepositoryV1(LogTrace logTrace) {
@@ -18,7 +20,7 @@ public class DynamicProxyBasicConfig {
         return (OrderRepositoryV1) Proxy.newProxyInstance(
                 OrderRepositoryV1.class.getClassLoader()
                 , new Class[]{OrderRepositoryV1.class}
-                , new LogTraceBasicHandler(orderRepository, logTrace));
+                , new LogTraceFilterHandler(orderRepository, logTrace, PATTERNS));
     }
 
     @Bean
@@ -28,7 +30,7 @@ public class DynamicProxyBasicConfig {
         return (OrderServiceV1) Proxy.newProxyInstance(
                 OrderServiceV1.class.getClassLoader()
                 , new Class[]{OrderServiceV1.class}
-                , new LogTraceBasicHandler(orderService, logTrace)
+                , new LogTraceFilterHandler(orderService, logTrace, PATTERNS)
         );
     }
 
@@ -39,7 +41,7 @@ public class DynamicProxyBasicConfig {
         return (OrderControllerV1) Proxy.newProxyInstance(
                 OrderControllerV1.class.getClassLoader()
                 , new Class[]{OrderControllerV1.class}
-                , new LogTraceBasicHandler(orderController, logTrace)
+                , new LogTraceFilterHandler(orderController, logTrace, PATTERNS)
         );
     }
 
