5f0c5bdef6c4434b199a0bf6c881005b2e8269cc
soonho <dbstnsgh2@naver.com>
2022-06-11 20:38:00 +0900

    aop: @Aspect 로 Advisor 등록

---
+++ aop/src/main/java/hello/aop/order/aop/AspectV1.java
@@ -0,0 +1,19 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect
+public class AspectV1 {
+
+    @Around("execution (* hello.aop.order..*(..))")
+    public Object doLog(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        log.info("[log] {}", proceedingJoinPoint.getSignature());   // join point signature
+
+        return proceedingJoinPoint.proceed();
+    }
+
+}
