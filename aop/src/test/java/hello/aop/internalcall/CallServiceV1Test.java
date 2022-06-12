a8963f3d52d0d881be69659c0e704716f24da66e
soonho <dbstnsgh2@naver.com>
2022-06-12 14:48:53 +0900

    aop: 자기 자신 주입으로 aop 적용된 메소드의 내부에서 메소드 호출 시 aop 적용

--- aop/src/test/java/hello/aop/internalcall/CallServiceV1Test.java
+++ aop/src/test/java/hello/aop/internalcall/CallServiceV1Test.java
@@ -7,30 +7,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @Slf4j
 @SpringBootTest
 @Import(CallLogAspect.class)
-class CallServiceV0Test {
+class CallServiceV1Test {
 
     @Autowired
-    CallServiceV0 callServiceV0;
+    CallServiceV1 callServiceV1;
 
     @Test
     void external() {
-        callServiceV0.external();
-        /*
-        external 메소드는 aop 적용이 되지만,
-        external 메소드의 내부에서 호출하는
-        internal 메소드는 aop 적용 X
-         */
+        callServiceV1.external();
     }
 
     @Test
     void internal() {
-        // proxy 를 통해 호출하는 internal 메소드의 경우 aop 적용 O
-        callServiceV0.internal();
+        callServiceV1.internal();
     }
 
 }
