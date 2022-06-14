aab7a88c62e2821a80fa38e202f32a915c1d4f22
soonho <dbstnsgh2@naver.com>
2022-06-14 23:25:10 +0900

    add controller - RootController

---
+++ src/main/java/handlebar/study/controller/RootController.java
@@ -0,0 +1,14 @@
+package handlebar.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class RootController {
+
+    @RequestMapping("")
+    public String root() {
+        return "home";
+    }
+}
