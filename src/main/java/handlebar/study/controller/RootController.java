3078d7f79e2516e01d680e0f6d71ca1f2b47b29f
soonho <dbstnsgh2@naver.com>
2022-06-19 15:39:31 +0900

    Built-in Helpers - #with (alias 사용)

--- src/main/java/handlebar/study/controller/RootController.java
+++ src/main/java/handlebar/study/controller/RootController.java
@@ -1,6 +1,7 @@
 package handlebar.study.controller;
 
 import handlebar.study.entity.User;
+import handlebar.study.entity.UserDetail;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +28,21 @@ public class RootController {
         model.addAttribute("messages", "hello handlebars");
         return "comment";
     }
-    
+
+    @RequestMapping("with")
+    public String with(Model model) {
+        User user = new User();
+        user.setId("id_1");
+        user.setName("name_1");
+        user.setAge(10);
+
+        UserDetail detail = new UserDetail();
+        detail.setAddress("주소_1");
+        detail.setEmail("이메일_1");
+        detail.setMobile("전화번호_1");
+
+        user.setDetail(detail);
+        model.addAttribute("user", user);
+        return "with";
+    }
 }
