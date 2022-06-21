6a24cd28e0918fa3d09d8385a08772950635db60
soonho <dbstnsgh2@naver.com>
2022-06-22 06:24:49 +0900

    객체 출력하기 - 인라인, th 속성

--- src/main/java/thymeleaf/study/controller/RootController.java
+++ src/main/java/thymeleaf/study/controller/RootController.java
@@ -3,6 +3,7 @@ package thymeleaf.study.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import thymeleaf.study.entity.User;
 
 @Controller
 @RequestMapping("")
@@ -10,7 +11,13 @@ public class RootController {
 
     @RequestMapping({"", "/"})
     public String root(Model model) {
+        User user = new User();
+        user.setId("id_1");
+        user.setName("홍길동");
+        user.setMobile("010-1234-5678");
+
         model.addAttribute("message", "hello thymeleaf");
+        model.addAttribute("user", user);
         return "main";
     }
 }
