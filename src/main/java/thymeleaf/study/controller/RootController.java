00ebe161f6c1d8ceca82ec4cbb6893e521c6b82c
soonho <dbstnsgh2@naver.com>
2022-06-21 21:14:12 +0900

    변수 출력하기 - 인라인, th: 속성

--- src/main/java/thymeleaf/study/controller/RootController.java
+++ src/main/java/thymeleaf/study/controller/RootController.java
@@ -1,6 +1,7 @@
 package thymeleaf.study.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RootController {
 
     @RequestMapping({"", "/"})
-    public String root() {
+    public String root(Model model) {
+        model.addAttribute("message", "hello thymeleaf");
         return "main";
     }
 }
