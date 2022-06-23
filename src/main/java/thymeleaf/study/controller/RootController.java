6546f8ee1bc5420f719d475853cbaa04ee1eb7b5
soonho <dbstnsgh2@naver.com>
2022-06-23 20:39:15 +0900

    th:if

--- src/main/java/thymeleaf/study/controller/RootController.java
+++ src/main/java/thymeleaf/study/controller/RootController.java
@@ -16,8 +16,14 @@ public class RootController {
         user.setName("홍길동");
         user.setMobile("010-1234-5678");
 
+        User user2 = new User();
+        user2.setId("id_2");
+        user2.setName("test");
+        user2.setMobile("010-1234-5678");
+
         model.addAttribute("message", "hello thymeleaf");
         model.addAttribute("user", user);
+        model.addAttribute("user2", user2);
         return "main";
     }
 }
