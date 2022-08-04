7a5b78f0f7ed5c5f265e0cf19ba0d15d7e3588ae
shyoon <shyoon991@gmail.com>
2022-08-04 14:19:46 +0900

    item: add business logic & exception

---
+++ src/main/java/com/example/springdatajpa/exception/NotEnoughStockException.java
@@ -0,0 +1,17 @@
+package com.example.springdatajpa.exception;
+
+public class NotEnoughStockException extends RuntimeException {
+
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
