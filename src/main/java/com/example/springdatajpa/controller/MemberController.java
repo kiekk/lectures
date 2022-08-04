9c17c6f13964bd58d7444932ae7a63144ebc4856
shyoon <dbstnsgh2@naver.com>
2022-08-04 16:02:28 +0900

    web: add RequestMapping "/members" & edit RequestMapping

--- src/main/java/com/example/springdatajpa/controller/MemberController.java
+++ src/main/java/com/example/springdatajpa/controller/MemberController.java
@@ -4,27 +4,38 @@ import com.example.springdatajpa.domain.Address;
 import com.example.springdatajpa.domain.Member;
 import com.example.springdatajpa.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import net.bytebuddy.agent.builder.AgentBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("members")
 public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/members/new")
+    @GetMapping("")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberlist";
+    }
+
+    @GetMapping("new")
     public String createForm(Model model) {
         model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
     }
 
-    @PostMapping("/members/new")
+    @PostMapping("new")
     public String create(@Valid MemberForm memberForm, BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
