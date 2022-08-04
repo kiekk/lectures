4cdf161d01e64678a60e10d0c14745fd8e152a0f
shyoon <shyoon991@gmail.com>
2022-08-04 14:05:27 +0900

    add test - 회원가입 test

---
+++ src/test/java/com/example/springdatajpa/service/MemberServiceTest.java
@@ -0,0 +1,42 @@
+package com.example.springdatajpa.service;
+
+import com.example.springdatajpa.domain.Member;
+import com.example.springdatajpa.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Rollback(false)
+    public void 회원가입() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        assertEquals(member, memberRepository.find(saveId));
+    }
+
+}
