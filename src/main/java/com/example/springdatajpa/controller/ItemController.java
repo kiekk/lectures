630385152bb5820684875401c64c013508c69986
shyoon <dbstnsgh2@naver.com>
2022-08-04 16:39:08 +0900

    web: 상품 수정 기능 추가

--- src/main/java/com/example/springdatajpa/controller/ItemController.java
+++ src/main/java/com/example/springdatajpa/controller/ItemController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -68,6 +65,21 @@ public class ItemController {
 
         model.addAttribute("form", form);
         return "items/updateItemForm";
+    }
+
+    @PostMapping("{itemId}/edit")
+    public String updateItem(@PathVariable Long itemId,
+                             @ModelAttribute("form") BookForm form) {
+        Book book = new Book();
+        book.setId(itemId);
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
     }
 
 }
