591afd1e8f2971b3949a6875331dce630940d9ea
shyoon <shyoon991@gmail.com>
2022-08-04 15:12:39 +0900

    order: 주문취소 test

--- src/test/java/com/example/springdatajpa/service/OrderServiceTest.java
+++ src/test/java/com/example/springdatajpa/service/OrderServiceTest.java
@@ -81,5 +81,25 @@ public class OrderServiceTest {
 
         // then
         fail("재고 수량 부족 에외가 발생해야 한다.");
-   }
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+        // given
+        Member member = createMember();
+        Item item = createBook("JPA", 10_000, 10);
+
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        // when
+        orderService.cancel(orderId);
+
+        // then
+        Order findOrder = orderRepository.find(orderId);
+
+        assertEquals("주문 취소시 상태는 CANCEL 이다.", OrderStatus.CANCEL, findOrder.getStatus());
+        assertEquals("주문이 취소된 상품은 그만큼 재고가 증가해야 한다.", 10, item.getStockQuantity());
+    }
+
 }
