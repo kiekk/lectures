436be2e7022dd3bd9a19c311d8b9fad2878187dd
shyoon <shyoon991@gmail.com>
2022-08-04 14:39:45 +0900

    order: 주문 비지니스/ 조회 로직 추가

--- src/main/java/com/example/springdatajpa/domain/Order.java
+++ src/main/java/com/example/springdatajpa/domain/Order.java
@@ -67,4 +67,30 @@ public class Order {
         order.setOrderDate(LocalDateTime.now());
         return order;
     }
+
+    // == 비지니스 로직 == //
+
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+
+        orderItems.forEach(OrderItem::cancel);
+    }
+
+    // == 조회 로직 == //
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        return orderItems
+                .stream()
+                .map(OrderItem::getTotalPrice)
+                .reduce(0, Integer::sum);
+    }
 }
