845cd84aa0c5547530eef2709472f31ae493215d
shyoon <shyoon991@gmail.com>
2022-08-04 13:52:32 +0900

    add service MemberService

---
+++ src/main/java/com/example/springdatajpa/service/MemberService.java
@@ -0,0 +1,43 @@
+package com.example.springdatajpa.service;
+
+import com.example.springdatajpa.domain.Member;
+import com.example.springdatajpa.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 회원 가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMember = memberRepository.findByName(member.getName());
+        if (!findMember.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // 회원 상세 조회
+    public Member findMember(Long id) {
+        return memberRepository.find(id);
+    }
+
+}
