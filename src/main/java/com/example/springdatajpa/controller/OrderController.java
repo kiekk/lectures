c068e0e0d09a8209182bdd991fb3a6a075a891f0
shyoon <dbstnsgh2@naver.com>
2022-08-04 17:13:14 +0900

    web: 주문 목록 페이지/기능 추가

--- src/main/java/com/example/springdatajpa/controller/OrderController.java
+++ src/main/java/com/example/springdatajpa/controller/OrderController.java
@@ -1,30 +1,36 @@
 package com.example.springdatajpa.controller;
 
 import com.example.springdatajpa.domain.Member;
+import com.example.springdatajpa.domain.Order;
 import com.example.springdatajpa.domain.item.Item;
+import com.example.springdatajpa.repository.OrderSearch;
 import com.example.springdatajpa.service.ItemService;
 import com.example.springdatajpa.service.MemberService;
 import com.example.springdatajpa.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("order")
 public class OrderController {
 
     private final OrderService orderService;
     private final MemberService memberService;
     private final ItemService itemService;
 
-    @GetMapping("")
+    @GetMapping("orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch,
+                            Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+        return "order/orderList";
+    }
+
+    @GetMapping("order")
     public String createForm(Model model) {
         List<Member> members = memberService.findMembers();
         List<Item> items = itemService.findItems();
@@ -35,7 +41,7 @@ public class OrderController {
         return "order/orderForm";
     }
 
-    @PostMapping("")
+    @PostMapping("order")
     public String order(@RequestParam("memberId") Long memberId,
                         @RequestParam("itemId") Long itemId,
                         @RequestParam("count") int count) {
