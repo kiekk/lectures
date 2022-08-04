1254b09b7e67fe537bbe1eee1b4f20794b746b18
shyoon <shyoon991@gmail.com>
2022-08-04 14:05:43 +0900

    add test - 중복 회원 체크 test

--- src/test/java/com/example/springdatajpa/service/MemberServiceTest.java
+++ src/test/java/com/example/springdatajpa/service/MemberServiceTest.java
@@ -39,4 +39,25 @@ public class MemberServiceTest {
         assertEquals(member, memberRepository.find(saveId));
     }
 
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // when
+        memberService.join(member1);
+        try {
+            memberService.join(member2); // 중복 예외가 발생해야 함
+        } catch (IllegalStateException e) {
+            return;
+        }
+
+        // then
+        fail("예외가 발생해야 함");
+    }
+
 }
