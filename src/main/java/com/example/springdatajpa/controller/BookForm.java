5acde433f7c22471de5d4cd4dc26c6e89aaeab50
shyoon <dbstnsgh2@naver.com>
2022-08-04 16:18:38 +0900

    web: 상품 등록 validation 추가

--- src/main/java/com/example/springdatajpa/controller/BookForm.java
+++ src/main/java/com/example/springdatajpa/controller/BookForm.java
@@ -3,15 +3,25 @@ package com.example.springdatajpa.controller;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class BookForm {
 
     private Long id;
+    @NotEmpty(message = "상품 이름은 필수 입니다.")
     private String name;
+    @NotNull(message = "상품 가격은 필수 입니다.")
+    @Min(message = "상품 가격은 0원 이상이어야 합니다.", value = 0)
     private int price;
+    @NotNull(message = "상품 재고는 필수 입니다.")
+    @Min(message = "상품 재고는 0개 이상이어야 합니다.", value = 0)
     private int stockQuantity;
 
+    @NotEmpty(message = "저자는 필수 입니다.")
     private String author;
     private String isbn;
 
--- src/main/java/com/example/springdatajpa/controller/ItemController.java
+++ src/main/java/com/example/springdatajpa/controller/ItemController.java
@@ -5,9 +5,12 @@ import com.example.springdatajpa.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,12 +21,17 @@ public class ItemController {
 
     @GetMapping("new")
     public String createForm(Model model) {
-        model.addAttribute("form", new BookForm());
+        model.addAttribute("bookForm", new BookForm());
         return "items/createItemForm";
     }
 
     @PostMapping("new")
-    public String create(BookForm bookForm) {
+    public String create(@Valid BookForm bookForm, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "items/createItemForm";
+        }
+
         Book book = new Book();
         book.setName(bookForm.getName());
         book.setPrice(bookForm.getPrice());
