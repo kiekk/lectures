0c59809670f1c4603971bb84c0cfe4d169ccca94
shyoon <dbstnsgh2@naver.com>
2022-08-05 09:58:51 +0900

    api-orders: 주문 조회 API V1 추가

---
+++ src/main/java/com/example/springdatajpa/api/OrderSimpleApiController.java
@@ -0,0 +1,30 @@
+package com.example.springdatajpa.api;
+
+import com.example.springdatajpa.domain.Order;
+import com.example.springdatajpa.repository.OrderRepository;
+import com.example.springdatajpa.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * xToOne (ManyToOne, OneToOne) 성능 최적화
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all;
+    }
+
+}
