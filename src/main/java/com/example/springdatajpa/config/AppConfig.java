dbb913291e22d87bcd0ada3799254eb335970d1c
shyoon <dbstnsgh2@naver.com>
2022-08-05 10:04:16 +0900

    api-orders: add config hibernate5 (set hibernate loading option)
    --
    지연 로딩이므로 응답 값에는 null로 설정됨

--- src/main/java/com/example/springdatajpa/config/AppConfig.java
+++ src/main/java/com/example/springdatajpa/config/AppConfig.java
@@ -1,5 +1,7 @@
 package com.example.springdatajpa.config;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.hibernate.Hibernate;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +12,13 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public Hibernate5Module hibernate5Module() {
+        Hibernate5Module hibernate5Module = new Hibernate5Module();
+        //강제 지연 로딩 설정
+        hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+        return hibernate5Module;
     }
 }
