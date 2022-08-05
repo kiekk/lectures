db6ae8564b52804fc4531d31bf1f94429881a7a7
shyoon <dbstnsgh2@naver.com>
2022-08-05 10:28:10 +0900

    api-orders: 주문 조회 API V2 추가 (dto 사용, 필요한 항목만 노출)

--- src/main/java/com/example/springdatajpa/api/OrderSimpleApiController.java
+++ src/main/java/com/example/springdatajpa/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package com.example.springdatajpa.api;
 
+import com.example.springdatajpa.domain.Address;
 import com.example.springdatajpa.domain.Order;
+import com.example.springdatajpa.enums.OrderStatus;
 import com.example.springdatajpa.repository.OrderRepository;
 import com.example.springdatajpa.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne (ManyToOne, OneToOne) 성능 최적화
@@ -31,4 +36,31 @@ public class OrderSimpleApiController {
         return all;
     }
 
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        return orderRepository
+                .findAllByString(new OrderSearch())
+                .stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Data
+    static class SimpleOrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+        }
+
+    }
 }
