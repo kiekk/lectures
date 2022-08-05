2a53c766b6479381c4b01a20a8b988ba179e86ec
shyoon <dbstnsgh2@naver.com>
2022-08-05 11:39:11 +0900

    api-orders: 주문 조회 API V2 추가 (dto 사용, 필요한 항목만 노출)

--- src/main/java/com/example/springdatajpa/api/OrderApiController.java
+++ src/main/java/com/example/springdatajpa/api/OrderApiController.java
@@ -1,13 +1,20 @@
 package com.example.springdatajpa.api;
 
+import com.example.springdatajpa.domain.Address;
 import com.example.springdatajpa.domain.Order;
+import com.example.springdatajpa.domain.OrderItem;
+import com.example.springdatajpa.enums.OrderStatus;
 import com.example.springdatajpa.repository.OrderRepository;
 import com.example.springdatajpa.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,4 +35,46 @@ public class OrderApiController {
         return all;
     }
 
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        return orderRepository.findAllByString(new OrderSearch()).stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Data
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.orderItems = order.getOrderItems().stream().map(OrderItemDto::new).collect(Collectors.toList());
+            this.address = order.getDelivery().getAddress();
+        }
+
+    }
+
+    @Data
+    static class OrderItemDto {
+
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            this.itemName = orderItem.getItem().getName();
+            this.orderPrice = orderItem.getOrderPrice();
+            this.count = orderItem.getCount();
+        }
+
+    }
 }
