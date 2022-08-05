f80eb01a3b6afd86f6efa2f25a8a5b49a291bf7a
shyoon <dbstnsgh2@naver.com>
2022-08-05 14:10:55 +0900

    api-orders: 주문 조회 API V5 추가 (컬렉션 1:N 조회 성능 개선)

--- src/main/java/com/example/springdatajpa/repository/OrderQueryRepository.java
+++ src/main/java/com/example/springdatajpa/repository/OrderQueryRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -39,5 +41,33 @@ public class OrderQueryRepository {
                         " join o.member m" +
                         " join o.delivery d", OrderQueryDto.class
         ).getResultList();
+    }
+
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = findOrderItemMap(toOrderIds(result));
+
+        result.forEach(o -> {
+            o.setOrderItems(orderItemMap.get(o.getOrderId()));
+        });
+
+        return result;
+    }
+
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+                        "select new com.example.springdatajpa.repository.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count) from OrderItem oi" +
+                                " join oi.item i" +
+                                " where oi.order.id IN :orderIds", OrderItemQueryDto.class
+                )
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+
+        return orderItems.stream().collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
+    }
+
+    private List<Long> toOrderIds(List<OrderQueryDto> result) {
+        return result.stream().map(OrderQueryDto::getOrderId).collect(Collectors.toList());
     }
 }
