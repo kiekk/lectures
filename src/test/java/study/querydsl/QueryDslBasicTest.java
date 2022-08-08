f8ea52e70a91aac99f1815ae95ad004b2a8ef742
soonho <dbstnsgh2@naver.com>
2022-08-08 14:29:11 +0900

    query-dsl: 조인 - 3 (페치 조인 적용 - fetchJoin() 사용)

--- src/test/java/study/querydsl/QueryDslBasicTest.java
+++ src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -320,12 +320,13 @@ public class QueryDslBasicTest {
 
         Member findMember = queryFactory
                 .selectFrom(member)
+                .join(member.team, team).fetchJoin()
                 .where(member.username.eq("member1"))
                 .fetchOne();
 
         boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
 
-        assertThat(loaded).as("페치 조인 미적용").isFalse();
+        assertThat(loaded).as("페치 조인 적용").isTrue();
 
     }
 
