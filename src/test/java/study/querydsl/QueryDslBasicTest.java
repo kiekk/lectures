1f77b115f7d451cbd01809e4412d96c80983a904
soonho <dbstnsgh2@naver.com>
2022-08-08 13:50:06 +0900

    query-dsl: 결과 조회

--- src/test/java/study/querydsl/QueryDslBasicTest.java
+++ src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
@@ -87,5 +88,34 @@ public class QueryDslBasicTest {
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void resultFetch() {
+        // 목록 조회
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+        // 단 건 조회
+        Member fetchOne = queryFactory
+                .selectFrom(member)
+                .fetchOne();
+
+        // 처음 한 건 조회
+        Member fetchFirst = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+        // 페이징에서 사용
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        results.getTotal();
+        List<Member> content = results.getResults();
+
+        // count 쿼리로 변경
+        long count = queryFactory
+                .selectFrom(member)
+                .fetchCount();
     }
 }
