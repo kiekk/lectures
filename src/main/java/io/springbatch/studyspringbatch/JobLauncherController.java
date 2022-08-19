6b66675fd6d18418a1ad2e8a8284c659f7eb7ed6
shyoon <dbstnsgh2@naver.com>
2022-08-19 09:10:21 +0900

    오타 수정

--- src/main/java/io/springbatch/studyspringbatch/JobLauncherController.java
+++ src/main/java/io/springbatch/studyspringbatch/JobLauncherController.java
@@ -42,7 +42,7 @@ public class JobLauncherController {
         simpleJobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
         jobLauncher.run(job, jobParameters);
 
-        return "batch complted";
+        return "batch completed";
     }
 
 }
