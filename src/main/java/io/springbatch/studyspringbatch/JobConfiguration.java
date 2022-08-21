0bc1e3846561b5319ca5acc67463d78781a92c4d
shyoon <dbstnsgh2@naver.com>
2022-08-22 06:31:23 +0900

    batch-job: incrementer

--- src/main/java/io/springbatch/studyspringbatch/JobConfiguration.java
+++ src/main/java/io/springbatch/studyspringbatch/JobConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,7 +22,7 @@ public class JobConfiguration {
         return jobBuilderFactory.get("job")
                 .start(step1())
                 .next(step2())
-                .preventRestart()
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
