5b9f6c291f9d044d65acad347576182d8c3d21c7
shyoon <dbstnsgh2@naver.com>
2022-08-24 17:10:33 +0900

    batch-item-reader: FlatFileItemReaderBuilder - fixedLength

--- src/main/java/io/springbatch/studyspringbatch/JobConfiguration.java
+++ src/main/java/io/springbatch/studyspringbatch/JobConfiguration.java
@@ -8,10 +8,11 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.transform.Range;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -53,13 +54,15 @@ public class JobConfiguration {
     public ItemReader<Customer> itemReader() {
         return new FlatFileItemReaderBuilder<Customer>()
                 .name("flatFile")
-                .resource(new ClassPathResource("/customer.csv"))
+                .resource(new FileSystemResource("C:\\study\\springbatch\\src\\main\\resources\\customer.txt"))
                 .fieldSetMapper(new BeanWrapperFieldSetMapper<>())
                 .targetType(Customer.class)
                 .linesToSkip(1)
-                .delimited()
-                .delimiter(",")
-                .names("name", "age", "year")
+                .fixedLength()
+                .addColumns(new Range(1, 5))
+                .addColumns(new Range(6, 9))
+                .addColumns(new Range(10, 11))
+                .names("name", "year", "age")
                 .build();
     }
 
