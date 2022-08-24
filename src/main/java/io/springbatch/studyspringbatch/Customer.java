51ca64dc918c226d742fe002196d4622de40624a
shyoon <dbstnsgh2@naver.com>
2022-08-25 06:43:33 +0900

    batch-item-reader: StaxEventItemReader

--- src/main/java/io/springbatch/studyspringbatch/Customer.java
+++ src/main/java/io/springbatch/studyspringbatch/Customer.java
@@ -5,8 +5,8 @@ import lombok.Data;
 @Data
 public class Customer {
 
+    private long id;
     private String name;
     private int age;
-    private String year;
 
 }
--- src/main/java/io/springbatch/studyspringbatch/JobConfiguration.java
+++ src/main/java/io/springbatch/studyspringbatch/JobConfiguration.java
@@ -6,13 +6,16 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.batch.item.file.transform.Range;
+import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.oxm.xstream.XStreamMarshaller;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
@@ -52,19 +55,26 @@ public class JobConfiguration {
 
     @Bean
     public ItemReader<Customer> itemReader() {
-        return new FlatFileItemReaderBuilder<Customer>()
-                .name("flatFile")
-                .resource(new FileSystemResource("C:\\study\\springbatch\\src\\main\\resources\\customer.txt"))
-                .fieldSetMapper(new BeanWrapperFieldSetMapper<>())
-                .targetType(Customer.class)
-                .linesToSkip(1)
-                .fixedLength()
-                .strict(false)
-                .addColumns(new Range(1, 5))
-                .addColumns(new Range(6, 9))
-                .addColumns(new Range(10, 11))
-                .names("name", "year", "age")
+        return new StaxEventItemReaderBuilder<Customer>()
+                .name("statXml")
+                .resource(new ClassPathResource("customer.xml"))
+                .addFragmentRootElements("customer")
+                .unmarshaller(itemUnmarshaller())
                 .build();
+    }
+
+    @Bean
+    public Unmarshaller itemUnmarshaller() {
+        Map<String, Class<?>> aliases = new HashMap<>();
+        aliases.put("customer", Customer.class);
+        aliases.put("id", Long.class);
+        aliases.put("name", String.class);
+        aliases.put("age", Integer.class);
+
+        XStreamMarshaller xStreamMarshaller = new XStreamMarshaller();
+        xStreamMarshaller.setAliases(aliases);
+
+        return xStreamMarshaller;
     }
 
 }
