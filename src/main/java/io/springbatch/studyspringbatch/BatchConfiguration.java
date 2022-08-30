44ee06d2d1a5172a0dc57d3897aca5699c56cd77
shyoon <dbstnsgh2@naver.com>
2022-08-30 09:37:06 +0900

    multi-thread-processing: not thread safe item reader

--- src/main/java/io/springbatch/studyspringbatch/BatchConfiguration.java
+++ src/main/java/io/springbatch/studyspringbatch/BatchConfiguration.java
@@ -1,6 +1,7 @@
 package io.springbatch.studyspringbatch;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.ItemReadListener;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -9,17 +10,15 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.database.JdbcPagingItemReader;
-import org.springframework.batch.item.database.Order;
-import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
@@ -33,72 +32,50 @@ public class BatchConfiguration {
     public Job job() throws Exception {
         return jobBuilderFactory.get("batchJob")
                 .incrementer(new RunIdIncrementer())
-                .start(masterStep())
+                .start(step1())
                 .build();
     }
 
     @Bean
-    public Step masterStep() throws Exception {
-        return stepBuilderFactory.get("masterStep")
-                .partitioner(slaveStep().getName(), partitioner())
-                .step(slaveStep())
-                .gridSize(4)
-                .taskExecutor(new SimpleAsyncTaskExecutor())
-                .build();
-    }
-
-    @Bean
-    public Step slaveStep() {
-        return stepBuilderFactory.get("slaveStep")
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
                 .<Customer, Customer>chunk(100)
-                .reader(pagingItemReader(null, null))
-                .writer(customerItemWriter())
+                .reader(customItemReader())
+                .listener(new ItemReadListener<>() {
+                    @Override
+                    public void beforeRead() {
+                    }
+
+                    @Override
+                    public void afterRead(Customer item) {
+                        System.out.println("item.getId() : " + item.getId());
+                    }
+
+                    @Override
+                    public void onReadError(Exception ex) {
+                    }
+                })
+                .writer(customItemWriter())
+                .taskExecutor(taskExecutor())
                 .build();
     }
 
     @Bean
-    public ColumnRangePartitioner partitioner() {
-        ColumnRangePartitioner columnRangePartitioner = new ColumnRangePartitioner();
-
-        columnRangePartitioner.setColumn("id");
-        columnRangePartitioner.setDataSource(this.dataSource);
-        columnRangePartitioner.setTable("customer");
-
-        return columnRangePartitioner;
-    }
-
-
-    @Bean
     @StepScope
-    public JdbcPagingItemReader<Customer> pagingItemReader(
-            @Value("#{stepExecutionContext['minValue']}") Long minValue,
-            @Value("#{stepExecutionContext['maxValue']}") Long maxValue) {
-        System.out.println("reading " + minValue + " to " + maxValue);
-        JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
-
-        reader.setDataSource(this.dataSource);
-        reader.setFetchSize(1000);
-        reader.setRowMapper(new CustomerRowMapper());
-
-        MySqlPagingQueryProvider queryProvider = new MySqlPagingQueryProvider();
-        queryProvider.setSelectClause("id, firstName, lastName, birthdate");
-        queryProvider.setFromClause("from customer");
-        queryProvider.setWhereClause("where id >= " + minValue + " and id < " + maxValue);
-
-        Map<String, Order> sortKeys = new HashMap<>(1);
-
-        sortKeys.put("id", Order.ASCENDING);
-
-        queryProvider.setSortKeys(sortKeys);
-
-        reader.setQueryProvider(queryProvider);
-
-        return reader;
+    public JdbcCursorItemReader<Customer> customItemReader() {
+        return new JdbcCursorItemReaderBuilder<Customer>()
+//                .fetchSize(100)
+                .dataSource(this.dataSource)
+                .beanRowMapper(Customer.class)
+//                .rowMapper(new BeanPropertyRowMapper<>(Customer.class))
+                .sql("select id, firstName, lastName, birthdate from customer")
+                .name("NotSafetyReader")
+                .build();
     }
 
     @Bean
     @StepScope
-    public JdbcBatchItemWriter<Customer> customerItemWriter() {
+    public JdbcBatchItemWriter<Customer> customItemWriter() {
         JdbcBatchItemWriter<Customer> itemWriter = new JdbcBatchItemWriter<>();
 
         itemWriter.setDataSource(this.dataSource);
@@ -109,5 +86,13 @@ public class BatchConfiguration {
         return itemWriter;
     }
 
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(8);
+        executor.setThreadNamePrefix("not-safety-thread-");
+        return executor;
+    }
 
 }
--- src/main/java/io/springbatch/studyspringbatch/Customer.java
+++ src/main/java/io/springbatch/studyspringbatch/Customer.java
@@ -2,16 +2,18 @@ package io.springbatch.studyspringbatch;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
 
-    private final long id;
-    private final String firstName;
-    private final String lastName;
-    private final Date birthdate;
+    private long id;
+    private String firstName;
+    private String lastName;
+    private Date birthdate;
 
 }
