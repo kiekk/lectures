8b06fd5ff24c56a82bc22d44e5fe624e9165f443
shyoon <dbstnsgh2@naver.com>
2022-09-01 14:56:19 +0900

    batch-test: JobExplorer / JobRegistry / JobOperator

---
+++ src/main/java/io/springbatch/studyspringbatch/JobInfo.java
@@ -0,0 +1,8 @@
+package io.springbatch.studyspringbatch;
+
+import lombok.Data;
+
+@Data
+public class JobInfo {
+    private String id;
+}
--- src/main/java/io/springbatch/studyspringbatch/SimpleJobConfiguration.java
+++ src/main/java/io/springbatch/studyspringbatch/SimpleJobConfiguration.java
@@ -3,79 +3,64 @@ package io.springbatch.studyspringbatch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.database.JdbcPagingItemReader;
-import org.springframework.batch.item.database.Order;
-import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
 public class SimpleJobConfiguration {
 
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory;
-    private final DataSource dataSource;
+    public final JobBuilderFactory jobBuilderFactory;
+    public final StepBuilderFactory stepBuilderFactory;
+    public final JobRegistry jobRegistry;
 
     @Bean
-    public Job job() throws Exception {
+    public Job job1() throws Exception {
         return jobBuilderFactory.get("batchJob")
                 .incrementer(new RunIdIncrementer())
                 .start(step1())
+                .next(step2())
                 .build();
     }
 
     @Bean
     public Step step1() throws Exception {
         return stepBuilderFactory.get("step1")
-                .<Customer, Customer>chunk(100)
-                .reader(customItemReader())
-                .writer(customItemWriter())
+                .tasklet((contribution, chunkContext) ->
+                        {
+                            System.out.println("step1 was executed");
+                            Thread.sleep(3000);
+                            return RepeatStatus.FINISHED;
+                        }
+                )
                 .build();
     }
 
     @Bean
-    public JdbcPagingItemReader<Customer> customItemReader() {
-        JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
-
-        reader.setDataSource(this.dataSource);
-        reader.setPageSize(100);
-        reader.setRowMapper(new CustomerRowMapper());
-
-        MySqlPagingQueryProvider queryProvider = new MySqlPagingQueryProvider();
-        queryProvider.setSelectClause("id, firstName, lastName, birthdate");
-        queryProvider.setFromClause("from customer");
-
-        Map<String, Order> sortKeys = new HashMap<>(1);
-
-        sortKeys.put("id", Order.ASCENDING);
-
-        queryProvider.setSortKeys(sortKeys);
-
-        reader.setQueryProvider(queryProvider);
-
-        return reader;
+    public Step step2() throws Exception {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) ->
+                        {
+                            System.out.println("step2 was executed");
+                            Thread.sleep(3000);
+                            return RepeatStatus.FINISHED;
+                        }
+                )
+                .build();
     }
 
     @Bean
-    public JdbcBatchItemWriter<Customer> customItemWriter() {
-        JdbcBatchItemWriter<Customer> itemWriter = new JdbcBatchItemWriter<>();
-
-        itemWriter.setDataSource(this.dataSource);
-        itemWriter.setSql("insert into customer2 values (:id, :firstName, :lastName, :birthdate)");
-        itemWriter.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
-        itemWriter.afterPropertiesSet();
-
-        return itemWriter;
+    public BeanPostProcessor jobRegistryBeanPostProcessor() throws Exception {
+        JobRegistryBeanPostProcessor postProcessor = new JobRegistryBeanPostProcessor();
+        postProcessor.setJobRegistry(jobRegistry);
+        return postProcessor;
     }
 
 }
