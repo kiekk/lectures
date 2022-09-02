55158a5c5103700c4bc6277628ff4e436da6f736
shyoon <dbstnsgh2@naver.com>
2022-09-02 15:29:24 +0900

    application: add service ApiService

---
+++ src/main/java/io/springbatch/studyspringbatch/service/ApiService1.java
@@ -0,0 +1,18 @@
+package io.springbatch.studyspringbatch.service;
+
+import io.springbatch.studyspringbatch.batch.domain.ApiInfo;
+import io.springbatch.studyspringbatch.batch.domain.ApiResponseVO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ApiService1 extends AbstractApiService {
+
+    @Override
+    protected ApiResponseVO doApiService(RestTemplate restTemplate, ApiInfo apiInfo) {
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity("http://localhost:8081/product/1", apiInfo, String.class);
+        int statusCodeValue = responseEntity.getStatusCodeValue();
+        return ApiResponseVO.builder().status(statusCodeValue).msg(responseEntity.getBody()).build();
+    }
+}
