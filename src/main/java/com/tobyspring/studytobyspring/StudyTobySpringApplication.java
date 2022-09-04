b11ec7abf298a83d31b9e33f393ecff8c80f801d
soonho <dbstnsgh2@naver.com>
2022-09-04 19:52:39 +0900

    ch01: Object 직접 생성과 Bean 조회 시 동일성 비교

--- src/main/java/com/tobyspring/studytobyspring/StudyTobySpringApplication.java
+++ src/main/java/com/tobyspring/studytobyspring/StudyTobySpringApplication.java
@@ -16,24 +16,39 @@ public class StudyTobySpringApplication {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 //        SpringApplication.run(StudyTobySpringApplication.class, args);
 
-        // DaoFactory Bean 으로 생성
+        // 직접 DaoFactory 를 생성할 경우 매번 새로운 Object 가 생성됨
+        // Bean 으로 조회 시 기본 Singleton 으로 생성되기 떄문에
+        // 매번 같은 Object 가 반환됨.
+        DaoFactory factory = new DaoFactory();
+        UserDao dao1 = factory.userDao();
+        UserDao dao2 = factory.userDao();
+
+        System.out.println("dao1 : " + dao1);
+        System.out.println("dao2 : " + dao2);
+
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        System.out.println("");
-        UserDao dao = context.getBean("userDao", UserDao.class);
-        User user = new User();
+        UserDao dao3 = context.getBean("userDao", UserDao.class);
+        UserDao dao4 = context.getBean("userDao", UserDao.class);
 
-        user.setId(UUID.randomUUID().toString());
-        user.setName("순호");
-        user.setPassword("1234");
+        System.out.println("dao3 : " + dao3);
+        System.out.println("dao4 : " + dao4);
 
-        dao.add(user);
-
-        System.out.println(user.getId() + " 등록 성공");
-
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-        System.out.println(user2.getId() + " 조회 성공");
+//        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+//        UserDao dao = context.getBean("userDao", UserDao.class);
+//        User user = new User();
+//
+//        user.setId(UUID.randomUUID().toString());
+//        user.setName("순호");
+//        user.setPassword("1234");
+//
+//        dao.add(user);
+//
+//        System.out.println(user.getId() + " 등록 성공");
+//
+//        User user2 = dao.get(user.getId());
+//        System.out.println(user2.getName());
+//        System.out.println(user2.getPassword());
+//        System.out.println(user2.getId() + " 조회 성공");
 
 
     }
