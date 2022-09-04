4c1c0e6b8afcc5da78cb716c76195349a0cace90
soonho <dbstnsgh2@naver.com>
2022-09-04 17:46:56 +0900

    Initial Commit

---
+++ src/main/java/com/tobyspring/studytobyspring/StudyTobySpringApplication.java
@@ -0,0 +1,13 @@
+package com.tobyspring.studytobyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTobySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyTobySpringApplication.class, args);
+    }
+
+}
