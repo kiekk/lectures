c3175cb06290bace5d8b8cd1c24ef7422fe435e6
soonho <dbstnsgh2@naver.com>
2022-09-04 17:49:26 +0900

    ch01: add domain User

---
+++ src/main/java/com/tobyspring/studytobyspring/domain/User.java
@@ -0,0 +1,12 @@
+package com.tobyspring.studytobyspring.domain;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String id;
+    private String name;
+    private String password;
+
+}
