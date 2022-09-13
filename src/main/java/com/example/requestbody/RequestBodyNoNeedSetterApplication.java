bbe7337794f16acfca3711e78f49e07263ab791f
soonho <dbstnsgh2@naver.com>
2022-09-13 22:08:07 +0900

    Initial Commit

---
+++ src/main/java/com/example/requestbody/RequestBodyNoNeedSetterApplication.java
@@ -0,0 +1,13 @@
+package com.example.requestbody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestBodyNoNeedSetterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestBodyNoNeedSetterApplication.class, args);
+    }
+
+}
