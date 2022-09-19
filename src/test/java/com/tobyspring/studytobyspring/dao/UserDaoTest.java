8fb488ea41abb717a221c19d08e0d930babad2e1
soonho <dbstnsgh2@naver.com>
2022-09-19 21:03:51 +0900

    ch02: add test deleteAll / getCount

--- src/test/java/com/tobyspring/studytobyspring/dao/UserDaoTest.java
+++ src/test/java/com/tobyspring/studytobyspring/dao/UserDaoTest.java
@@ -34,5 +34,20 @@ public class UserDaoTest {
 
         assertThat(user2.getName()).isEqualTo(user.getName());
         assertThat(user2.getPassword()).isEqualTo(user.getPassword());
+
+        dao.deleteAll();
+        assertThat(dao.getCount()).isEqualTo(0);
+
+        User user3 = new User();
+        user3.setId("zooni");
+        user3.setName("주니");
+        user3.setPassword("1234");
+
+        dao.add(user3);
+
+        User user4 = dao.get(user3.getId());
+
+        assertThat(user3.getName()).isEqualTo(user4.getName());
+        assertThat(user3.getPassword()).isEqualTo(user4.getPassword());
     }
 }
