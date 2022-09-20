7388b2916efa41ec3fe0885e253d8c0d4ba8e9bf
soonho <dbstnsgh2@naver.com>
2022-09-20 21:01:25 +0900

    ch03: try-catch-finally 구문으로 커넥션 무조건 반환

--- src/main/java/com/tobyspring/studytobyspring/dao/UserDao.java
+++ src/main/java/com/tobyspring/studytobyspring/dao/UserDao.java
@@ -21,44 +21,63 @@ public class UserDao {
         this.connectionMaker = context.getBean("connectionMaker", ConnectionMaker.class);
     }
 
+    /*
+    일반적으로 서버에서는 제한된 개수의 DB 커넥션을 만들어서 재사용 가능한
+    풀로 관리하기 때문에 매번 사용한 커넥션을 명시적으로 close() 해서 돌려줘야 합니다.
+    어떤 상황에서도 커넥션이 반환될 수 있도록 try-catch-finally 구문으로 작성합니다.
+     */
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
-        PreparedStatement ps = c.prepareStatement(
-                "insert into users (id, name, password) values (?, ?, ?)"
-        );
+        Connection c = null;
+        PreparedStatement ps = null;
+        try {
+            c = connectionMaker.makeConnection();
+            ps = c.prepareStatement(
+                    "insert into users (id, name, password) values (?, ?, ?)"
+            );
 
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
 
-        ps.executeUpdate();
-
-        ps.close();
-        c.close();
+            ps.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            ps.close();
+            c.close();
+        }
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
-        PreparedStatement ps = c.prepareStatement(
-                "select * from users where id = ?"
-        );
-
-        ps.setString(1, id);
-
-        ResultSet rs = ps.executeQuery();
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
         User user = null;
+        try {
+            c = connectionMaker.makeConnection();
+            ps = c.prepareStatement(
+                    "select * from users where id = ?"
+            );
 
-        if (rs.next()) {
-            user = new User();
-            user.setId(rs.getString("id"));
-            user.setName(rs.getString("name"));
-            user.setPassword(rs.getString("password"));
+            ps.setString(1, id);
+
+            rs = ps.executeQuery();
+
+
+            if (rs.next()) {
+                user = new User();
+                user.setId(rs.getString("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            rs.close();
+            ps.close();
+            c.close();
         }
-
-        rs.close();
-        ps.close();
-        c.close();
 
         if (user == null) {
             throw new EmptyResultDataAccessException(1);
@@ -68,30 +87,41 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException, ClassNotFoundException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = null;
+        PreparedStatement ps = null;
+        try {
+            c = connectionMaker.makeConnection();
 
-        PreparedStatement ps = c.prepareStatement("delete from users");
+            ps = c.prepareStatement("delete from users");
 
-        ps.executeUpdate();
-
-        ps.close();
-        c.close();
+            ps.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            ps.close();
+            c.close();
+        }
     }
 
     public int getCount() throws SQLException, ClassNotFoundException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            c = connectionMaker.makeConnection();
 
-        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+            ps = c.prepareStatement("select count(*) from users");
 
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int count = rs.getInt(1);
-
-        rs.close();
-        ps.close();
-        c.close();
-
-        return count;
+            rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            rs.close();
+            ps.close();
+            c.close();
+        }
     }
 
 }
