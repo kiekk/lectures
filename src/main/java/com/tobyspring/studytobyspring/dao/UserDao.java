655f88918d727a5b4061e0e3ee3cce2227934eef
soonho <dbstnsgh2@naver.com>
2022-09-25 01:33:00 +0900

    ch04: 예외 블랙홀 (무책임한 예외 처리)

--- src/main/java/com/tobyspring/studytobyspring/dao/UserDao.java
+++ src/main/java/com/tobyspring/studytobyspring/dao/UserDao.java
@@ -2,12 +2,12 @@ package com.tobyspring.studytobyspring.dao;
 
 import com.tobyspring.studytobyspring.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.UncategorizedSQLException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -32,8 +32,13 @@ public class UserDao {
     }
 
     public void add(final User user) throws ClassNotFoundException, SQLException {
-        this.jdbcTemplate.update("insert into users(id, name, password) values (?, ?, ?)",
-                user.getId(), user.getName(), user.getPassword());
+        try {
+            this.jdbcTemplate.update("insert into users(id, name, password) values (?, ?, ?)",
+                    user.getId(), user.getName(), user.getPassword());
+        } catch (UncategorizedSQLException e) {
+            // 예외 블랙홀
+            // trh-catch 문으로 예외를 캐치했지만 아무 처리도 하지 않는 것은 매우 위험
+        }
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
