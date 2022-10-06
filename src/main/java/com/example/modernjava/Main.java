f4623d23f7310377c4a9549ca4e4c54197d68aee
shyoon <dbstnsgh2@naver.com>
2022-10-06 16:54:46 +0900

    ch01: 반복문을 사용하여 number list 출력 - 2

--- src/main/java/com/example/modernjava/Main.java
+++ src/main/java/com/example/modernjava/Main.java
@@ -11,9 +11,23 @@ public class Main {
         final StringBuilder builder = new StringBuilder();
 
         // 1. 반복문을 사용하여 numbers 사이에 : 추가하기
-        for (Integer number : numbers) {
-            builder.append(number).append(" : ");
+//        for (Integer number : numbers) {
+//            builder.append(number).append(" : ");
+//        }
+
+        // 2. 마지막 " : " 제거하기
+//      for (int i = 0; i < numbers.size(); i++) {
+//      for 문에 numbers.size() 를 작성하면 반복시마다 numbers.size() 를 호출하여 비효율
+        final int size = numbers.size();
+        for (int i = 0; i < size; i++) {
+            builder.append(numbers.get(i));
+            if (i != size - 1) {
+                builder.append(" : ");
+            }
         }
+        // 단점
+        // 1. index 로 numbers 에 접근하기 때문에 index 를 잘 못 설정할 경우 ArrayIndexOutOfBoundsException 발생합니다.
+        // 2. if 문이 어떤 의도인지 파악하기 쉽지 않습니다.
 
         System.out.println(builder);
     }
