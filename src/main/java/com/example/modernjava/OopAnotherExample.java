9cc952c4115098ba8aceae1c0b166a867ba6669e
shyoon <dbstnsgh2@naver.com>
2022-10-07 15:46:58 +0900

    ch01-1: CalculatorService - add business logic

--- src/main/java/com/example/modernjava/OopAnotherExample.java
+++ src/main/java/com/example/modernjava/OopAnotherExample.java
@@ -47,6 +47,10 @@ class CalculatorService {
     }
 
     public int calculate(int num1, int num2) {
-        return calculation.calculate(num1, num2);
+        if (num1 > 10 && num2 < num1) {
+            return calculation.calculate(num1, num2);
+        } else {
+            throw new IllegalArgumentException("Invalid input num1: " + num1 + ", num2: " + num2);
+        }
     }
 }
