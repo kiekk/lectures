d40c2e300290dbc17356ea406ebbcd504524ba8e
shyoon <dbstnsgh2@naver.com>
2022-10-07 15:37:20 +0900

    ch01-1: CalculatorService 개선 - Strategy Pattern + Composite Pattern 사용하여 덧셈 기능 개선

--- src/main/java/com/example/modernjava/OopAnotherExample.java
+++ src/main/java/com/example/modernjava/OopAnotherExample.java
@@ -3,38 +3,29 @@ package com.example.modernjava;
 public class OopAnotherExample {
 
     public static void main(String[] args) {
-        CalculatorService calculatorService = new CalculatorService();
-        int additionResult = calculatorService.calculate('+', 1, 1);
-        System.out.println(additionResult);
+    }
+}
 
-        int subtractionResult = calculatorService.calculate('-', 1, 1);
-        System.out.println(subtractionResult);
+interface Calculation {
+    int calculate(int num1, int num2);
+}
 
-        int multiplicationResult = calculatorService.calculate('*', 1, 1);
-        System.out.println(multiplicationResult);
-
-        int divisionResult = calculatorService.calculate('/', 9, 3);
-        System.out.println(divisionResult);
+class Addition implements Calculation {
+    @Override
+    public int calculate(int num1, int num2) {
+        return num1 + num2;
     }
 }
 
 class CalculatorService {
-    // 요구사항 추가
-    // 1. 뺄셈 기능 추가 - calculation 추가하여 덧셈 / 뺄셈 처리
-    // 2. 곱셈 기능 추가 - calculation if문 추가
-    // 3. 나눗셈 기능 추가 - calculation if문 추가
-    public int calculate(char calculation, int num1, int num2) {
 
-        if (calculation == '+') {
-            return num1 + num2;
-        } else if (calculation == '-') {
-            return num1 - num2;
-        } else if (calculation == '*') {
-            return num1 * num2;
-        } else if (calculation == '/') {
-            return num1 / num2;
-        } else {
-            throw new IllegalArgumentException("Unknown calculation : " + calculation);
-        }
+    private final Calculation calculation;
+
+    public CalculatorService(Calculation calculation) {
+        this.calculation = calculation;
+    }
+
+    public int calculate(int num1, int num2) {
+        return calculation.calculate(num1, num2);
     }
 }
--- src/test/java/com/example/modernjava/CalculatorServiceTest.java
+++ src/test/java/com/example/modernjava/CalculatorServiceTest.java
@@ -9,41 +9,41 @@ class CalculatorServiceTest {
 
     @Test
     public void testCalculateAddition() {
-        CalculatorService calculatorService = new CalculatorService();
-        int additionResult = calculatorService.calculate('+', 1, 1);
+        CalculatorService calculatorService = new CalculatorService(new Addition());
+        int additionResult = calculatorService.calculate(1, 1);
 
         assertEquals(additionResult, 2);
     }
 
-    @Test
-    public void testCalculateSubtraction() {
-        CalculatorService calculatorService = new CalculatorService();
-        int additionResult = calculatorService.calculate('-', 1, 1);
-
-        assertEquals(additionResult, 0);
-    }
-
-    @Test
-    public void testCalculateMultiplication() {
-        CalculatorService calculatorService = new CalculatorService();
-        int additionResult = calculatorService.calculate('*', 1, 1);
-
-        assertEquals(additionResult, 1);
-    }
-
-    @Test
-    public void testCalculateDivision() {
-        CalculatorService calculatorService = new CalculatorService();
-        int additionResult = calculatorService.calculate('/', 9, 3);
-
-        assertEquals(additionResult, 3);
-    }
-
-    @Test
-    public void testCalculateDivisionFailed() {
-        // 0 으로 나누면 에러 발생
-        CalculatorService calculatorService = new CalculatorService();
-        assertThrows(ArithmeticException.class, () -> calculatorService.calculate('/', 9, 0));
-    }
+//    @Test
+//    public void testCalculateSubtraction() {
+//        CalculatorService calculatorService = new CalculatorService();
+//        int additionResult = calculatorService.calculate('-', 1, 1);
+//
+//        assertEquals(additionResult, 0);
+//    }
+//
+//    @Test
+//    public void testCalculateMultiplication() {
+//        CalculatorService calculatorService = new CalculatorService();
+//        int additionResult = calculatorService.calculate('*', 1, 1);
+//
+//        assertEquals(additionResult, 1);
+//    }
+//
+//    @Test
+//    public void testCalculateDivision() {
+//        CalculatorService calculatorService = new CalculatorService();
+//        int additionResult = calculatorService.calculate('/', 9, 3);
+//
+//        assertEquals(additionResult, 3);
+//    }
+//
+//    @Test
+//    public void testCalculateDivisionFailed() {
+//        // 0 으로 나누면 에러 발생
+//        CalculatorService calculatorService = new CalculatorService();
+//        assertThrows(ArithmeticException.class, () -> calculatorService.calculate('/', 9, 0));
+//    }
 
 }
