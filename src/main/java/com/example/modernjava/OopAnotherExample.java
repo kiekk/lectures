2bf4106ede14832457208d0605ab2b9ffba16f55
shyoon <dbstnsgh2@naver.com>
2022-10-07 15:50:40 +0900

    ch01-1: CalculatorService - add method compute

--- src/main/java/com/example/modernjava/OopAnotherExample.java
+++ src/main/java/com/example/modernjava/OopAnotherExample.java
@@ -41,14 +41,24 @@ class Division implements Calculation {
 class CalculatorService {
 
     private final Calculation calculation;
+    private final Calculation calculation2;
 
-    public CalculatorService(Calculation calculation) {
+    public CalculatorService(Calculation calculation, Calculation calculation2) {
         this.calculation = calculation;
+        this.calculation2 = calculation2;
     }
 
     public int calculate(int num1, int num2) {
         if (num1 > 10 && num2 < num1) {
             return calculation.calculate(num1, num2);
+        } else {
+            throw new IllegalArgumentException("Invalid input num1: " + num1 + ", num2: " + num2);
+        }
+    }
+
+    public int compute(int num1, int num2) {
+        if (num1 > 10 && num2 < num1) {
+            return calculation2.calculate(num1, num2);
         } else {
             throw new IllegalArgumentException("Invalid input num1: " + num1 + ", num2: " + num2);
         }
