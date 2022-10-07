cdd85d9e40e0fd114d0707bb2e72730d523b7be4
shyoon <dbstnsgh2@naver.com>
2022-10-07 15:30:09 +0900

    ch01-1: add test CalculatorService - substraction / multiplication / division

--- src/test/java/com/example/modernjava/CalculatorServiceTest.java
+++ src/test/java/com/example/modernjava/CalculatorServiceTest.java
@@ -13,4 +13,29 @@ class CalculatorServiceTest {
 
         assertEquals(additionResult, 2);
     }
+
+    @Test
+    public void testCalculateSubtraction() {
+        CalculatorService calculatorService = new CalculatorService();
+        int additionResult = calculatorService.calculate('-', 1, 1);
+
+        assertEquals(additionResult, 0);
+    }
+
+    @Test
+    public void testCalculateMultiplication() {
+        CalculatorService calculatorService = new CalculatorService();
+        int additionResult = calculatorService.calculate('*', 1, 1);
+
+        assertEquals(additionResult, 1);
+    }
+
+    @Test
+    public void testCalculateDivision() {
+        CalculatorService calculatorService = new CalculatorService();
+        int additionResult = calculatorService.calculate('/', 9, 3);
+
+        assertEquals(additionResult, 3);
+    }
+
 }
