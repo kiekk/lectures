b9c20382a8b3d824cd686cf2d38a9504101868aa
shyoon <dbstnsgh2@naver.com>
2022-10-07 15:31:39 +0900

    ch01-1: calculate 에러 확인 - divide 시 0으로 나누면 에러 발생

--- src/test/java/com/example/modernjava/CalculatorServiceTest.java
+++ src/test/java/com/example/modernjava/CalculatorServiceTest.java
@@ -3,6 +3,7 @@ package com.example.modernjava;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CalculatorServiceTest {
 
@@ -36,6 +37,13 @@ class CalculatorServiceTest {
         int additionResult = calculatorService.calculate('/', 9, 3);
 
         assertEquals(additionResult, 3);
+    }
+
+    @Test
+    public void testCalculateDivisionFailed() {
+        // 0 으로 나누면 에러 발생
+        CalculatorService calculatorService = new CalculatorService();
+        assertThrows(ArithmeticException.class, () -> calculatorService.calculate('/', 9, 0));
     }
 
 }
