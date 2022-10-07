a0792c30c1088b2a1d4329cafb7362ad3bbc1f6b
shyoon <dbstnsgh2@naver.com>
2022-10-07 16:23:14 +0900

    ch01-1: add test FpCalculatorService - Calculator 구현체 필요 없이 익명 클래스 / 람다식으로 대체

--- src/test/java/com/example/modernjava/FpCalculatorServiceTest.java
+++ src/test/java/com/example/modernjava/FpCalculatorServiceTest.java
@@ -9,7 +9,7 @@ class FpCalculatorServiceTest {
     @Test
     public void testFpCalculateAddition() {
         FpCalculatorService fpCalculatorService = new FpCalculatorService();
-        int additionResult = fpCalculatorService.calculate(new Addition(), 11, 10);
+        int additionResult = fpCalculatorService.calculate((num1, num2) -> num1 + num2, 11, 10);
 
         assertEquals(additionResult, 21);
     }
@@ -17,7 +17,7 @@ class FpCalculatorServiceTest {
     @Test
     public void testFpCalculateSubtraction() {
         FpCalculatorService fpCalculatorService = new FpCalculatorService();
-        int additionResult = fpCalculatorService.calculate(new Subtraction(), 11, 10);
+        int additionResult = fpCalculatorService.calculate((num1, num2) -> num1 - num2, 11, 10);
 
         assertEquals(additionResult, 1);
     }
@@ -25,7 +25,7 @@ class FpCalculatorServiceTest {
     @Test
     public void testFpCalculateMultiplication() {
         FpCalculatorService fpCalculatorService = new FpCalculatorService();
-        int additionResult = fpCalculatorService.calculate(new Multiplication(), 11, 10);
+        int additionResult = fpCalculatorService.calculate((num1, num2) -> num1 * num2, 11, 10);
 
         assertEquals(additionResult, 110);
     }
@@ -33,7 +33,7 @@ class FpCalculatorServiceTest {
     @Test
     public void testFpCalculateDivision() {
         FpCalculatorService fpCalculatorService = new FpCalculatorService();
-        int additionResult = fpCalculatorService.calculate(new Division(), 11, 10);
+        int additionResult = fpCalculatorService.calculate((num1, num2) -> num1 / num2, 11, 10);
 
         assertEquals(additionResult, 1);
     }
