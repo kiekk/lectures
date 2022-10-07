cc39924b3e3cfe5ef81a1e818814cf4fb45cc347
shyoon <dbstnsgh2@naver.com>
2022-10-07 15:18:18 +0900

    ch01-1: 요구사항 추가에 따른 CalculatorService 변경 - 2

--- src/main/java/com/example/modernjava/OopAnotherExample.java
+++ src/main/java/com/example/modernjava/OopAnotherExample.java
@@ -9,18 +9,24 @@ public class OopAnotherExample {
 
         int subtractionResult = calculatorService.calculate('-', 1, 1);
         System.out.println(subtractionResult);
+
+        int multiplicationResult = calculatorService.calculate('*', 1, 1);
+        System.out.println(multiplicationResult);
     }
 }
 
 class CalculatorService {
     // 요구사항 추가
     // 1. 뺄셈 기능 추가 - calculation 추가하여 덧셈 / 뺄셈 처리
+    // 2. 곱셈 기능 추가 - calculation if문 추가
     public int calculate(char calculation, int num1, int num2) {
 
         if (calculation == '+') {
             return num1 + num2;
         } else if (calculation == '-') {
             return num1 - num2;
+        } else if (calculation == '*') {
+            return num1 * num2;
         } else {
             throw new IllegalArgumentException("Unknown calculation : " + calculation);
         }
