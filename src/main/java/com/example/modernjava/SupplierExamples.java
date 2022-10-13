9218390c52f4f6f55ad061de9a5db8d3755b64b4
soonho <dbstnsgh2@naver.com>
2022-10-13 17:02:14 +0900

    ch05: Supplier 사용

--- src/main/java/com/example/modernjava/SupplierExamples.java
+++ src/main/java/com/example/modernjava/SupplierExamples.java
@@ -13,18 +13,19 @@ public class SupplierExamples {
         // 왜 굳이 Supplier 를 사용해야 하는가?
 
         long start = System.currentTimeMillis();
-        printIfValid(0, "Hello");
-        printIfValid(1, "Hello");
-        printIfValid(-1, "Hello");
+        printIfValid(0, () -> "Hello");
+        printIfValid(1, () -> "Hello");
+        printIfValid(-1, () -> "Hello");
         System.out.println("It took : " + ((System.currentTimeMillis() - start) / 1000) + " seconds");
 
         long start2 = System.currentTimeMillis();
-        printIfValid(0, getVeryExpensiveValue());
-        printIfValid(1, getVeryExpensiveValue());
-        printIfValid(-1, getVeryExpensiveValue());
+        printIfValid(0, SupplierExamples::getVeryExpensiveValue);
+        printIfValid(1, SupplierExamples::getVeryExpensiveValue);
+        printIfValid(-1, SupplierExamples::getVeryExpensiveValue);
 
-        // 실제로는 printIfValid(0, getVeryExpensiveValue()); 에서만 value 가 사용되기 때문에
-        // 나머지 메소드 실행 시간인 6초가 낭비됨.
+        // Supplier 적용시 실제 값을 사용할 때 호출
+        // 실제 값이 출력되는 printIfValid(0, SupplierExamples::getVeryExpensiveValue); printIfValid(1, SupplierExamples::getVeryExpensiveValue); 만 실행되어
+        // 총 6초의 시간이 소요
         System.out.println("It took : " + ((System.currentTimeMillis() - start2) / 1000) + " seconds");
     }
 
@@ -38,10 +39,10 @@ public class SupplierExamples {
         return "Hello";
     }
 
-    private static void printIfValid(int number, String value) {
+    private static void printIfValid(int number, Supplier<String> valueSupplier) {
         // 조건에 부합할 경우에만 value 가 사용됨.
         if (number >= 0) {
-            System.out.println("The value is " + value + ".");
+            System.out.println("The value is " + valueSupplier.get() + ".");
         } else {
             System.out.println("Invalid");
         }
