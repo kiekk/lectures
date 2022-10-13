d88fe4d6ea349c83452d529e41ee48b5d55c04d2
soonho <dbstnsgh2@naver.com>
2022-10-13 16:59:00 +0900

    ch05: Supplier - 일반 메소드 사용시 단점

--- src/main/java/com/example/modernjava/SupplierExamples.java
+++ src/main/java/com/example/modernjava/SupplierExamples.java
@@ -1,5 +1,6 @@
 package com.example.modernjava;
 
+import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
 public class SupplierExamples {
@@ -11,9 +12,30 @@ public class SupplierExamples {
 
         // 왜 굳이 Supplier 를 사용해야 하는가?
 
+        long start = System.currentTimeMillis();
         printIfValid(0, "Hello");
         printIfValid(1, "Hello");
         printIfValid(-1, "Hello");
+        System.out.println("It took : " + ((System.currentTimeMillis() - start) / 1000) + " seconds");
+
+        long start2 = System.currentTimeMillis();
+        printIfValid(0, getVeryExpensiveValue());
+        printIfValid(1, getVeryExpensiveValue());
+        printIfValid(-1, getVeryExpensiveValue());
+
+        // 실제로는 printIfValid(0, getVeryExpensiveValue()); 에서만 value 가 사용되기 때문에
+        // 나머지 메소드 실행 시간인 6초가 낭비됨.
+        System.out.println("It took : " + ((System.currentTimeMillis() - start2) / 1000) + " seconds");
+    }
+
+    // 생성 비용이 매우 비싼 객체를 반환받는다고 가정
+    private static String getVeryExpensiveValue() {
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return "Hello";
     }
 
     private static void printIfValid(int number, String value) {
