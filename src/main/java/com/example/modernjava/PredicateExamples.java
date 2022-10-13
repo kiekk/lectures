01f2181bd4c057893898c1fb9633a4c3904655de
soonho <dbstnsgh2@naver.com>
2022-10-13 16:38:06 +0900

    ch04: Predicate 예제 - 2. 반복문에서 Predicate 사용

--- src/main/java/com/example/modernjava/PredicateExamples.java
+++ src/main/java/com/example/modernjava/PredicateExamples.java
@@ -1,5 +1,8 @@
 package com.example.modernjava;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class PredicateExamples {
@@ -13,6 +16,18 @@ public class PredicateExamples {
         System.out.println(isPositive.test(1));
         System.out.println(isPositive.test(0));
         System.out.println(isPositive.test(-1));
+
+        List<Integer> numbers = Arrays.asList(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5);
+        List<Integer> positiveNumbers = new ArrayList<>();
+
+        // 2. 반복문에서 Predicate 사용
+        for (Integer number : numbers) {
+            if (isPositive.test(number)) {
+                positiveNumbers.add(number);
+            }
+        }
+
+        System.out.println(positiveNumbers);
     }
 
 }
