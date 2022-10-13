249db5f7911c292eb86cf18e7c8108bb779898cd
soonho <dbstnsgh2@naver.com>
2022-10-13 16:54:16 +0900

    ch05: Supplier 이유 - 메소드 사용할 경우 조건에 부합할 경우에만 value가 사용

--- src/main/java/com/example/modernjava/SupplierExamples.java
+++ src/main/java/com/example/modernjava/SupplierExamples.java
@@ -10,6 +10,19 @@ public class SupplierExamples {
         System.out.println(helloSupplier.get() + "world");
 
         // 왜 굳이 Supplier 를 사용해야 하는가?
+
+        printIfValid(0, "Hello");
+        printIfValid(1, "Hello");
+        printIfValid(-1, "Hello");
+    }
+
+    private static void printIfValid(int number, String value) {
+        // 조건에 부합할 경우에만 value 가 사용됨.
+        if (number >= 0) {
+            System.out.println("The value is " + value + ".");
+        } else {
+            System.out.println("Invalid");
+        }
     }
 
 }
