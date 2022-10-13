c15373ca9aca56f91092cf459ae35b6eb4eaa9ec
soonho <dbstnsgh2@naver.com>
2022-10-13 16:39:33 +0900

    ch04: Predicate 예제 - 3. stream filter 사용

--- src/main/java/com/example/modernjava/PredicateExamples.java
+++ src/main/java/com/example/modernjava/PredicateExamples.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class PredicateExamples {
 
@@ -28,6 +29,12 @@ public class PredicateExamples {
         }
 
         System.out.println(positiveNumbers);
+
+        // 3. stream, filter 사용
+        List<Integer> numbers2 = Arrays.asList(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5);
+        List<Integer> positiveNumbers2 = numbers2.stream().filter(isPositive).collect(Collectors.toList());
+
+        System.out.println(positiveNumbers2);
     }
 
 }
