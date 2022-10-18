5f5db94fa2871451252c04a44107815e7b6035bf
shyoon <dbstnsgh2@naver.com>
2022-10-18 09:02:26 +0900

    ch07: add examples FunctionalInterface

---
+++ src/main/java/com/example/modernjava/FunctionalInterfaceExamples2.java
@@ -0,0 +1,83 @@
+package com.example.modernjava;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class FunctionalInterfaceExamples2 {
+
+    public static void main(String[] args) {
+        List<Product> products = Arrays.asList(
+                new Product(1L, "A", new BigDecimal("10.00")),
+                new Product(2L, "B", new BigDecimal("55.50")),
+                new Product(3L, "C", new BigDecimal("17.45")),
+                new Product(4L, "D", new BigDecimal("23.00")),
+                new Product(5L, "E", new BigDecimal("110.00")),
+                new Product(6L, "F", new BigDecimal("22.00"))
+        );
+    }
+
+}
+
+class Product {
+
+    private Long id;
+    private String name;
+    private BigDecimal price;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) && Objects.equals(name, product.name) && Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+}
