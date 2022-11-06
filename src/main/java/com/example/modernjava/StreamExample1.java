99064c3b50f9d239d48e51df1d7b38c474dbd373
shyoon <dbstnsgh2@naver.com>
2022-11-07 08:13:51 +0900

    stream - 2

--- src/main/java/com/example/modernjava/StreamExample1.java
+++ src/main/java/com/example/modernjava/StreamExample1.java
@@ -1,11 +1,22 @@
 package com.example.modernjava;
 
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class StreamExample1 {
     public static void main(String[] args) {
-        IntStream.rangeClosed(1, 10).forEach(i -> System.out.print(i + " "));
-        System.out.println();
-        IntStream.iterate(1, i -> i + 1).forEach(i -> System.out.print(i + " "));
+        // Stream 은 일종의 Collection Builder
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> result = new ArrayList<>();
+
+        // 일반적인 순회 / 필터
+        for (Integer number : numbers) {
+            if (number > 3 && number < 9) {
+                result.add(number);
+            }
+        }
+        System.out.println("Imperative Result: " + result);
     }
 }
