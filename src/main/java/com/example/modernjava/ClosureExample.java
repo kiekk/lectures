2fd7b5915b85a576d610806d6bd029d87e2acef2
shyoon <dbstnsgh2@naver.com>
2022-11-08 16:35:46 +0900

    ch09: closure - 2

--- src/main/java/com/example/modernjava/ClosureExample.java
+++ src/main/java/com/example/modernjava/ClosureExample.java
@@ -2,13 +2,36 @@ package com.example.modernjava;
 
 public class ClosureExample {
     public static void main(String[] args) {
-        int number = 100;
+        int number = 100; // Effectively Final = final 처럼 보이지는 않지만 final 로 동작
 
-        Runnable runnable = () -> System.out.println(number);
+        System.out.println("Anonymous Class");
+        testClosure("Anonymous Class", new Runnable() {
+            @Override
+            public void run() {
+                // number = 200;
+                // Variable 'number' is accessed from within inner class, needs to be final or effectively final
+                System.out.println(number);
+            }
+        });
 
-        runnable.run(); // 100
+        // number = 200;
+        // Variable used in lambda expression should be final or effectively final
+        testClosure("Lambda Expression", () -> System.out.println(number));
 
-        // 람다식 에서 외부 변수를 참조하는 것 = 클로저 (Closure)
-        // scope 확장, 람다 scope 를 외부 scope 까지로 확장
+        /*
+            Anonymous Class vs Lambda Expression
+
+           java 7 까지는 외부 변수가 final 이 아닐 경우 error 발생
+           java 8 부터는 final 이 아니어도 접근 가능
+
+           대신 외부 scope 의 변수를 read (읽기) 는 가능,
+           외부 scope 의 변수를 update (조작) 하는 것은 불가능
+         */
+    }
+
+    private static void testClosure(String name, Runnable runnable) {
+        System.out.println("Start " + name + ": ");
+        runnable.run();
+        System.out.println("==========================");
     }
 }
