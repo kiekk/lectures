180bb4c7b336b6eb6215d13caa426eef299992e5
shyoon <dbstnsgh2@naver.com>
2022-11-11 08:26:12 +0900

    ch11: Method Reference - 2

--- src/main/java/com/example/modernjava/MethodReferenceExample.java
+++ src/main/java/com/example/modernjava/MethodReferenceExample.java
@@ -1,10 +1,26 @@
 package com.example.modernjava;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MethodReferenceExample {
     public static void main(String[] args) {
         Arrays.asList(1, 2, 3, 4, 5)
                 .forEach(System.out::println);
+
+        // sorting
+        System.out.println(Stream.of(new BigDecimal("10.0"), new BigDecimal("23"), new BigDecimal("5"))
+                .sorted(BigDecimal::compareTo)
+//                .sorted(BigDecimalUtil::compare)
+                .collect(Collectors.toList()));
     }
 }
+
+class BigDecimalUtil {
+    public static int compare(BigDecimal bd1, BigDecimal bd2) {
+        return bd1.compareTo(bd2);
+    }
+}
+
