a4bc7afba6369a3d1ccc9102ad3bbbe4bf1e4301
shyoon <dbstnsgh2@naver.com>
2022-11-11 08:44:54 +0900

    ch11: Method Reference - 4

--- src/main/java/com/example/modernjava/MethodReferenceExample.java
+++ src/main/java/com/example/modernjava/MethodReferenceExample.java
@@ -2,6 +2,7 @@ package com.example.modernjava;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -21,6 +22,41 @@ public class MethodReferenceExample {
         System.out.println(Stream.of("a", "b", "c", "d")
                 .anyMatch("c"::equals));
 //                .anyMatch(s -> s.equals("c")));
+
+        methodReference3();
+    }
+
+    private static void methodReference3() {
+        // using lambda expression
+        System.out.println(testFirstClassFunction1(3, integer -> String.valueOf(integer * 2)));
+
+        // using method reference
+        System.out.println(testFirstClassFunction1(3, MethodReferenceExample::doubleThenToString));
+
+        // using lambda expression in function
+        Function<Integer, String> fl = getDoubleThenToStringUsingLambdaExpression();
+        String resultFromFl = fl.apply(3);
+        System.out.println(resultFromFl);
+
+        Function<Integer, String> fmr = getDoubleThenToStringUsingMethodReference();
+        String resultFromFmr = fmr.apply(3);
+        System.out.println(resultFromFmr);
+    }
+
+    private static String testFirstClassFunction1(int n, Function<Integer, String> f) {
+        return "The result is " + f.apply(n);
+    }
+
+    private static String doubleThenToString(int n) {
+        return String.valueOf(n * 2);
+    }
+
+    private static Function<Integer, String> getDoubleThenToStringUsingLambdaExpression() {
+        return i -> String.valueOf(i * 2);
+    }
+
+    private static Function<Integer, String> getDoubleThenToStringUsingMethodReference() {
+        return MethodReferenceExample::doubleThenToString;
     }
 }
 
