af1c48c22a5070241fb275a8e49f3301ad3acb85
soonho <dbstnsgh2@naver.com>
2022-11-15 21:01:21 +0900

    Initial Commit

---
+++ src/main/java/com/example/studyhtmx/StudyHtmxApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyhtmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyHtmxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyHtmxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyhtmx/StudyHtmxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyhtmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyHtmxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
