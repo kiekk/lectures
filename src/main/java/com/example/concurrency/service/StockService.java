14dee77702ceb75b21d263f251c9486bda95be30
soonho <dbstnsgh2@naver.com>
2022-11-26 20:26:11 +0900

    동시성 문제 해결 - 1 (synchronized 사용)

--- src/main/java/com/example/concurrency/service/StockService.java
+++ src/main/java/com/example/concurrency/service/StockService.java
@@ -4,6 +4,8 @@ import com.example.concurrency.domain.Stock;
 import com.example.concurrency.repository.StockRepository;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 public class StockService {
 
@@ -13,7 +15,12 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    public void decrease(Long id, Long quantity) {
+    /*
+    synchronized 키워드를 사용하여 동시성 문제 해결
+    @Transactional 이 선언되어 있을 경우에는 synchronized 를 사용해도 에러 발생
+     */
+//    @Transactional
+    public synchronized void decrease(Long id, Long quantity) {
         /*
         1. Stock 가져오기
         2. 재고 감소
