4a64b2ae748b52778efabb284f068f5371d375c3
soonho <dbstnsgh2@naver.com>
2022-11-26 21:11:55 +0900

    동시성 문제 해결 - 4 (DB - Named Lock 사용)

--- src/main/java/com/example/concurrency/service/StockService.java
+++ src/main/java/com/example/concurrency/service/StockService.java
@@ -3,8 +3,8 @@ package com.example.concurrency.service;
 import com.example.concurrency.domain.Stock;
 import com.example.concurrency.repository.StockRepository;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StockService {
@@ -15,18 +15,8 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    /*
-    synchronized 키워드를 사용하여 동시성 문제 해결
-    @Transactional 이 선언되어 있을 경우에는 synchronized 를 사용해도 에러 발생
-     */
-//    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public synchronized void decrease(Long id, Long quantity) {
-        /*
-        1. Stock 가져오기
-        2. 재고 감소
-        3. 저장
-         */
-
         Stock stock = stockRepository.findById(id).orElseThrow();
 
         stock.decrease(quantity);
