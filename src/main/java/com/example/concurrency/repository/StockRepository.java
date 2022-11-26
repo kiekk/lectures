693fedc0d347b26c18aec06558fc548b9fa73be7
soonho <dbstnsgh2@naver.com>
2022-11-26 20:54:41 +0900

    동시성 문제 해결 - 2 (DB - Pessimistic Lock 사용)

--- src/main/java/com/example/concurrency/repository/StockRepository.java
+++ src/main/java/com/example/concurrency/repository/StockRepository.java
@@ -2,6 +2,14 @@ package com.example.concurrency.repository;
 
 import com.example.concurrency.domain.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.persistence.LockModeType;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("select s from Stock s where s.id = :id")
+    Stock findByIdWithPessimisticLock(@Param("id") Long id);
 }
