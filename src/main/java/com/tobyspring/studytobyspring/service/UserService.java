c22cdbce54c2c5a18549c94023a13dcdd58687ce
soonho <dbstnsgh2@naver.com>
2022-11-27 11:34:51 +0900

    ch05: 사용자 레벨 업그레이드 기능 추가 및 테스트

--- src/main/java/com/tobyspring/studytobyspring/service/UserService.java
+++ src/main/java/com/tobyspring/studytobyspring/service/UserService.java
@@ -1,7 +1,11 @@
 package com.tobyspring.studytobyspring.service;
 
 import com.tobyspring.studytobyspring.dao.UserDao;
+import com.tobyspring.studytobyspring.domain.User;
+import com.tobyspring.studytobyspring.enums.Level;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class UserService {
@@ -10,5 +14,28 @@ public class UserService {
 
     public UserService(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    public void upgradeLevels() {
+        List<User> users = userDao.getAll();
+
+        for (User user : users) {
+            boolean changed;
+            if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
+                user.setLevel(Level.SILVER);
+                changed = true;
+            } else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
+                user.setLevel(Level.GOLD);
+                changed = true;
+            } else if (user.getLevel() == Level.GOLD) {
+                changed = false;
+            } else {
+                changed = false;
+            }
+
+            if (changed) {
+                userDao.update(user);
+            }
+        }
     }
 }
--- src/test/java/com/tobyspring/studytobyspring/service/UserServiceTest.java
+++ src/test/java/com/tobyspring/studytobyspring/service/UserServiceTest.java
@@ -1,11 +1,19 @@
 package com.tobyspring.studytobyspring.service;
 
+import com.tobyspring.studytobyspring.dao.UserDao;
+import com.tobyspring.studytobyspring.domain.User;
+import com.tobyspring.studytobyspring.enums.Level;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application.yml")
@@ -13,8 +21,46 @@ class UserServiceTest {
     @Autowired
     UserService userService;
 
+    @Autowired
+    UserDao userDao;
+
+    List<User> users;
+
     @Test
     public void bean() {
         assertNotNull(this.userService);
+    }
+
+    @BeforeEach
+    public void setUp() {
+        users = Arrays.asList(
+                new User("1", "soonho", "1234", Level.BASIC, 49, 0),
+                new User("2", "sooni", "1234", Level.BASIC, 50, 10),
+                new User("3", "zooni", "1234", Level.SILVER, 60, 29),
+                new User("4", "오민규", "1234", Level.SILVER, 60, 30),
+                new User("5", "박범진", "1234", Level.GOLD, 100, 100)
+        );
+    }
+
+    @Test
+    public void upgradeLevels() {
+        userDao.deleteAll();
+
+        for (User user : users) {
+            userDao.add(user);
+        }
+
+        userService.upgradeLevels();
+
+        checkLevel(users.get(0), Level.BASIC);
+        checkLevel(users.get(1), Level.SILVER);
+        checkLevel(users.get(2), Level.SILVER);
+        checkLevel(users.get(3), Level.GOLD);
+        checkLevel(users.get(4), Level.GOLD);
+    }
+
+    private void checkLevel(User user, Level expectedLevel) {
+        User userUpdate = userDao.get(user.getId());
+        assertEquals(userUpdate.getLevel(), expectedLevel);
     }
 }
