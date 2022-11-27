a680858207fee51bf9a2b469cbce460782d4f8a8
soonho <dbstnsgh2@naver.com>
2022-11-27 10:20:13 +0900

    ch05: 로그인 횟수/추천수 필드 추가

--- src/main/java/com/tobyspring/studytobyspring/domain/User.java
+++ src/main/java/com/tobyspring/studytobyspring/domain/User.java
@@ -15,13 +15,7 @@ public class User {
     private String password;
 
     private Level level;
-    /*
-    상수로 관리하는 것 보다 Enum 으로 관리하는 것이 좋다.
-
-    이제 의도하지 않은 다른 값을 level 에 넣을 경우 컴파일 단계에서 에러가 발생한다.
-    user.setLevel(1000);
-    ->
-    user.setLevel(Level.BASIC);
-     */
+    private int login; // 로그인 횟수
+    private int recommend; // 추천수
 
 }
