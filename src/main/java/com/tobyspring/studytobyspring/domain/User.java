bfc0a78446bd001eaf124e5b0a92e968fd3feba6
soonho <dbstnsgh2@naver.com>
2022-11-27 10:19:28 +0900

    ch05: 사용자 레벨 관리 기능 추가 - 2 (Enum 으로 관리)

--- src/main/java/com/tobyspring/studytobyspring/domain/User.java
+++ src/main/java/com/tobyspring/studytobyspring/domain/User.java
@@ -1,5 +1,6 @@
 package com.tobyspring.studytobyspring.domain;
 
+import com.tobyspring.studytobyspring.enums.Level;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,19 +14,14 @@ public class User {
     private String name;
     private String password;
 
-    private static final int BASIC = 1;
-    private static final int SILVER = 2;
-    private static final int GOLD = 3;
-
-    private int level;
+    private Level level;
     /*
+    상수로 관리하는 것 보다 Enum 으로 관리하는 것이 좋다.
 
-    사용자 레벨 관리 기능 추가로 level 필드를 추가했다.
-    level 필드는 단순히 int 형이기 때문에 의도하지 않은 다른 값을 넣어도 에러가 발생하지 않는다.
-
-    설계 의도는 BASIC, SILVER, GOLD 세 레벨만 관리하려고 했는데, 1000을 넣어도 에러가 발생하지 않는다.
+    이제 의도하지 않은 다른 값을 level 에 넣을 경우 컴파일 단계에서 에러가 발생한다.
     user.setLevel(1000);
-
+    ->
+    user.setLevel(Level.BASIC);
      */
 
 }
