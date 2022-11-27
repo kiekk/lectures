10c0bed1e5c0f84282a54ea93c2f11e50bddab77
soonho <dbstnsgh2@naver.com>
2022-11-27 12:27:05 +0900

    ch05: upgradeLevel 테스트

---
+++ src/test/java/com/tobyspring/studytobyspring/UserTest.java
@@ -0,0 +1,50 @@
+package com.tobyspring.studytobyspring;
+
+import com.tobyspring.studytobyspring.domain.User;
+import com.tobyspring.studytobyspring.enums.Level;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application.yml")
+class UserTest {
+
+    User user;
+
+    @BeforeEach
+    public void setUp() {
+        user = new User();
+    }
+
+    @Test
+    public void upgradeLevel() {
+        Level[] levels = Level.values();
+        for (Level level : levels) {
+            if (level.nextLevel() == null) {
+                continue;
+            }
+            user.setLevel(level);
+            user.upgradeLevel();
+            assertThat(user.getLevel()).isEqualTo(level.nextLevel());
+        }
+    }
+
+    @Test
+    public void cannotUpgradeLevel() {
+        Level[] levels = Level.values();
+        for (Level level : levels) {
+            if (level.nextLevel() != null) {
+                continue;
+            }
+            user.setLevel(level);
+            assertThrows(IllegalArgumentException.class, () -> {
+                user.upgradeLevel();
+            });
+        }
+    }
+}
