561c8864858069b3191e6f39c770c8f417e1bd5d
shyoon <dbstnsgh2@naver.com>
2022-12-01 17:14:51 +0900

    iot: add entity iot

---
+++ src/main/java/com/example/studyhtmx/entity/IotDevice.java
@@ -0,0 +1,13 @@
+package com.example.studyhtmx.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class IotDevice {
+    private Long id;
+    private String name;
+}
