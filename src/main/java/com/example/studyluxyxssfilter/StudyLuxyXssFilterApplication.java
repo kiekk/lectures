af402964ab5cac960840b350d16f7eb08354a9d6
soonho <dbstnsgh2@naver.com>
2022-12-14 21:11:53 +0900

    Initial Commit

---
+++ src/main/java/com/example/studyluxyxssfilter/StudyLuxyXssFilterApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyluxyxssfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyLuxyXssFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyLuxyXssFilterApplication.class, args);
+    }
+
+}
