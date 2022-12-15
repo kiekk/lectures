397aa06c62261ecdce49b0fbb235a5431017c676
soonho <dbstnsgh2@naver.com>
2022-12-15 21:56:57 +0900

    add @Entity to Board

--- src/main/java/com/example/studyluxyxssfilter/entity/Board.java
+++ src/main/java/com/example/studyluxyxssfilter/entity/Board.java
@@ -1,14 +1,23 @@
 package com.example.studyluxyxssfilter.entity;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@Entity
 public class Board {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+
     private String id;
     private String title;
     private String contents;
