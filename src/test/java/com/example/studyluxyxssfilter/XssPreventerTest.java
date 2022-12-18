3068f991a55de0374132aeef278bc88c98dda44e
soonho <dbstnsgh2@naver.com>
2022-12-18 17:59:10 +0900

    edit class name

--- src/test/java/com/example/studyluxyxssfilter/XssPreventerTest.java
+++ src/test/java/com/example/studyluxyxssfilter/XssPreventerTest.java
@@ -1,6 +1,8 @@
 package com.example.studyluxyxssfilter;
 
+import com.nhncorp.lucy.security.xss.LucyXssFilter;
 import com.nhncorp.lucy.security.xss.XssPreventer;
+import com.nhncorp.lucy.security.xss.XssSaxFilter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -10,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
-public class LucyXssTest {
+public class XssPreventerTest {
 
     /*
     XssPreventer 객체의 escape 메소드를 사용하여 XSS 방어
@@ -24,4 +26,5 @@ public class LucyXssTest {
         System.out.println("clean : " + clean);
         assertEquals("&lt;img src=&quot;javascript:alert(&quot;test&quot;);&quot;&gt;&lt;/img&gt;", clean);
     }
+
 }
