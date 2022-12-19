e06505f4b1907562c727399e3b64780286281fa1
shyoon <dbstnsgh2@naver.com>
2022-12-19 16:13:46 +0900

    board: XssFilter 적용 (DOM Parser)

--- src/main/java/com/example/studyluxyxssfilter/controller/api/board/BoardFormDataController.java
+++ src/main/java/com/example/studyluxyxssfilter/controller/api/board/BoardFormDataController.java
@@ -2,6 +2,7 @@ package com.example.studyluxyxssfilter.controller.api.board;
 
 import com.example.studyluxyxssfilter.entity.Board;
 import com.example.studyluxyxssfilter.service.BoardService;
+import com.example.studyluxyxssfilter.util.XssFilterUtil;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,12 +17,14 @@ public class BoardFormDataController {
 
     @PostMapping("")
     public void insert(Board board) {
+        board.setContents(XssFilterUtil.xssEscape(board.getContents()));
         boardService.save(board);
     }
 
     @PutMapping("{id}")
     public void update(@PathVariable String id, Board board) {
         board.setId(id);
+        board.setContents(XssFilterUtil.xssEscape(board.getContents()));
         boardService.update(board);
     }
 
