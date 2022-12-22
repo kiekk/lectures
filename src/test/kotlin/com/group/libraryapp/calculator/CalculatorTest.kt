2c0981119e1d67d91b89fa65d429c22f676ef202
shyoon <shyoon991@gmail.com>
2022-12-22 09:58:44 +0900

    kotlin test 학습 - Calculator 테스트 4. minus, multiply, divide 테스트

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -3,6 +3,10 @@ package com.group.libraryapp.calculator
 fun main() {
     val calculatorTest = CalculatorTest()
     calculatorTest.addTest();
+    calculatorTest.minusTest();
+    calculatorTest.multiplyTest();
+    calculatorTest.divideTestGivenZero();
+    calculatorTest.divideTestGivenNotZero()
 }
 
 class CalculatorTest {
@@ -16,6 +20,68 @@ class CalculatorTest {
 
         // then
         if (calculator.number != 8) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun minusTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.minus(4)
+
+        // then
+        if (calculator.number != 1) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun multiplyTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.multiply(3)
+
+        // then
+        if (calculator.number != 15) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun divideTestGivenZero() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        // then
+        try {
+            calculator.divide(0)
+        } catch (e: IllegalArgumentException) {
+            // 테스트 실패
+            if (e.message != "0으로 나눌 수 없습니다.") {
+                throw IllegalStateException()
+            }
+            // 테스트 성공
+            return
+        } catch (e: Exception) {
+            // 테스트 실패
+            throw IllegalStateException()
+        }
+        // 테스트 실패
+        throw IllegalStateException()
+    }
+
+    fun divideTestGivenNotZero() {
+        // given
+        val calculator = Calculator(10)
+
+        // when
+        calculator.divide(2)
+
+        // then
+        if (calculator.number != 5) {
             throw IllegalStateException()
         }
     }
