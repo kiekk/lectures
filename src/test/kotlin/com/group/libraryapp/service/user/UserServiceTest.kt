652af60702aeb8bd486baa4a9088230c1baef4b6
shyoon <shyoon991@gmail.com>
2022-12-22 10:56:53 +0900

    UserService 테스트 - 5. @DisplayName 추가

--- src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest
 import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -23,6 +24,7 @@ class UserServiceTest @Autowired constructor(
     }
 
     @Test
+    @DisplayName("유저 저장이 정상적으로 동작한다.")
     fun saveUserTest() {
         // given
         val request = UserCreateRequest("soono", null)
@@ -38,6 +40,7 @@ class UserServiceTest @Autowired constructor(
     }
 
     @Test
+    @DisplayName("유저 조회가 정상적으로 동작한다.")
     fun getUsersTest() {
         // given
         userRepository.saveAll(
@@ -62,6 +65,7 @@ class UserServiceTest @Autowired constructor(
     }
 
     @Test
+    @DisplayName("유저 수정이 정상적으로 동작한다.")
     fun updateUserNameTest() {
         // given
         val savedUser = userRepository.save(User("A", null))
@@ -76,6 +80,7 @@ class UserServiceTest @Autowired constructor(
     }
 
     @Test
+    @DisplayName("유저 삭제가 정상적으로 동작한다.")
     fun deleteUserTest() {
         // given
         userRepository.save(User("A", null))
