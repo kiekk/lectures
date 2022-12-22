fa5d9c87a542c8899b1fe3fbdd0f9a353c8423ea
shyoon <shyoon991@gmail.com>
2022-12-22 14:40:50 +0900

    refactoring-kotlin: 중복 코드 제거

--- src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -5,6 +5,7 @@ import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
 import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import com.group.libraryapp.dto.user.response.UserResponse
+import com.group.libraryapp.util.fail
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -34,7 +35,7 @@ class UserService(
 
     @Transactional
     fun deleteUser(name: String) {
-        val user = userRepository.findByName(name) ?: throw IllegalArgumentException()
+        val user = userRepository.findByName(name) ?: fail()
         userRepository.delete(user)
     }
 
