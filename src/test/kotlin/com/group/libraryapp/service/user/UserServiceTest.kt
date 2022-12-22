7dcf0cbca9a36d6bbca49a823369151301ef4f0a
shyoon <shyoon991@gmail.com>
2022-12-22 10:49:40 +0900

    UserService 테스트 - 2. getUsers

--- src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -1,5 +1,6 @@
 package com.group.libraryapp.service.user
 
+import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
@@ -13,6 +14,12 @@ class UserServiceTest @Autowired constructor(
     private val userService: UserService
 ) {
 
+    /*
+    현재 getUsersTest 는 실패한다.
+    saveUserTest 에서 이미 User 하나를 추가했기 때문에
+    getUsersTest 에서 추가한 2명이 아닌 총 3명이 조회되어 테스트 실패
+    각 테스트별로 테스트 종료 후 데이터를 초기화 해줄 필요가 있다.
+     */
     @Test
     fun saveUserTest() {
         // given
@@ -26,6 +33,30 @@ class UserServiceTest @Autowired constructor(
         assertThat(results).hasSize(1)
         assertThat(results[0].name).isEqualTo("soono")
         assertThat(results[0].age).isNull()
+    }
+
+    @Test
+    fun getUsersTest() {
+        // given
+        userRepository.saveAll(
+            listOf(
+                User("A", 20),
+                User("B", null)
+            )
+        )
+
+        // when
+        val results = userService.getUsers()
+
+        // then
+        assertThat(results).hasSize(2) // [UserResponse(), UserResponse()
+        assertThat(results)
+            .extracting("name") // ["A", "B"]
+            .containsExactlyInAnyOrder("A", "B")
+
+        assertThat(results)
+            .extracting("age") // [20, null]
+            .containsExactlyInAnyOrder(20, null)
     }
 
 }
