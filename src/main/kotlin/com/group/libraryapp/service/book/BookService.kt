0a65bb3e3f8fdabfa052dbe96f6561aa3de5403f
soonho <shyoon991@gmail.com>
2022-12-24 11:03:20 +0900

    querydsl: 대출 현황 통계 querydsl 로 변경

--- src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -9,15 +9,17 @@ import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
 import com.group.libraryapp.dto.book.response.BookStatResponse
 import com.group.libraryapp.enums.user.UserLoanStatus
+import com.group.libraryapp.respository.book.BookQuerydslRepository
 import com.group.libraryapp.util.fail
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class BookService(
-    private val bookRepository: BookRepository,
-    private val userRepository: UserRepository,
-    private val userLoanHistoryRepository: UserLoanHistoryRepository,
+        private val bookRepository: BookRepository,
+        private val bookQuerydslRepository: BookQuerydslRepository,
+        private val userRepository: UserRepository,
+        private val userLoanHistoryRepository: UserLoanHistoryRepository,
 ) {
 
     @Transactional
@@ -51,7 +53,7 @@ class BookService(
 
     @Transactional(readOnly = true)
     fun getBookStatistics(): List<BookStatResponse> {
-        return bookRepository.getStats()
+        return bookQuerydslRepository.getStats()
     }
 
 }
