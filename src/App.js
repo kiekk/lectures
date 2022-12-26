8fac1b35ee86c45ef7ae735f41dca9fdd9d711ba
soonho <shyoon991@gmail.com>
2022-12-26 20:01:14 +0900

    react-router: add component BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <h2>App.js</h2>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <h2>App.js</h2>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
