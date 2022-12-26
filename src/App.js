ccb8dd2f12b0e7a9aaa357e854724831d55a0005
shyoon <shyoon991@gmail.com>
2022-12-27 08:12:34 +0900

    project-setting: remove unused code

--- src/App.js
+++ src/App.js
@@ -5,23 +5,18 @@ import Home from "./pages/Home";
 import Edit from "./pages/Edit";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
-import RouterTest from "./components/RouterTest";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
                 <h2>App.js</h2>
-
-
                 <Routes>
                     <Route path='/' element={<Home/>}></Route>
                     <Route path='/edit' element={<Edit/>}></Route>
                     <Route path='/new' element={<New/>}></Route>
                     <Route path='/diary' element={<Diary/>}></Route>
                 </Routes>
-
-                <RouterTest/>
             </div>
         </BrowserRouter>
     );
