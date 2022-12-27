3384c0cf3890afbafaed0c914a6e0059ea949865
shyoon <shyoon991@gmail.com>
2022-12-28 08:27:02 +0900

    home-page: add select - sort list

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,6 +1,39 @@
+import {useState} from "react";
+
+const sortOptionList = [
+    {value: 'latest', name: '최신 순'},
+    {value: 'oldest', name: '오래된 순'},
+]
+
+const ControlMenu = ({value, onChange, optionList}) => {
+    return <select value={value} onChange={(e) => onChange(e.target.value)}>
+        {optionList.map((it, idx) => <option key={idx} value={it.value}>{it.name}</option>)}
+    </select>
+}
+
 const DiaryList = ({diaryList}) => {
+
+    const [sortType, setSortType] = useState('latest')
+
+    const getProcessDiaryList = () => {
+        const compare = (a, b) => {
+            if (sortType === 'latest') {
+                return parseInt(b.date) - parseInt(a.date)
+            } else {
+                return parseInt(a.date) - parseInt(b.date)
+            }
+        }
+        const copyList = JSON.parse(JSON.stringify(diaryList))
+        return copyList.sort(compare)
+    }
+
     return <div>
-        {diaryList.map((it) => (
+        <ControlMenu
+            value={sortType}
+            onChange={setSortType}
+            optionList={sortOptionList}
+        />
+        {getProcessDiaryList().map((it) => (
             <div key={it.id}>{it.content}</div>
         ))}
     </div>
