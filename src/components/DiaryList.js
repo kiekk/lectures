79d6bc03b83744e3b34186bfa1bd50ce222c1387
shyoon <shyoon991@gmail.com>
2022-12-28 08:32:50 +0900

    home-page: add select - filter emotion list

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,5 +1,11 @@
 import {useState} from "react";
 
+const filterOptionList = [
+    {value: 'all', name: '전부 다'},
+    {value: 'good', name: '좋은 감정만'},
+    {value: 'bad', name: '안좋은 감정만'},
+]
+
 const sortOptionList = [
     {value: 'latest', name: '최신 순'},
     {value: 'oldest', name: '오래된 순'},
@@ -14,8 +20,16 @@ const ControlMenu = ({value, onChange, optionList}) => {
 const DiaryList = ({diaryList}) => {
 
     const [sortType, setSortType] = useState('latest')
+    const [filter, setFilter] = useState('all')
 
     const getProcessDiaryList = () => {
+        const filterCallBack = (item) => {
+            if (filter === 'good') {
+                return parseInt(item.emotion) <= 3
+            } else {
+                return parseInt(item.emotion) > 3
+            }
+        }
         const compare = (a, b) => {
             if (sortType === 'latest') {
                 return parseInt(b.date) - parseInt(a.date)
@@ -24,7 +38,8 @@ const DiaryList = ({diaryList}) => {
             }
         }
         const copyList = JSON.parse(JSON.stringify(diaryList))
-        return copyList.sort(compare)
+        const filterdList = filter === 'all' ? copyList : copyList.filter(filterCallBack)
+        return filterdList.sort(compare)
     }
 
     return <div>
@@ -33,8 +48,13 @@ const DiaryList = ({diaryList}) => {
             onChange={setSortType}
             optionList={sortOptionList}
         />
+        <ControlMenu
+            value={filter}
+            onChange={setFilter}
+            optionList={filterOptionList}
+        />
         {getProcessDiaryList().map((it) => (
-            <div key={it.id}>{it.content}</div>
+            <div key={it.id}>{it.content} {it.emotion}</div>
         ))}
     </div>
 }
