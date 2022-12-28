aa96331bf4ec40ab5da6b6f1d1cd751565578ffb
soonho <shyoon991@gmail.com>
2022-12-28 21:16:59 +0900

    diary-new: add css DiaryEditor

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -11,15 +11,16 @@ const DiaryEditor = () => {
   const navigate = useNavigate()
   const [date, setDate] = useState(getStringDate(new Date()))
 
-  return <div>
+  return <div className='DiaryEditor'>
     <MyHeader
       headText={'새 일기쓰기'}
       leftChild={<MyButton text={'< 뒤로가기'} onClick={() => navigate(-1)}/>}
     />
     <section>
       <h4>오늘은 언제인가요?</h4>
-      <div className='input-box'>
+      <div className='input_box'>
         <input
+          className='input_date'
           type='date'
           value={date}
           onChange={(e) => setDate(e.target.value)}
