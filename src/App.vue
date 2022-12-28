bc6014e05c75de4d7c6bb5f2f8c3bcf774255a37
shyoon <shyoon991@gmail.com>
2022-12-28 15:32:24 +0900

    add header & router link

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// import {RouterLink, RouterView} from "vue-router";
+</script>
 
 <template>
   <div class="min-h-screen bg-gray-100 container mx-auto">
-    <h1>Hello, World!</h1>
+    <header class="p-8">
+      <nav>
+        <RouterLink to="/">Dashboard</RouterLink>
+        <a href="#" class="ml-4 text-indigo-400">Logout</a>
+      </nav>
+    </header>
+    <main class="p-8">
+      <RouterView/>
+    </main>
   </div>
 </template>
-
