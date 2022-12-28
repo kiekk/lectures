d33a14cebcdfb9f2259d7c7e0ab0c3a787973435
soonho <shyoon991@gmail.com>
2022-12-28 21:09:54 +0900

    diary-new: add input

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,9 +1,15 @@
 import MyHeader from "../components/MyHeader";
 import MyButton from "../components/MyButton";
 import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10)
+}
 
 const New = () => {
   const navigate = useNavigate()
+  const [date, setDate] = useState(getStringDate(new Date()))
 
   return (
     <div>
@@ -11,6 +17,18 @@ const New = () => {
         headText={'새 일기쓰기'}
         leftChild={<MyButton text={'< 뒤로가기'} onClick={() => navigate(-1)}/>}
       />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className='input-box'>
+            <input
+              type='date'
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
