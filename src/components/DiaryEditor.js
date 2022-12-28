df3172207c2befbd200b44175bb95491ab2aec97
shyoon <shyoon991@gmail.com>
2022-12-29 08:23:26 +0900

    diary-edit: implement function diary edit

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,4 +1,4 @@
-import {useContext, useRef, useState} from "react";
+import {useContext, useEffect, useRef, useState} from "react";
 import MyHeader from "./MyHeader";
 import MyButton from "./MyButton";
 import {useNavigate} from "react-router-dom";
@@ -40,14 +40,14 @@ const getStringDate = (date) => {
   return date.toISOString().slice(0, 10)
 }
 
-const DiaryEditor = () => {
+const DiaryEditor = ({isEdit, originData}) => {
   const navigate = useNavigate()
   const [date, setDate] = useState(getStringDate(new Date()))
   const [emotion, setEmotion] = useState(3)
   const contentRef = useRef()
   const [content, setContent] = useState('')
 
-  const {onCreate} = useContext(DiaryDispatchContext)
+  const {onCreate, onEdit} = useContext(DiaryDispatchContext)
 
   const handleClickEmote = (emotion) => {
     setEmotion(emotion)
@@ -58,13 +58,28 @@ const DiaryEditor = () => {
       contentRef.current.focus()
       return
     }
-    onCreate(date, content, emotion)
-    navigate('/', {replace: true})
+
+    if (window.confirm(isEdit ? '일기를 수정하시겠습니까?' : '새로운 일기를 작성하시겠습니까?')) {
+      if (!isEdit) {
+        onCreate(date, content, emotion)
+      } else {
+        onEdit(originData.id, date, content, emotion)
+      }
+      navigate('/', {replace: true})
+    }
   }
+
+  useEffect(() => {
+    if (isEdit) {
+      setDate(getStringDate(new Date(parseInt(originData.date))))
+      setEmotion(originData.emotion)
+      setContent(originData.content)
+    }
+  }, [isEdit, originData])
 
   return <div className='DiaryEditor'>
     <MyHeader
-      headText={'새 일기쓰기'}
+      headText={isEdit ? '일기 수정하기' : '새 일기쓰기'}
       leftChild={<MyButton text={'< 뒤로가기'} onClick={() => navigate(-1)}/>}
     />
     <section>
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,10 +1,31 @@
+import {useNavigate, useParams} from "react-router-dom";
+import {useContext, useEffect, useState} from "react";
+import {DiaryStateContext} from "../App";
+import DiaryEditor from "../components/DiaryEditor";
+
 const Edit = () => {
-    return (
-        <div>
-            <h1>Edit</h1>
-            <p>이곳은 수정페이지 입니다.</p>
-        </div>
-    )
+  const navigate = useNavigate()
+  const {id} = useParams()
+  const diaryList = useContext(DiaryStateContext)
+  const [originData, setOriginData] = useState()
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find((it) => it.id === parseInt(id))
+
+      if (targetDiary) {
+        setOriginData(targetDiary)
+      } else {
+        navigate('/', {replace: true})
+      }
+    }
+  }, [id, diaryList])
+
+  return (
+    <div>
+      {originData && <DiaryEditor isEdit={true} originData={originData}/>}
+    </div>
+  )
 }
 
 export default Edit;
