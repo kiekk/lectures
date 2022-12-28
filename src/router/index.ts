50aea96ee840bbfe34ad88f8bb029556a1d52d46
shyoon <shyoon991@gmail.com>
2022-12-28 15:33:45 +0900

    add routes to '/' & '/login'

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
+import DashboardView from "@/views/DashboardView.vue";
+import LoginView from "@/views/LoginView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'Dashboard',
+      component: DashboardView
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: LoginView
+    }
+  ]
+})
+
+router.beforeEach(async (to) => {
+  const publicPages = ['/login']
+  const authRequired = !publicPages.includes(to.path)
+
+  // if auth is required and the user is not logged in
+  if (authRequired) {
+    return '/login'
+  }
 })
 
 export default router
