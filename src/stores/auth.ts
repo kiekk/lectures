161e68c84f580688129ee36f7a95a2e047cb692e
shyoon <shyoon991@gmail.com>
2022-12-28 15:41:24 +0900

    add store - auth

---
+++ src/stores/auth.ts
@@ -0,0 +1,26 @@
+import {defineStore} from "pinia";
+
+export const useAuthStore = defineStore({
+  id: 'auth',
+  state: () => {
+    return {
+      user: null,
+      token: null,
+      returnUrl: '/',
+    }
+  },
+  actions: {
+    async login(username: string, password: string) {
+      const response = await fetch('http://localhost:8080/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username, password})
+      })
+
+      console.log(response.status)
+      console.log(response.text())
+    }
+  }
+})
