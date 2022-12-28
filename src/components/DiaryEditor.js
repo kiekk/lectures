e3591c27e911e9063ea4e97312c065c385d04553
shyoon <shyoon991@gmail.com>
2022-12-29 08:37:07 +0900

    diary-detail: add util emotionList

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -5,37 +5,7 @@ import {useNavigate} from "react-router-dom";
 import EmotionItem from "./EmotionItem";
 import {DiaryDispatchContext} from "../App";
 import {getStringDate} from "../util/date";
-
-const env = process.env
-env.PUBLIC_URL = env.PUBLIC_URL || ''
-
-const emotionList = [
-  {
-    emotion_id: 1,
-    emotion_img: env.PUBLIC_URL + `/assets/emotion1.png`,
-    emotion_descript: '완전 좋음',
-  },
-  {
-    emotion_id: 2,
-    emotion_img: env.PUBLIC_URL + `/assets/emotion2.png`,
-    emotion_descript: '좋음',
-  },
-  {
-    emotion_id: 3,
-    emotion_img: env.PUBLIC_URL + `/assets/emotion3.png`,
-    emotion_descript: '그럭저럭',
-  },
-  {
-    emotion_id: 4,
-    emotion_img: env.PUBLIC_URL + `/assets/emotion4.png`,
-    emotion_descript: '나쁨',
-  },
-  {
-    emotion_id: 5,
-    emotion_img: env.PUBLIC_URL + `/assets/emotion5.png`,
-    emotion_descript: '끔찍함',
-  },
-]
+import {emotionList} from "../util/emotion";
 
 const DiaryEditor = ({isEdit, originData}) => {
   const navigate = useNavigate()
