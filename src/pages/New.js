478928415a92c227b4c778026de0b02a64dfa53a
soonho <shyoon991@gmail.com>
2022-12-28 21:03:12 +0900

    diary-new: add header

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,10 +1,18 @@
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+import {useNavigate} from "react-router-dom";
+
 const New = () => {
-    return (
-        <div>
-            <h1>New</h1>
-            <p>이곳은 작성페이지 입니다.</p>
-        </div>
-    )
+  const navigate = useNavigate()
+
+  return (
+    <div>
+      <MyHeader
+        headText={'새 일기쓰기'}
+        leftChild={<MyButton text={'< 뒤로가기'} onClick={() => navigate(-1)}/>}
+      />
+    </div>
+  )
 }
 
 export default New;
