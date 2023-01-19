ad5d9dfd89a2e07a70e168390adcac718f724cca
shyoon <shyoon991@gmail.com>
2023-01-20 08:22:53 +0900

    Bean의 생명주기 메서드 - ApplicationContextAware

--- src/main/java/tobyspring/helloboot/InflearnTobySpringBootApplication.java
+++ src/main/java/tobyspring/helloboot/InflearnTobySpringBootApplication.java
@@ -3,6 +3,7 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -12,15 +13,29 @@ import org.springframework.web.servlet.DispatcherServlet;
 @ComponentScan
 public class InflearnTobySpringBootApplication {
 
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
     public static void main(String[] args) {
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
-                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+                // 직접 ApplicationContext 를 주입하지 않아도 Spring Container 가 자동으로 주입해줌
+                // ApplicationContextAware
+//                dispatcherServlet.setApplicationContext(this);
+
                 WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet",
-                                    new DispatcherServlet(this))
+                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
                             .addMapping("/*");
                 });
                 webServer.start();
