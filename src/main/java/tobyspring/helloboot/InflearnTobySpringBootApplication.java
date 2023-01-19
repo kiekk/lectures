c29eb0c530faf85f7028068f8b58613d02e8c7a5
shyoon <shyoon991@gmail.com>
2023-01-19 16:27:41 +0900

    서블릿 직접 등록

--- src/main/java/tobyspring/helloboot/InflearnTobySpringBootApplication.java
+++ src/main/java/tobyspring/helloboot/InflearnTobySpringBootApplication.java
@@ -1,15 +1,32 @@
 package tobyspring.helloboot;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
 
 public class InflearnTobySpringBootApplication {
 
     public static void main(String[] args) {
         // ServletWebServerFactory, WebServer 추상화 객체
         ServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = tomcatServletWebServerFactory.getWebServer();
+        WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println("Hello Servlet");
+                }
+            }).addMapping("/hello");
+        });
 
         webServer.start(); // Tomcat Servlet Container 동작
     }
