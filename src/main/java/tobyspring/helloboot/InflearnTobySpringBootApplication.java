9da2303e4611b84279a869c32c97c11e87461610
shyoon <shyoon991@gmail.com>
2023-01-20 08:27:31 +0900

    add Custom SpringBootApplication - MySpringApplication

--- src/main/java/tobyspring/helloboot/InflearnTobySpringBootApplication.java
+++ src/main/java/tobyspring/helloboot/InflearnTobySpringBootApplication.java
@@ -1,12 +1,10 @@
 package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -24,25 +22,7 @@ public class InflearnTobySpringBootApplication {
     }
 
     public static void main(String[] args) {
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-                // 직접 ApplicationContext 를 주입하지 않아도 Spring Container 가 자동으로 주입해줌
-                // ApplicationContextAware
-//                dispatcherServlet.setApplicationContext(this);
-
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                            .addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-        applicationContext.register(InflearnTobySpringBootApplication.class);
-        applicationContext.refresh();
+        MySpringApplication.run(InflearnTobySpringBootApplication.class, args);
     }
 
 }
