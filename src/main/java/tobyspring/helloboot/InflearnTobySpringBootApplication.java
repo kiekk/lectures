142e009e539335f2d29eaf231bc444939452ca0f
shyoon <shyoon991@gmail.com>
2023-01-19 16:18:10 +0900

    Tomcat Servlet Container 직접 구현

--- src/main/java/tobyspring/helloboot/InflearnTobySpringBootApplication.java
+++ src/main/java/tobyspring/helloboot/InflearnTobySpringBootApplication.java
@@ -1,13 +1,17 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
 public class InflearnTobySpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(InflearnTobySpringBootApplication.class, args);
+        // ServletWebServerFactory, WebServer 추상화 객체
+        ServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = tomcatServletWebServerFactory.getWebServer();
+
+        webServer.start(); // Tomcat Servlet Container 동작
     }
 
 }
