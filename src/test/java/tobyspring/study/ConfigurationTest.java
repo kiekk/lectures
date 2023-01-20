70f8d1b3f390d169ee3eff15882f9b33e82b3ddb
shyoon <shyoon991@gmail.com>
2023-01-20 12:08:35 +0900

    @Configuration과 proxyBeanMethods - 2

--- src/test/java/tobyspring/study/ConfigurationTest.java
+++ src/test/java/tobyspring/study/ConfigurationTest.java
@@ -2,18 +2,23 @@ package tobyspring.study;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 public class ConfigurationTest {
     @Test
     void configuration() {
-        MyConfig myConfig = new MyConfig();
-        Bean1 bean1 = myConfig.bean1();
-        Bean2 bean2 = myConfig.bean2();
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
+        annotationConfigApplicationContext.register(MyConfig.class);
+        annotationConfigApplicationContext.refresh();
 
-        // 일반 객체 생성 방식으로 작성했을 때
-        // bean1 과 bean2 의 common 은 같지 않음
+        Bean1 bean1 = annotationConfigApplicationContext.getBean(Bean1.class);
+        Bean2 bean2 = annotationConfigApplicationContext.getBean(Bean2.class);
+
+        // bean 등록 방식으로 작성했을 때
+        // bean1 과 bean2 의 common 은 같음
+        // @Configuration 애노테이션의 동작 방식을 확인
         Assertions.assertThat(bean1.common).isEqualTo(bean2.common);
     }
 
