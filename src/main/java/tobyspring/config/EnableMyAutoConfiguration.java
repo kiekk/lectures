d3b3fa987c12ba6d2a1e3f09ae09c2b68ad6cccb
shyoon <shyoon991@gmail.com>
2023-01-20 11:37:49 +0900

    인프라 빈 구성 정보의 분리 - 3

--- src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
+++ src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
@@ -1,7 +1,5 @@
-package tobyspring.helloboot;
+package tobyspring.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import tobyspring.config.autoconfig.DispatcherServletConfig;
 import tobyspring.config.autoconfig.TomcatWebServerConfig;
@@ -11,10 +9,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+// 이 애노테이션에 설정에 추가 할 클래스 정보를 입력
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Configuration
-@ComponentScan
 @Import({DispatcherServletConfig.class, TomcatWebServerConfig.class})
-public @interface MySpringBootApplication {
+public @interface EnableMyAutoConfiguration {
 }
