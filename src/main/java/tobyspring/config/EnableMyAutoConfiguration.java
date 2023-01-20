0ccc08c6c34139e483052b7c2e14f35869d05302
shyoon <shyoon991@gmail.com>
2023-01-20 11:43:31 +0900

    동적인 자동 구성 정보 등록

--- src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
+++ src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
@@ -1,8 +1,6 @@
 package tobyspring.config;
 
 import org.springframework.context.annotation.Import;
-import tobyspring.config.autoconfig.DispatcherServletConfig;
-import tobyspring.config.autoconfig.TomcatWebServerConfig;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -12,6 +10,6 @@ import java.lang.annotation.Target;
 // 이 애노테이션에 설정에 추가 할 클래스 정보를 입력
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class})
+@Import(MyAutoConfigImportSelector.class)
 public @interface EnableMyAutoConfiguration {
 }
