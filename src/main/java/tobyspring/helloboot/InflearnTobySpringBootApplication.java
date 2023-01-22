ca6f9bd9fc5f87cc3134d1187a7a5ed81aa5f5e9
soonho <shyoon991@gmail.com>
2023-01-22 12:24:07 +0900

    자동 구성에 Environment 프로퍼티 적용 및 우선순위 확인

--- src/main/java/tobyspring/helloboot/InflearnTobySpringBootApplication.java
+++ src/main/java/tobyspring/helloboot/InflearnTobySpringBootApplication.java
@@ -1,6 +1,9 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import tobyspring.config.MySpringBootApplication;
 
 @MySpringBootApplication
@@ -10,4 +13,16 @@ public class InflearnTobySpringBootApplication {
         SpringApplication.run(InflearnTobySpringBootApplication.class, args);
     }
 
+    /*
+    Environment 프로퍼티 적용 및 우선 순위 확인
+
+    System Property > Environment Variable > Application Properties
+     */
+    @Bean
+    ApplicationRunner applicationRunner(Environment env) {
+        return args -> {
+            String name = env.getProperty("my.name");
+            System.out.println("my.name: " + name);
+        };
+    }
 }
