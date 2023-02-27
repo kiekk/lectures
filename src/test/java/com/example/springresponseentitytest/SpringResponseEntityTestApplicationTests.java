b5c1d40f6c19eeed3826bef6e6501b76a7545743
soonho <shyoon991@gmail.com>
2023-02-27 20:55:10 +0900

    Initial Commit

---
+++ src/test/java/com/example/springresponseentitytest/SpringResponseEntityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springresponseentitytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResponseEntityTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
