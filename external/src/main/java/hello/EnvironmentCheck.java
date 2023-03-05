0c62072c0d3a170f8d43d555a38c5aeb15c1cff2
soonho <shyoon991@gmail.com>
2023-03-05 21:21:45 +0900

    external: 외부 설정 파일 - 1

--- external/src/main/java/hello/EnvironmentCheck.java
+++ external/src/main/java/hello/EnvironmentCheck.java
@@ -17,10 +17,13 @@ public class EnvironmentCheck {
     @PostConstruct
     public void init() {
         String url = env.getProperty("url");
+        String user = env.getProperty("user");
+        // window 에서는 username 값이 환경 변수의 username 을 읽어옴. *추후 확인
         String username = env.getProperty("username");
         String password = env.getProperty("password");
 
         log.info("env url = {}", url);
+        log.info("env user = {}", user);
         log.info("env username = {}", username);
         log.info("env password = {}", password);
     }
