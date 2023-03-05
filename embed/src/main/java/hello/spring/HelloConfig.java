0a1c1a1ab8ed8cc3fc7e620a7f63b7ead8639b26
soonho <shyoon991@gmail.com>
2023-03-05 14:01:40 +0900

    embed: 편리한 부트 클래스 만들기: add MySpringBootMain

--- embed/src/main/java/hello/spring/HelloConfig.java
+++ embed/src/main/java/hello/spring/HelloConfig.java
@@ -3,7 +3,8 @@ package hello.spring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+// @MySpringBootApplication 의 @ComponentScan 방식으로 변경
+//@Configuration
 public class HelloConfig {
 
     @Bean
