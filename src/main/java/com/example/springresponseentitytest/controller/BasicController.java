e786061de725689574c2f697593a071f134060eb
soonho <shyoon991@gmail.com>
2023-03-10 21:28:17 +0900

    add BasicController

---
+++ src/main/java/com/example/springresponseentitytest/controller/BasicController.java
@@ -0,0 +1,29 @@
+package com.example.springresponseentitytest.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    @GetMapping("home")
+    public String home() {
+        return "basic/home";
+    }
+
+    @GetMapping("response-body")
+    @ResponseBody
+    public String responseBody() {
+        return "basic-response-body";
+    }
+
+    @GetMapping("response-entity")
+    public ResponseEntity<?> responseEntity() {
+        return new ResponseEntity<>("basic-response-entity", HttpStatus.OK);
+    }
+}
