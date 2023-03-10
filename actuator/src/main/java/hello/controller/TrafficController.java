9ec0537bc6ebe92da320c45c620986435e408134
soonho <shyoon991@gmail.com>
2023-03-10 21:12:46 +0900

    prometheus: add TrafficController jdbc() (connection pool 부하용)

--- actuator/src/main/java/hello/controller/TrafficController.java
+++ actuator/src/main/java/hello/controller/TrafficController.java
@@ -1,9 +1,13 @@
 package hello.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,13 +17,26 @@ public class TrafficController {
 
     private List<String> list = new ArrayList<>();
 
+    @Autowired
+    DataSource dataSource;
+
+    @GetMapping("/jdbc")
+    public String jdbc() throws SQLException {
+        log.info("jdbc");
+        Connection conn = dataSource.getConnection();
+        log.info("connection info={}", conn);
+
+        //conn.close(); //커넥션을 닫지 않는다.
+        return "ok";
+    }
+
     @GetMapping("/jvm")
     public String jvm() {
         log.info("jvm");
         for (int i = 0; i < 1000000; i++) {
             list.add("hello jvm!" + i);
         }
-       return "ok";
+        return "ok";
     }
 
     @GetMapping("/cpu")
