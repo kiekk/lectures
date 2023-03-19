5b7c49f9c967988de9a7dba9d57f720061666720
soonho <shyoon991@gmail.com>
2023-03-19 18:24:46 +0900

    Initial Commit

---
+++ src/main/java/com/example/tobyspringboot/StudySpringBootInjectInitializerCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tobyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBootInjectInitializerCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringBootInjectInitializerCodeApplication.class, args);
+    }
+
+}
