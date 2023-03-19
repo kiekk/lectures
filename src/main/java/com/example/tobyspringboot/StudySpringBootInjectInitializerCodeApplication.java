1f953205f28e8cbbb003d34f3c7ac1bd9174a7a5
soonho <shyoon991@gmail.com>
2023-03-19 18:31:57 +0900

    use CommandLineRunner - 1. 클래스 선언

--- src/main/java/com/example/tobyspringboot/StudySpringBootInjectInitializerCodeApplication.java
+++ src/main/java/com/example/tobyspringboot/StudySpringBootInjectInitializerCodeApplication.java
@@ -1,7 +1,9 @@
 package com.example.tobyspringboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class StudySpringBootInjectInitializerCodeApplication {
@@ -10,4 +12,15 @@ public class StudySpringBootInjectInitializerCodeApplication {
         SpringApplication.run(StudySpringBootInjectInitializerCodeApplication.class, args);
     }
 
+}
+
+// CommandLineRunner interface 를 구현한 객체를 Bean 으로 등록하면
+// Spring Boot 초기화 후 코드를 실행할 수 있다.
+@Component
+class MyCommandLineRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello CommandLineRunner");
+    }
 }
