d76e09d23dcf536a189a477c6f1dea7facdcedb5
soonho <shyoon991@gmail.com>
2023-03-19 18:51:55 +0900

    Spring 에서 Listener 만들기

--- src/main/java/com/example/tobyspringboot/StudySpringBootInjectInitializerCodeApplication.java
+++ src/main/java/com/example/tobyspringboot/StudySpringBootInjectInitializerCodeApplication.java
@@ -2,18 +2,20 @@ package com.example.tobyspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StudySpringBootInjectInitializerCodeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StudySpringBootInjectInitializerCodeApplication.class, args);
+        ConfigurableApplicationContext ac = SpringApplication.run(StudySpringBootInjectInitializerCodeApplication.class, args);
+        // Listener 등록
+        ac.addApplicationListener(event -> System.out.println("Hello ApplicationEvent: " + event));
+
+        // Event Publish
+        ac.publishEvent(new ApplicationEvent(ac) {
+        });
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void init() {
-        System.out.println("Hello ApplicationReadyEvent");
-    }
 }
