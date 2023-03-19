2852a258bf59a2cebdd96f1ef7a78bacdeb8e39c
soonho <shyoon991@gmail.com>
2023-03-19 18:58:39 +0900

    use @EventListener - Custom ApplicationEvent

--- src/main/java/com/example/tobyspringboot/StudySpringBootInjectInitializerCodeApplication.java
+++ src/main/java/com/example/tobyspringboot/StudySpringBootInjectInitializerCodeApplication.java
@@ -3,19 +3,22 @@ package com.example.tobyspringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class StudySpringBootInjectInitializerCodeApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext ac = SpringApplication.run(StudySpringBootInjectInitializerCodeApplication.class, args);
-        // Listener 등록
-        ac.addApplicationListener((ApplicationListener<MyEvent>) event -> System.out.println("Hello ApplicationEvent: " + event.getMessage()));
-
         // Event Publish
         ac.publishEvent(new MyEvent(ac, "TobySpringBoot Event"));
+    }
+
+    // Event Listener
+    @EventListener(MyEvent.class)
+    public void onMyEvent(MyEvent event) {
+        System.out.println("Hello My Event: " + event.getMessage());
     }
 
     static class MyEvent extends ApplicationEvent {
