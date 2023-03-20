c5f39cfb1f77d9a0681e41d7060c52e623ad90f1
shyoon <shyoon991@gmail.com>
2023-03-20 11:20:09 +0900

    RestTemplate & ApplicationRunner로 스프링 부트 시작 시 환율 정보 가져오기

--- src/main/java/com/example/resttemplate/StudyTobySpringRestClientApplication.java
+++ src/main/java/com/example/resttemplate/StudyTobySpringRestClientApplication.java
@@ -1,7 +1,10 @@
 package com.example.resttemplate;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class StudyTobySpringRestClientApplication {
@@ -10,4 +13,13 @@ public class StudyTobySpringRestClientApplication {
         SpringApplication.run(StudyTobySpringRestClientApplication.class, args);
     }
 
+    @Bean
+    ApplicationRunner init() {
+        return args -> {
+            // https://open.er-api.com/v6/latest
+            RestTemplate restTemplate = new RestTemplate();
+            String res = restTemplate.getForObject("https://open.er-api.com/v6/latest", String.class);
+            System.out.println(res);
+        };
+    }
 }
