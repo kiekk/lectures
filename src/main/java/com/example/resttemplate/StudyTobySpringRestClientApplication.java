efa213ce0847d4038526ff4a7ef1f878f8e245fc
shyoon <shyoon991@gmail.com>
2023-03-20 11:33:05 +0900

    HTTP Interface 사용하여 환율 정보 가져오기

--- src/main/java/com/example/resttemplate/StudyTobySpringRestClientApplication.java
+++ src/main/java/com/example/resttemplate/StudyTobySpringRestClientApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.util.Map;
 
@@ -22,11 +25,24 @@ public class StudyTobySpringRestClientApplication {
             // https://open.er-api.com/v6/latest
             RestTemplate restTemplate = new RestTemplate();
             Map<String, Map<String, Double>> res = restTemplate.getForObject("https://open.er-api.com/v6/latest", Map.class);
-            System.out.println(res.get("rates").get("USD"));
+            System.out.println(res.get("rates").get("KRW"));
 
             WebClient client = WebClient.create("https://open.er-api.com");
-            Map<String, Map<String, Double>> res2  = client.get().uri("/v6/latest").retrieve().bodyToMono(Map.class).block();
-            System.out.println(res2.get("rates").get("USD"));
+            Map<String, Map<String, Double>> res2 = client.get().uri("/v6/latest").retrieve().bodyToMono(Map.class).block();
+            System.out.println(res2.get("rates").get("KRW"));
+
+            HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory
+                    .builder(WebClientAdapter.forClient(client))
+                    .build();
+
+            ErApi erApi = httpServiceProxyFactory.createClient(ErApi.class);
+            Map<String, Map<String, Double>> res3 = erApi.getLatest();
+            System.out.println(res3.get("rates").get("KRW"));
         };
+    }
+
+    interface ErApi {
+        @GetExchange("/v6/latest")
+        Map getLatest();
     }
 }
