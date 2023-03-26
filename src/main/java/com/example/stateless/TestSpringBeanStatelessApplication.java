91725305e5f94c3a4cc804f3d932004b9ef67712
soonho <shyoon991@gmail.com>
2023-03-26 21:43:42 +0900

    Initial Commit

---
+++ src/main/java/com/example/stateless/TestSpringBeanStatelessApplication.java
@@ -0,0 +1,13 @@
+package com.example.stateless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBeanStatelessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringBeanStatelessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stateless/TestSpringBeanStatelessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stateless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBeanStatelessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
