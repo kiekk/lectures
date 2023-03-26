7478b2b0ee19556b53d9357b21d7612d87b48f38
shyoon <shyoon991@gmail.com>
2023-03-27 07:50:44 +0900

    Initial Commit

---
+++ src/main/java/dev/StudySpringBootDevToolsApplication.java
@@ -0,0 +1,13 @@
+package dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBootDevToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringBootDevToolsApplication.class, args);
+    }
+
+}
