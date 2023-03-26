299b20b4e776982fb80ee3a542de438dd16da7f2
soonho <shyoon991@gmail.com>
2023-03-26 22:08:53 +0900

    add test - StatelessService

--- src/test/java/com/example/stateless/StatefulServiceTest.java
+++ src/test/java/com/example/stateless/StatefulServiceTest.java
@@ -71,6 +71,31 @@ public class StatefulServiceTest {
         Assertions.assertThat(statefulService1.getPrice()).isNotEqualTo(statefulService2.getPrice());
     }
 
+    // 위 문제를 모두 충족하려면 Bean 이 더 이상 상태를 가지지 않도록 하면 된다.
+    // 상태를 가지지 않도록 하여 Spring 이 지향하는 singleton 방식도 해결하고 동시성 이슈도 해결할 수 있다.
+    @Test
+    void statelessService() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(StatelessService.class);
+        StatelessService statelessService1 = ac.getBean(StatelessService.class);
+        StatelessService statelessService2 = ac.getBean(StatelessService.class);
+
+        //ThreadA: A사용자 10000원 주문
+        int priceOfUserA = statelessService1.order("userA", 10000);
+        //ThreadB: B사용자 20000원 주문
+        int priceOfUserB = statelessService2.order("userB", 20000);
+
+        System.out.println("priceOfUserA = " + priceOfUserA);
+        System.out.println("priceOfUserB = " + priceOfUserB);
+
+        // UserA의 statelessService1, UserB의 statelessService2 비교
+        System.out.println("statelessService1 : " + statelessService1);
+        System.out.println("statelessService2 : " + statelessService2);
+        System.out.println("statelessService1 == statelessService2 : " + (statelessService1 == statelessService2));
+
+        Assertions.assertThat(priceOfUserA).isEqualTo(10000);
+        Assertions.assertThat(priceOfUserB).isEqualTo(20000);
+    }
+
     static class StatefulService {
         private int price; // 상태를 가지고 있는 필드
 
@@ -90,10 +115,20 @@ public class StatefulServiceTest {
 
         public void order(String name, int price) {
             System.out.println("name = " + name + " price = " + price);
-            this.price = price; // 문제가 발생할 수 있는 코드
+            this.price = price;
+            // 이제 Bean 을 요청할 때마다 객체를 생성하기 떄문에
+            // 더 이상 동시성 이슈는 발생하지 않는다.
+            // 하지만 사용자마다 객체를 생성해주기 때문에 불필요한 객체들이 너무 많이 생성된다.
         }
 
         public int getPrice() {
+            return price;
+        }
+    }
+
+    static class StatelessService {
+        public int order(String name, int price) {
+            System.out.println("name = " + name + " price = " + price);
             return price;
         }
     }
