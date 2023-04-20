525dbeacef7fe4245086f558e4930e5fe8bc4dba
soonho <shyoon991@gmail.com>
2023-04-20 20:53:44 +0900

    inner class 외부로 추출

--- src/test/java/com/example/product/ProductServiceTest.java
+++ src/test/java/com/example/product/ProductServiceTest.java
@@ -2,10 +2,6 @@ package com.example.product;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.util.Assert;
-
-import java.util.HashMap;
-import java.util.Map;
 
 class ProductServiceTest {
 
@@ -29,94 +25,5 @@ class ProductServiceTest {
         productService.addProduct(request);
     }
 
-    private class ProductService {
 
-        private final ProductPort productPort;
-
-        private ProductService(ProductPort productPort) {
-            this.productPort = productPort;
-        }
-
-        public void addProduct(AddProductRequest request) {
-            final Product product = new Product(request.name(), request.price(), request.discountPolicy());
-            productPort.save(product);
-        }
-    }
-
-    private record AddProductRequest(String name, int price, DiscountPolicy discountPolicy) {
-
-        private AddProductRequest {
-            Assert.hasText(name, "상품명은 필수입니다.");
-            Assert.isTrue(price > 0, "상품 가격은 0보다 커야 합니다.");
-            Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
-        }
-    }
-
-    private enum DiscountPolicy {
-        NONE
-    }
-
-    private class Product {
-        private Long id;
-        private final String name;
-        private final int price;
-        private final DiscountPolicy discountPolicy;
-
-        public Product(final String name, final int price, final DiscountPolicy discountPolicy) {
-            Assert.hasText(name, "상품명은 필수입니다.");
-            Assert.isTrue(price > 0, "상품 가격은 0보다 커야 합니다.");
-            Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
-            this.name = name;
-            this.price = price;
-            this.discountPolicy = discountPolicy;
-        }
-
-        public void assignId(final Long id) {
-            this.id = id;
-        }
-
-        public Long getId() {
-            return id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public int getPrice() {
-            return price;
-        }
-
-        public DiscountPolicy getDiscountPolicy() {
-            return discountPolicy;
-        }
-    }
-
-    private interface ProductPort {
-        void save(final Product product);
-    }
-
-
-    private class ProductAdapter implements ProductPort {
-        private final ProductRepository productRepository;
-
-        private ProductAdapter(ProductRepository productRepository) {
-            this.productRepository = productRepository;
-        }
-
-        @Override
-        public void save(final Product product) {
-            productRepository.save(product);
-        }
-    }
-
-    private class ProductRepository {
-        private Map<Long, Product> persistence = new HashMap<>();
-        private Long sequence = 0L;
-
-        public void save(final Product product) {
-            product.assignId(++sequence);
-            persistence.put(product.getId(), product);
-        }
-    }
 }
