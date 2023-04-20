ad709f5bda490d0db2a781c6d4ba39742da1c371
soonho <shyoon991@gmail.com>
2023-04-20 20:54:25 +0900

    request 생성 코드 메서드로 추출

--- src/test/java/com/example/product/ProductServiceTest.java
+++ src/test/java/com/example/product/ProductServiceTest.java
@@ -18,11 +18,16 @@ class ProductServiceTest {
 
     @Test
     void 상품등록() {
+        final AddProductRequest request = 상품등록요청_생성();
+        productService.addProduct(request);
+    }
+
+    private static AddProductRequest 상품등록요청_생성() {
         final String name = "상품명";
         final int price = 1000;
         final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
         final AddProductRequest request = new AddProductRequest(name, price, discountPolicy);
-        productService.addProduct(request);
+        return request;
     }
 
 
