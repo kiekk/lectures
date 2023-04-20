92f24bb343897f1bb3419911687ab01ea1f03ee6
soonho <shyoon991@gmail.com>
2023-04-20 20:50:08 +0900

    POJO 상품 등록 기능 구현하기

--- src/test/java/com/example/product/ProductServiceTest.java
+++ src/test/java/com/example/product/ProductServiceTest.java
@@ -4,13 +4,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class ProductServiceTest {
 
     private ProductService productService;
+    private ProductPort productPort;
+    private ProductRepository productRepository;
 
     @BeforeEach
     void setUp() {
-        productService = new ProductService();
+        productRepository = new ProductRepository();
+        productPort = new ProductAdapter(productRepository);
+        productService = new ProductService(productPort);
     }
 
     @Test
@@ -24,8 +31,15 @@ class ProductServiceTest {
 
     private class ProductService {
 
+        private final ProductPort productPort;
+
+        private ProductService(ProductPort productPort) {
+            this.productPort = productPort;
+        }
+
         public void addProduct(AddProductRequest request) {
-            throw new UnsupportedOperationException("Unsupported addProduct");
+            final Product product = new Product(request.name(), request.price(), request.discountPolicy());
+            productPort.save(product);
         }
     }
 
@@ -40,5 +54,69 @@ class ProductServiceTest {
 
     private enum DiscountPolicy {
         NONE
+    }
+
+    private class Product {
+        private Long id;
+        private final String name;
+        private final int price;
+        private final DiscountPolicy discountPolicy;
+
+        public Product(final String name, final int price, final DiscountPolicy discountPolicy) {
+            Assert.hasText(name, "상품명은 필수입니다.");
+            Assert.isTrue(price > 0, "상품 가격은 0보다 커야 합니다.");
+            Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
+            this.name = name;
+            this.price = price;
+            this.discountPolicy = discountPolicy;
+        }
+
+        public void assignId(final Long id) {
+            this.id = id;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+
+        public DiscountPolicy getDiscountPolicy() {
+            return discountPolicy;
+        }
+    }
+
+    private interface ProductPort {
+        void save(final Product product);
+    }
+
+
+    private class ProductAdapter implements ProductPort {
+        private final ProductRepository productRepository;
+
+        private ProductAdapter(ProductRepository productRepository) {
+            this.productRepository = productRepository;
+        }
+
+        @Override
+        public void save(final Product product) {
+            productRepository.save(product);
+        }
+    }
+
+    private class ProductRepository {
+        private Map<Long, Product> persistence = new HashMap<>();
+        private Long sequence = 0L;
+
+        public void save(final Product product) {
+            product.assignId(++sequence);
+            persistence.put(product.getId(), product);
+        }
     }
 }
