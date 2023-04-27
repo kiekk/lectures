e26b19f7f81e71a621122b20b8dbc9b06cce320e
shyoon <shyoon991@gmail.com>
2023-04-28 08:35:47 +0900

    extract method to ProductSteps - 메서드 재사용을 위해

--- src/test/java/com/example/product/ProductApiTest.java
+++ src/test/java/com/example/product/ProductApiTest.java
@@ -1,11 +1,9 @@
 package com.example.product;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -14,31 +12,12 @@ class ProductApiTest extends Apitest {
 
     @Test
     void 상품등록() {
-        final AddProductRequest request = 상품등록요청_생성();
+        final AddProductRequest request = ProductSteps.상품등록요청_생성();
 
         // API 요청
-        final ExtractableResponse<Response> response = 상품등록요청(request);
+        final ExtractableResponse<Response> response = ProductSteps.상품등록요청(request);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
-
-    private static ExtractableResponse<Response> 상품등록요청(AddProductRequest request) {
-        return RestAssured.given().log().all() // 요청을 보내는 로그를 모두 남긴다.
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when()
-                .post("/products")
-                .then()
-                .log().all().extract();
-    }
-
-    private static AddProductRequest 상품등록요청_생성() {
-        final String name = "상품명";
-        final int price = 1000;
-        final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
-        final AddProductRequest request = new AddProductRequest(name, price, discountPolicy);
-        return request;
-    }
-
 
 }
