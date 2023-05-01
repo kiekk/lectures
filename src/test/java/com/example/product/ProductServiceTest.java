66c6d2ab82349eeeea238bbd00aa1591976979c2
soonho <shyoon991@gmail.com>
2023-05-01 23:34:33 +0900

    POJO 상품 수정 기능 구현하기 - convert class → record

--- src/test/java/com/example/product/ProductServiceTest.java
+++ src/test/java/com/example/product/ProductServiceTest.java
@@ -33,19 +33,12 @@ public class ProductServiceTest {
         productService.updateProduct(productId, request);
     }
 
-    private class UpdateProductRequest {
+    private record UpdateProductRequest(String name, int price, DiscountPolicy discountPolicy) {
 
-        private final String name;
-        private final int price;
-        private final DiscountPolicy discountPolicy;
-
-        public UpdateProductRequest(final String name, final int price, final DiscountPolicy discountPolicy) {
+        private UpdateProductRequest {
             Assert.hasText(name, "상품명은 필수입니다.");
             Assert.isTrue(price > 0, "상품 가격은 0보다 커야 합니다.");
             Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
-            this.name = name;
-            this.price = price;
-            this.discountPolicy = discountPolicy;
         }
-    }
+        }
 }
