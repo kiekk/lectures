2e67101401cbaaf5b7ce4e50019b19010cb8cff7
soonho <shyoon991@gmail.com>
2023-05-01 23:34:09 +0900

    POJO 상품 수정 기능 구현하기 - add class UpdateProductRequest

--- src/test/java/com/example/product/ProductServiceTest.java
+++ src/test/java/com/example/product/ProductServiceTest.java
@@ -26,4 +26,26 @@ public class ProductServiceTest {
         assertThat(response).isNotNull();
     }
 
+    @Test
+    void 상품수정() {
+        final Long productId = 1L;
+        final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
+        productService.updateProduct(productId, request);
+    }
+
+    private class UpdateProductRequest {
+
+        private final String name;
+        private final int price;
+        private final DiscountPolicy discountPolicy;
+
+        public UpdateProductRequest(final String name, final int price, final DiscountPolicy discountPolicy) {
+            Assert.hasText(name, "상품명은 필수입니다.");
+            Assert.isTrue(price > 0, "상품 가격은 0보다 커야 합니다.");
+            Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
+            this.name = name;
+            this.price = price;
+            this.discountPolicy = discountPolicy;
+        }
+    }
 }
