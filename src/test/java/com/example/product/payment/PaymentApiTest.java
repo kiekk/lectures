bbc4b258994ddf4b9ee4ed6fa7dcb93e019549ad
shyoon <shyoon991@gmail.com>
2023-05-11 09:32:03 +0900

    주문 결제 API 개발 - PaymentSteps 추출

--- src/test/java/com/example/product/payment/PaymentApiTest.java
+++ src/test/java/com/example/product/payment/PaymentApiTest.java
@@ -3,12 +3,10 @@ package com.example.product.payment;
 import com.example.product.Apitest;
 import com.example.product.ProductSteps;
 import com.example.product.order.OrderSteps;
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -17,14 +15,8 @@ public class PaymentApiTest extends Apitest {
     void 상품주문() {
         ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
         OrderSteps.상품주문요청(OrderSteps.상품주문요청_생성());
-        final PaymentRequest request = PaymentSteps.주문결제요청_생성();
 
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when()
-                .post("/payments")
-                .then().log().all().extract();
+        ExtractableResponse<Response> response = PaymentSteps.주문결제요청(PaymentSteps.주문결제요청_생성());
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
