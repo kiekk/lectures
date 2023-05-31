a065c36538c7bac0239f3b8dc8cc7eeeef128dbe
soonho <shyoon991@gmail.com>
2023-05-31 22:12:35 +0900

    section2 - number type

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,10 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = .123;
+let num5: number = -0.123;
+let num6: number = -.123;
+let num7: number = Infinity;
+let num8: number = -Infinity;
+let num9: number = -NaN;
