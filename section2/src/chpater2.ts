7b5a39904f8f52a5dfe892dbbb54b8f641d2362e
soonho <shyoon991@gmail.com>
2023-05-31 22:31:00 +0900

    section2 - array

---
+++ section2/src/chpater2.ts
@@ -0,0 +1,8 @@
+// array
+let numArr: number[] = [1, 2, 3]
+
+let strArr: string[] = ["hello", "im", "winterlood"];
+
+let boolArr: Array<boolean> = [true, false, true];
+
+// number[]와 Array<number>는 동일합니다.
