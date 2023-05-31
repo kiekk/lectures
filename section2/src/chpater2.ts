f117924d3b1f533a7773b7841aeb3ac634f9507b
soonho <shyoon991@gmail.com>
2023-05-31 22:31:35 +0900

    section2 - nested array

--- section2/src/chpater2.ts
+++ section2/src/chpater2.ts
@@ -6,3 +6,9 @@ let strArr: string[] = ["hello", "im", "winterlood"];
 let boolArr: Array<boolean> = [true, false, true];
 
 // number[]와 Array<number>는 동일합니다.
+
+// nested array
+let doubleArr : number[][] = [
+    [1, 2, 3],
+    [4, 5],
+];
