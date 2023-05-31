8b7f9a9bbc16663f8999f6560acbb56da2d9e1c5
soonho <shyoon991@gmail.com>
2023-05-31 22:14:01 +0900

    section2 - boolean type

--- section2/src/chapter1.ts
+++ section2/src/chapter1.ts
@@ -14,3 +14,7 @@ let str1: string = "hello";
 let str2: string = 'hello';
 let str3: string = `hello`;
 let str4: string = `hello ${str1}`; // template literal string
+
+// boolean
+let bool1 : boolean = true;
+let bool2 : boolean = false;
