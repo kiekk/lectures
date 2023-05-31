be6707c87667143209704f9234591941fff26b14
soonho <shyoon991@gmail.com>
2023-05-31 22:14:50 +0900

    section2 - null type

--- section2/src/chapter1.ts
+++ section2/src/chapter1.ts
@@ -18,3 +18,11 @@ let str4: string = `hello ${str1}`; // template literal string
 // boolean
 let bool1 : boolean = true;
 let bool2 : boolean = false;
+
+// null
+let null1: null = null;
+
+// null 타입은 null 외에 다른 값을 할당할 수 없음.
+// null1 = undefined
+// null1 = 1;
+// null1 = 'hello';
