c663a3b54f9cebcb020687a5208df08fdb703fad
soonho <shyoon991@gmail.com>
2023-05-31 22:13:16 +0900

    section2 - string type

--- section2/src/chapter1.ts
+++ section2/src/chapter1.ts
@@ -8,3 +8,9 @@ let num6: number = -.123;
 let num7: number = Infinity;
 let num8: number = -Infinity;
 let num9: number = -NaN;
+
+// string
+let str1: string = "hello";
+let str2: string = 'hello';
+let str3: string = `hello`;
+let str4: string = `hello ${str1}`; // template literal string
