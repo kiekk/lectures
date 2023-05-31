9ac0cdc657c4d28a6f96177aece62ec5e7194c03
soonho <shyoon991@gmail.com>
2023-05-31 22:33:53 +0900

    section2 - 다양한 타입의 값을 갖는 배열

--- section2/src/chpater2.ts
+++ section2/src/chpater2.ts
@@ -12,3 +12,15 @@ let doubleArr : number[][] = [
     [1, 2, 3],
     [4, 5],
 ];
+
+// 다양한 타입을 갖는 배열
+let multiArr = [1, "hello"];
+
+// 유니온 타입이라고도 합니다.
+let multiArr2: (number | string)[] = [1, "hello"];
+
+let multiArr3: (number | string)[]
+
+// 단점, 타입의 순서를 지정할 수는 없습니다.
+multiArr3 = [1, "hello"]
+multiArr3 = ["hello", 1]
