a7bc5c871724d85f338dcf41e0adf8ee9c418e19
shyoon <shyoon991@gmail.com>
2023-06-03 01:35:10 +0900

    oauth2Login()

--- src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/config/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/config/OAuth2ClientConfig.java
@@ -2,26 +2,20 @@ package io.security.oauth2.inflearnspringsecurityoauth2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.ClientRegistrations;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class OAuth2ClientConfig {
 
     @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(keycloakClientRegistration());
-    }
-
-    private ClientRegistration keycloakClientRegistration() {
-        return ClientRegistrations.fromIssuerLocation("http://localhost:18080/realms/master")
-                .registrationId("keycloak")
-                .clientId("oauth2-client-app")
-                .clientSecret("client secret")
-                .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
-                .scope("openid", "email", "profile")
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests((authz) -> {
+                    authz.anyRequest().authenticated();
+                })
+                .oauth2Login()
+                .and()
                 .build();
     }
 }
