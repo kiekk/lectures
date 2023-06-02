b0fcbbd4ae5121f06dbe8760a22669e88c2d18b4
shyoon <shyoon991@gmail.com>
2023-06-02 15:08:01 +0900

    ClientRegistrationRepository test

--- src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/controller/IndexController.java
+++ src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/controller/IndexController.java
@@ -1,13 +1,28 @@
 package io.security.oauth2.inflearnspringsecurityoauth2.controller;
 
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
+    public IndexController(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
     @GetMapping("")
     public String index() {
+        ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak");
+
+        String clientId = clientRegistration.getClientId();
+        System.out.println("clientId = " + clientId);
+
+        String redirectUri = clientRegistration.getRedirectUri();
+        System.out.println("redirectUri = " + redirectUri);
         return "index";
     }
 }
