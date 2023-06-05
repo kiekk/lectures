8c9f2adaa506c8f8d1b73a73ed938066b04cb362
shyoon <shyoon991@gmail.com>
2023-06-05 17:50:27 +0900

    OpenID Connect logout

--- src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/config/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/config/OAuth2ClientConfig.java
@@ -1,21 +1,41 @@
 package io.security.oauth2.inflearnspringsecurityoauth2.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 public class OAuth2ClientConfig {
+
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeRequests((authz) -> {
-                    authz.anyRequest().permitAll();
+                    authz.anyRequest().authenticated();
                 })
                 .oauth2Login(Customizer.withDefaults())
+                .logout(logout -> {
+                    logout.logoutSuccessHandler(oidcLogoutSuccessHandler());
+                    logout.invalidateHttpSession(true);
+                    logout.clearAuthentication(true);
+                    logout.deleteCookies("JSESSIONID");
+                })
                 .build();
+    }
+
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        // client logout redirect uri 에 등록된 uri 이어야 합니다.
+        successHandler.setPostLogoutRedirectUri("http://localhost:8081/login");
+        return successHandler;
     }
 }
