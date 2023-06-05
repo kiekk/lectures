7d454411400c097945794005b4ee156562874c25
shyoon <shyoon991@gmail.com>
2023-06-05 13:33:54 +0900

    authentication by oidc

--- src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/controller/IndexController.java
+++ src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/controller/IndexController.java
@@ -1,15 +1,21 @@
 package io.security.oauth2.inflearnspringsecurityoauth2.controller;
 
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class IndexController {
@@ -37,5 +43,30 @@ public class IndexController {
         OAuth2UserRequest oAuth2UserRequest = new OAuth2UserRequest(clientRegistration, oAuth2AccessToken);
         DefaultOAuth2UserService userService = new DefaultOAuth2UserService();
         return userService.loadUser(oAuth2UserRequest);
+    }
+
+    @GetMapping("oidc")
+    public OAuth2User oidc(String accessToken, String idToken) {
+        ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("keycload");
+        OAuth2AccessToken oAuth2AccessToken = new OAuth2AccessToken(
+                OAuth2AccessToken.TokenType.BEARER, // 토큰 타입
+                accessToken, // 토큰 값
+                Instant.now(), // 발행일
+                Instant.MAX // 만료일
+        );
+        Map<String, Object> idTokenClaims = Map.of(
+                IdTokenClaimNames.ISS, "http://localhost:18080/realms/master",
+                IdTokenClaimNames.SUB, "OIDC0",
+                "preferred_username", "username"
+        );
+        OidcIdToken oidcIdToken = new OidcIdToken(
+                idToken, // 토큰 값
+                Instant.now(), // 발행일
+                Instant.MAX, // 만료일
+                idTokenClaims
+        );
+        OidcUserRequest oidcUserRequest = new OidcUserRequest(clientRegistration, oAuth2AccessToken, oidcIdToken);
+        OidcUserService userService = new OidcUserService();
+        return userService.loadUser(oidcUserRequest);
     }
 }
