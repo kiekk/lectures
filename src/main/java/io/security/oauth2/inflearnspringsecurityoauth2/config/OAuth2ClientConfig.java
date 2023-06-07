6a44c3c61fd405eba44e3f10a366f8ad14c80bf1
shyoon <shyoon991@gmail.com>
2023-06-07 12:15:36 +0900

    이전 예제 코드 제거

--- src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/config/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/config/OAuth2ClientConfig.java
@@ -1,36 +1,19 @@
 package io.security.oauth2.inflearnspringsecurityoauth2.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class OAuth2ClientConfig {
 
-    @Autowired
-    private ClientRegistrationRepository clientRegistrationRepository;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeRequests((authz) -> {
-                    authz.antMatchers("/home").permitAll();
                     authz.anyRequest().authenticated();
-                })
-                .oauth2Login(oauth2Login -> {
-                    oauth2Login.authorizationEndpoint(authEndpoint -> {
-                        authEndpoint.authorizationRequestResolver(customOAuthRequestResolver());
-                    });
                 })
                 .build();
     }
-
-    private OAuth2AuthorizationRequestResolver customOAuthRequestResolver() {
-        return new CustomOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization");
-    }
-
 }
