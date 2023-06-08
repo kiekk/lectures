a326bf0ae67b89f03bc3718f7f2081657d4ab51c
shyoon <shyoon991@gmail.com>
2023-06-08 09:00:24 +0900

    DefaultOAuth2AuthorizedClientManager - Resource Owner Password 권한 부여 구현하기(1)

--- src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/config/AppConfig.java
+++ src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/config/AppConfig.java
@@ -2,18 +2,23 @@ package io.security.oauth2.inflearnspringsecurityoauth2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.*;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public OAuth2AuthorizedClientManager authorizedClientManager(ClientRegistrationRepository clientRegistrationRepository,
+    public DefaultOAuth2AuthorizedClientManager authorizedClientManager(ClientRegistrationRepository clientRegistrationRepository,
                                                                  OAuth2AuthorizedClientRepository authorizedClientRepository) {
 
         OAuth2AuthorizedClientProvider authorizedClientProvider =
@@ -28,8 +33,29 @@ public class AppConfig {
                 new DefaultOAuth2AuthorizedClientManager(
                         clientRegistrationRepository, authorizedClientRepository);
         authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+        authorizedClientManager.setContextAttributesMapper(contextAttributesMapper());
 
         return authorizedClientManager;
+    }
+
+    private Function<OAuth2AuthorizeRequest, Map<String, Object>> contextAttributesMapper() {
+        return oAuth2AuthorizeRequest -> {
+            // request 정보 가져오기
+            HttpServletRequest request = oAuth2AuthorizeRequest.getAttribute(HttpServletRequest.class.getName());
+
+            if (request != null) {
+                String username = request.getParameter(OAuth2ParameterNames.USERNAME);
+                String password = request.getParameter(OAuth2ParameterNames.PASSWORD);
+
+                if (StringUtils.hasText(username) && StringUtils.hasText(password)) {
+                    return Map.of(
+                            OAuth2AuthorizationContext.USERNAME_ATTRIBUTE_NAME, username,
+                            OAuth2AuthorizationContext.PASSWORD_ATTRIBUTE_NAME, password
+                    );
+                }
+            }
+            return Collections.emptyMap();
+        };
     }
 
 }
--- src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/controller/LoginController.java
+++ src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/controller/LoginController.java
@@ -1,6 +1,11 @@
 package io.security.oauth2.inflearnspringsecurityoauth2.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +16,23 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class LoginController {
 
+    @Autowired
+    private DefaultOAuth2AuthorizedClientManager oAuth2AuthorizedClientManager;
+
     @GetMapping("oauth2Login")
-    public String oauth2Login() {
+    public String oauth2Login(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        OAuth2AuthorizeRequest auth2AuthorizeRequest = OAuth2AuthorizeRequest
+                .withClientRegistrationId("keycloak")
+                .principal(authentication)
+                .attribute(HttpServletRequest.class.getName(), request)
+                .attribute(HttpServletResponse.class.getName(), response)
+                .build();
+        OAuth2AuthorizedClient authorize = oAuth2AuthorizedClientManager.authorize(auth2AuthorizeRequest);
+
+        if (authorize != null) {
+
+        }
         return "redirect:/";
     }
 
