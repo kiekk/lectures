d6c4c2181772b0dcde4782800221ff066c27c52c
shyoon <shyoon991@gmail.com>
2023-06-14 14:54:44 +0900

    DefaultOAuth2AuthorizedClientManager -필터 기반으로 구현하기

--- src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/config/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/config/OAuth2ClientConfig.java
@@ -1,27 +1,43 @@
 package io.security.oauth2.inflearnspringsecurityoauth2.config;
 
+import io.security.oauth2.inflearnspringsecurityoauth2.filter.CustomOAuth2AuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration(proxyBeanMethods = false)
 public class OAuth2ClientConfig {
+
+    @Autowired
+    private DefaultOAuth2AuthorizedClientManager oAuth2AuthorizedClientManager;
+
+    @Autowired
+    private OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository;
 
     @Bean
     SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeRequests((requests) -> {
-                    requests.antMatchers("/", "/oauth2Login", "/client", "/logout").permitAll();
+                    requests.antMatchers("/", "/oauth2Login", "/client").permitAll();
                     requests.anyRequest().authenticated();
                 })
                 .oauth2Client()
                 .and()
-                .logout(logout -> {
-                    logout.invalidateHttpSession(true);
-                    logout.deleteCookies("JSESSIONID");
-                    logout.clearAuthentication(true);
-                })
+                .addFilterBefore(customOAuth2AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    private CustomOAuth2AuthenticationFilter customOAuth2AuthenticationFilter() {
+        CustomOAuth2AuthenticationFilter customOAuth2AuthenticationFilter = new CustomOAuth2AuthenticationFilter(oAuth2AuthorizedClientManager, oAuth2AuthorizedClientRepository);
+        customOAuth2AuthenticationFilter.setAuthenticationSuccessHandler((request, response, authentication) -> {
+            response.sendRedirect("/home");
+        });
+        return customOAuth2AuthenticationFilter;
     }
 }
