df34664f4e1384402b9133b27d7fdc0ac80e887c
shyoon <shyoon991@gmail.com>
2023-06-16 09:31:25 +0900

    add interface ProviderUser - Common User Interface

---
+++ src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/entity/ProviderUser.java
@@ -0,0 +1,22 @@
+package io.security.oauth2.inflearnspringsecurityoauth2.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ProviderUser {
+    String getId();
+
+    String getUsername();
+
+    String getPassword();
+
+    String getEmail();
+
+    String getProvider();
+
+    List<? extends GrantedAuthority> getAuthorities();
+
+    Map<String, Object> getAttributes();
+}
