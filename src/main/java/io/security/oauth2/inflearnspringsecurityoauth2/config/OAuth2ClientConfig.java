2c9bf705b14c1c801e4b0e0d42641843a31d81f6
shyoon <shyoon991@gmail.com>
2023-06-16 09:15:45 +0900

    edit config OAuth2 Config

--- src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/config/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/config/OAuth2ClientConfig.java
@@ -1,42 +1,29 @@
 package io.security.oauth2.inflearnspringsecurityoauth2.config;
 
-import io.security.oauth2.inflearnspringsecurityoauth2.filter.CustomOAuth2AuthenticationFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration(proxyBeanMethods = false)
 public class OAuth2ClientConfig {
 
-    @Autowired
-    private DefaultOAuth2AuthorizedClientManager oAuth2AuthorizedClientManager;
-
-    @Autowired
-    private OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository;
-
     @Bean
     SecurityFilterChain oauth2SecurityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests((requests) -> {
-                    requests.requestMatchers("/", "/oauth2Login", "/client", "/v2/oauth2Login").permitAll();
-                    requests.anyRequest().authenticated();
+                .authorizeHttpRequests((authz) -> {
+                    authz
+                            // static resources setting
+                            .requestMatchers("/static/**").permitAll()
+                            .requestMatchers("/").permitAll()
+                            .anyRequest().authenticated();
                 })
-                .oauth2Client()
-                .and()
-                .addFilterBefore(customOAuth2AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .oauth2Login(Customizer.withDefaults())
+                .logout(logout -> {
+                    logout.logoutSuccessUrl("/");
+                })
                 .build();
     }
 
-    private CustomOAuth2AuthenticationFilter customOAuth2AuthenticationFilter() {
-        CustomOAuth2AuthenticationFilter customOAuth2AuthenticationFilter = new CustomOAuth2AuthenticationFilter(oAuth2AuthorizedClientManager, oAuth2AuthorizedClientRepository);
-        customOAuth2AuthenticationFilter.setAuthenticationSuccessHandler((request, response, authentication) -> {
-            response.sendRedirect("/home");
-        });
-        return customOAuth2AuthenticationFilter;
-    }
 }
