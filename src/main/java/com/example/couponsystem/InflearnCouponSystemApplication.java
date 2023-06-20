b1115a76ceb232745124003c3699b8d2ad2fb5de
shyoon <shyoon991@gmail.com>
2023-06-21 01:35:10 +0900

    Initial Commit

---
+++ src/main/java/com/example/couponsystem/InflearnCouponSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.couponsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnCouponSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnCouponSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/couponsystem/InflearnCouponSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.couponsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnCouponSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
