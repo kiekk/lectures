4834545b1d713101745a871478eda8dfdcb3e48b
shyoon <shyoon991@gmail.com>
2023-06-23 16:45:51 +0900

    Initial Commit

---
+++ src/main/java/com/example/actuatordemo/InflearnSpringBootActuatorForBeginnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.actuatordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringBootActuatorForBeginnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnSpringBootActuatorForBeginnerApplication.class, args);
+    }
+
+}
