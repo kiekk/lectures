6bfbbe0bbff6b04e24c45f7be7837f754f3da921
shyoon <shyoon991@gmail.com>
2023-06-23 10:23:03 +0900

    feat: 응모 기능 문제점 - 동시 접근 시 의도한대로 동작하지 않음 (raceCondition 발생)

--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
@@ -22,6 +26,31 @@ class ApplyServiceTest {
 
         long count = couponRepository.count();
         assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    void 여러명응모() throws InterruptedException {
+        // 동시 접근 시 의도한대로 동작하지 않음
+        int threadCount = 1_000;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+
+        long count = couponRepository.count();
+        assertThat(count).isEqualTo(100);
     }
 
 }
