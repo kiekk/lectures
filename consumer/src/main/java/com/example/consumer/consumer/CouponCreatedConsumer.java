0d254f0036a36f2b0d80148985caef16bf1e6b65
shyoon <shyoon991@gmail.com>
2023-06-27 01:35:10 +0900

    feat: add kafka consumer - coupon create consumer

---
+++ consumer/src/main/java/com/example/consumer/consumer/CouponCreatedConsumer.java
@@ -0,0 +1,13 @@
+package com.example.consumer.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CouponCreatedConsumer {
+
+    @KafkaListener(topics = "coupon-create", groupId = "group_1")
+    public void listener(Long userId) {
+        System.out.println(userId);
+    }
+}
