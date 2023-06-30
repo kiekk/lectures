df8037af53cd5c109f4b5278e87e99d1132566d6
shyoon <shyoon991@gmail.com>
2023-07-01 01:35:10 +0900

    feat: AppliedUserRepository 사용

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,8 +1,8 @@
 package com.example.api.service;
 
 import com.example.api.kafka.producer.CouponCreateProducer;
+import com.example.api.repository.AppliedUserRepository;
 import com.example.api.repository.CouponCountRepository;
-import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,14 +10,22 @@ public class ApplyService {
 
     private final CouponCountRepository couponCountRepository;
     private final CouponCreateProducer couponCreateProducer;
+    private final AppliedUserRepository appliedUserRepository;
 
-    public ApplyService(CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
+    public ApplyService(CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer, AppliedUserRepository appliedUserRepository) {
         this.couponCountRepository = couponCountRepository;
         this.couponCreateProducer = couponCreateProducer;
+        this.appliedUserRepository = appliedUserRepository;
     }
 
     public void apply(Long userId) {
-        // redis 로 대체
+        Long apply = appliedUserRepository.add(userId);
+
+        // 이미 발급된 유저
+        if (apply != 1) {
+            return;
+        }
+
         Long count = couponCountRepository.increment();
 
         // 발급 가능 개수 확인
