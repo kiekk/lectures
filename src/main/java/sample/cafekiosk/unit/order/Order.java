8ba7455e5dfe6017e22a55aea9b777da8753c599
shyoon <shyoon991@gmail.com>
2023-07-03 16:11:18 +0900

    feat: add class Order

---
+++ src/main/java/sample/cafekiosk/unit/order/Order.java
@@ -0,0 +1,17 @@
+package sample.cafekiosk.unit.order;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import sample.cafekiosk.unit.beverage.Beverage;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class Order {
+    private final LocalDateTime orderDateTime;
+    private final List<Beverage> beverages = new ArrayList<>();
+
+}
