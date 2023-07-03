b9e74960ca522a212ac7157e21423480733b33e7
shyoon <shyoon991@gmail.com>
2023-07-03 16:09:10 +0900

    feat: implement function - add remove clear calculateTotalPrice

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -1,11 +1,25 @@
 package sample.cafekiosk.unit;
 
-public class CafeKiosk {
-    public void add(Beverage beverage) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class CafeKiosk {
+
+    private final List<Beverage> beverages = new ArrayList<>();
+
+    public void add(Beverage beverage) {
+        beverages.add(beverage);
+    }
+
+    public void remove(Beverage beverage) {
+        beverages.remove(beverage);
+    }
+
+    public void clear() {
+        beverages.clear();
     }
 
     public int calculateTotalPrice() {
-        return 0;
+        return beverages.stream().mapToInt(Beverage::getPrice).sum();
     }
 }
