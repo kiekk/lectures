ea1f3bd26e485ec745bb0a65d5f9a0314bb74d4c
shyoon <shyoon991@gmail.com>
2023-07-03 16:38:36 +0900

    rename: add → add_manual_test

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -3,12 +3,10 @@ package sample.cafekiosk.unit;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverage.Americano;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class CafeKioskTest {
 
     @Test
-    void add() {
+    void add_manual_test() {
         CafeKiosk cafeKiosk = new CafeKiosk();
         cafeKiosk.add(new Americano());
 
