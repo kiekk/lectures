127577586d631070cd62256aae21c6f60f6c23c9
shyoon <shyoon991@gmail.com>
2023-07-04 11:59:18 +0900

    feat: add entity Order

---
+++ src/main/java/sample/cafekiosk/spring/domain/order/Order.java
@@ -0,0 +1,26 @@
+package sample.cafekiosk.spring.domain.order;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    private int totalPrice;
+
+    private LocalDateTime registeredDateTime;
+}
