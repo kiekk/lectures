d283cd242c32fd313ca0a1582071b17aa609b152
shyoon <shyoon991@gmail.com>
2023-07-04 16:09:09 +0900

    feat: 중복되는 상품번호에 대한 주문 생성 테스트 - 1. Red (실패)

--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -62,6 +62,37 @@ class OrderServiceTest {
                 );
     }
 
+    @DisplayName("중복되는 상품번호 리스트로 주문을 생성할 수 있다.")
+    @Test
+    void createOrderWithDuplicateProductNumbers() {
+        // given - 테스트를 위한 준비 단계
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+        Product product1 = createProduct(HANDMADE, "001", 1_000, SELLING, "아메리카노");
+        Product product2 = createProduct(HANDMADE, "002", 3_000, SELLING, "에스프레소");
+        Product product3 = createProduct(HANDMADE, "003", 5_000, SELLING, "카페라뗴");
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001"))
+                .build();
+
+        // when - 실제 테스트
+        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+
+        // then - 테스트 결과 검증
+        assertThat(orderResponse).isNotNull();
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredDateTime, 2_000);
+        assertThat(orderResponse.getProducts())
+                .hasSize(2)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 1_000),
+                        tuple("001", 1_000)
+                );
+    }
+
     private Product createProduct(ProductType type, String productNumber, int price, ProductSellingStatus status, String name) {
         return Product.builder()
                 .productNumber(productNumber)
