ac0b5c41b952055488937395d82154c4cdbcaa2c
shyoon <shyoon991@gmail.com>
2023-07-05 10:22:27 +0900

    feat: 상품 첫 등록 기능 추가 테스트 - 2. Green (성공)

--- src/main/java/sample/cafekiosk/spring/api/service/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/ProductService.java
@@ -2,6 +2,7 @@ package sample.cafekiosk.spring.api.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
@@ -35,6 +36,10 @@ public class ProductService {
     }
 
     private String createNextProductNumber() {
-        return String.format("%03d", Integer.parseInt(productRepository.findLatestProductNumber()) + 1);
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if (!StringUtils.hasText(latestProductNumber)) {
+            return "001";
+        }
+        return String.format("%03d", Integer.parseInt(latestProductNumber) + 1);
     }
 }
