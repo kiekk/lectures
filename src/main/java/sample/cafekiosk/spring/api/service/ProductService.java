2b3d3e9f96c4148b7cbecd43f2b8437daee4d66f
shyoon <shyoon991@gmail.com>
2023-07-05 10:10:05 +0900

    feat: 상품 등록 기능 추가 테스트 - 1. Red (실패)

--- src/main/java/sample/cafekiosk/spring/api/service/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/ProductService.java
@@ -2,6 +2,7 @@ package sample.cafekiosk.spring.api.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
@@ -19,5 +20,14 @@ public class ProductService {
                 .findAllBySellingStatusIn(ProductSellingStatus.forDisplay())
                 .stream().map(ProductResponse::of)
                 .toList();
+    }
+
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        // generate productNumber
+        // DB 에서 마지막 저장된 Product 의 상품 번호를 읽어와서 +1
+        // ex: 001 002 003 004
+        String latestProductNumber = productRepository.findLatestProductNumber();
+
+        return null;
     }
 }
---
+++ src/test/java/sample/cafekiosk/spring/api/service/ProductServiceTest.java
@@ -0,0 +1,62 @@
+package sample.cafekiosk.spring.api.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
+import sample.cafekiosk.spring.api.service.response.ProductResponse;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
+import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.domain.product.ProductType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
+import static sample.cafekiosk.spring.domain.product.ProductType.BOTTLE;
+import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
+
+@SpringBootTest
+class ProductServiceTest {
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @DisplayName("신규 상품을 등록한다. 상품번호는 가장 최근 상품의 상품번호에서 1 증가한 값이다.")
+    @Test
+    void createProduct() {
+        // given
+        Product product = createProduct(BOTTLE, "001", 1_000, SELLING, "아메리카노");
+        productRepository.save(product);
+
+        ProductCreateRequest request = ProductCreateRequest.builder()
+                .type(HANDMADE)
+                .sellingStatus(SELLING)
+                .name("카푸치노")
+                .price(5_000)
+                .build();
+
+        // when
+        ProductResponse productResponse = productService.createProduct(request);
+
+        // then
+        assertThat(productResponse)
+                .extracting("productNUmber", "type", "sellingStatus", "price", "name")
+                .contains("002", HANDMADE, SELLING, "카푸치노", 5_000);
+
+    }
+
+    private Product createProduct(ProductType type, String productNumber, int price, ProductSellingStatus status, String name) {
+        return Product.builder()
+                .productNumber(productNumber)
+                .type(type)
+                .sellingStatus(status)
+                .name(name)
+                .price(price)
+                .build();
+    }
+
+}
