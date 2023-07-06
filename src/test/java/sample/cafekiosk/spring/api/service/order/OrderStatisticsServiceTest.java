ed43d3ad4d4dd53e0c28e7a7918311d9ede87909
shyoon <shyoon991@gmail.com>
2023-07-06 14:41:22 +0900

    refactor: @MockBean 상위 클래스로 추출 - @MockBean이 있으면 테스트 환경(Spring Boot)를 다시 실행해 설정함 (권장하지는 않음)
    다른 테스트 서비스에도 @MockBean이 주입되기 때문

--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderStatisticsServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderStatisticsServiceTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import sample.cafekiosk.spring.IntegrationTestSupport;
-import sample.cafekiosk.spring.client.mail.MailSendClient;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
 import sample.cafekiosk.spring.domain.order.Order;
@@ -43,9 +41,6 @@ class OrderStatisticsServiceTest extends IntegrationTestSupport {
 
     @Autowired
     private OrderProductRepository orderProductRepository;
-
-    @MockBean
-    private MailSendClient mailSendClient;
 
     @AfterEach
     void tearDown() {
