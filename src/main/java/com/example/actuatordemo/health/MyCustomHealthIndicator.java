8f734e901414d0d907d9db15236446b937d05b9a
shyoon <shyoon991@gmail.com>
2023-07-07 10:33:53 +0900

    feat: 상태에 따라 headl 정보 변경

--- src/main/java/com/example/actuatordemo/health/MyCustomHealthIndicator.java
+++ src/main/java/com/example/actuatordemo/health/MyCustomHealthIndicator.java
@@ -8,9 +8,20 @@ import org.springframework.stereotype.Component;
 public class MyCustomHealthIndicator implements HealthIndicator {
     @Override
     public Health health() {
-        return Health.up()
-                .withDetail("key1", "value1")
-                .withDetail("key2", "value2")
+        boolean isUp = getStatus();
+        if (isUp) {
+            return Health.up()
+                    .withDetail("key1", "value1")
+                    .withDetail("key2", "value2")
+                    .build();
+        }
+        return Health.down()
+                .withDetail("key3", "value3")
+                .withDetail("key4", "value4")
                 .build();
+    }
+
+    boolean getStatus() {
+        return System.currentTimeMillis() % 2 == 0;
     }
 }
