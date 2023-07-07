0c5ce899db469dad1c8fcf78b766e46505d7a6ec
shyoon <shyoon991@gmail.com>
2023-07-07 15:56:02 +0900

    feat: custom counter metrics

---
+++ src/main/java/com/example/actuatordemo/counter/CounterController.java
@@ -0,0 +1,23 @@
+package com.example.actuatordemo.counter;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("counter")
+public class CounterController {
+
+    private final MyHttpRequestManager myHttpRequestManager;
+
+    public CounterController(MyHttpRequestManager myHttpRequestManager) {
+        this.myHttpRequestManager = myHttpRequestManager;
+    }
+
+    @GetMapping("req")
+    public String req() {
+        myHttpRequestManager.increase();
+        return "ok";
+    }
+
+}
