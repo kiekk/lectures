f95c70f772eeb3986b101263166efcc8b69e1ed0
shyoon <shyoon991@gmail.com>
2023-07-10 08:40:17 +0900

    feat: FunctionTimer

---
+++ src/main/java/com/example/actuatordemo/timer/FunctionTimerConfig.java
@@ -0,0 +1,21 @@
+package com.example.actuatordemo.timer;
+
+import io.micrometer.core.instrument.FunctionTimer;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class FunctionTimerConfig {
+
+    @Bean
+    public FunctionTimer myFunctionTimer(MyTimerManager myTimerManager, MeterRegistry meterRegistry) {
+        return FunctionTimer.builder("my.timer4",
+                myTimerManager,
+                MyTimerManager::getCount,
+                MyTimerManager::getTotalTime,
+                TimeUnit.SECONDS).register(meterRegistry);
+    }
+}
