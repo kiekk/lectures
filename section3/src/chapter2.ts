2dbf0e7ead32614078c9caf33c432b7ef9c114ac
shyoon <shyoon991@gmail.com>
2023-07-13 09:47:51 +0900

    section3: 타입 계층도 - never

--- section3/src/chapter2.ts
+++ section3/src/chapter2.ts
@@ -20,3 +20,24 @@ function unknownExam() {
     // let str: string = unknownVar // 오류
     // let bool: boolean = unknownVar // 오류
 }
+
+/**
+ * Never 타입
+ */
+
+function neverExam() {
+    function neverFunc(): never {
+        while (true) {
+        }
+    }
+
+    // never 타입은 모든 타입의 서브 타입이므로 업 캐스팅이 가능하므로 할당 가능
+    let num: number = neverFunc()
+    let str: string = neverFunc()
+    let bool: boolean = neverFunc()
+
+    // 다운 캐스팅은 불가능
+    // let never1: never = 10 // 오류
+    // let never2: never = 'string' // 오류
+    // let never3: never = true // 오류
+}
