3b06350d184b079d6658038fd3026396635217b0
shyoon <shyoon991@gmail.com>
2023-07-17 15:55:56 +0900

    feat: 페이스북 소셜 로그인 기능 추가

--- src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/converters/DelegatingProviderUserConverter.java
+++ src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/converters/DelegatingProviderUserConverter.java
@@ -15,7 +15,8 @@ public class DelegatingProviderUserConverter<T, R> implements ProviderUserConver
         this.converters = List.of(
                 new OAuth2GoogleProviderUserConverter(),
                 new OAuth2NaverProviderUserConverter(),
-                new OAuth2KakaoProviderUserConverter()
+                new OAuth2KakaoProviderUserConverter(),
+                new OAuth2FacebookProviderUserConverter()
         );
     }
 
--- src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/entity/social/FacebookUser.java
+++ src/main/java/io/security/oauth2/inflearnspringsecurityoauth2/entity/social/FacebookUser.java
@@ -1,23 +1,24 @@
 package io.security.oauth2.inflearnspringsecurityoauth2.entity.social;
 
+import io.security.oauth2.inflearnspringsecurityoauth2.entity.Attributes;
 import io.security.oauth2.inflearnspringsecurityoauth2.entity.OAuth2ProviderUser;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 public class FacebookUser extends OAuth2ProviderUser {
 
-    public FacebookUser(OAuth2User oAuth2User, ClientRegistration clientRegistration) {
-        super(oAuth2User.getAttributes(), oAuth2User, clientRegistration);
+    public FacebookUser(Attributes mainAttributes, OAuth2User oAuth2User, ClientRegistration clientRegistration) {
+        super(mainAttributes.getMainAttributes(), oAuth2User, clientRegistration);
     }
 
     @Override
     public String getId() {
-        return (String) getAttributes().get("sub");
+        return (String) getAttributes().get("id");
     }
 
     @Override
     public String getUsername() {
-        return (String) getAttributes().get("sub");
+        return (String) getAttributes().get("name");
     }
 
     @Override
