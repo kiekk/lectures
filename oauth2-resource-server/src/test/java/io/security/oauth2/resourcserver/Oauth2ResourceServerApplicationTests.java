625b7d1217fa23081812464600fcbedbcb1f5971
shyoon <shyoon991@gmail.com>
2023-07-18 10:45:39 +0900

    OAuth 2.0 Resource Server 소개 및 프로젝트 생성

---
+++ oauth2-resource-server/src/test/java/io/security/oauth2/resourcserver/Oauth2ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.oauth2.resourcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
