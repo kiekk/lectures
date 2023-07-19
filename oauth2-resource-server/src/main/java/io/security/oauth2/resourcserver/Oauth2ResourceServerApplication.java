f7360904487603c03e8c1b9e8078c29190507ee6
shyoon <shyoon991@gmail.com>
2023-07-19 09:15:19 +0900

    JCA & JCE - 소개 및 구조

--- oauth2-resource-server/src/main/java/io/security/oauth2/resourcserver/Oauth2ResourceServerApplication.java
+++ oauth2-resource-server/src/main/java/io/security/oauth2/resourcserver/Oauth2ResourceServerApplication.java
@@ -1,13 +1,17 @@
 package io.security.oauth2.resourcserver;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static io.security.oauth2.resourcserver.jca.MessageDigestTest.messageDigest;
+import static io.security.oauth2.resourcserver.jca.SignatureTest.signature;
 
 @SpringBootApplication
 public class Oauth2ResourceServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Oauth2ResourceServerApplication.class, args);
+    public static void main(String[] args) throws Exception {
+//        messageDigest("Spring Security");
+        signature("Spring Security");
+//        SpringApplication.run(Oauth2ResourceServerApplication.class, args);
     }
 
 }
