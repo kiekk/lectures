d1133a73926d2f83f0303f2d485ccd3409529ac6
shyoon <shyoon991@gmail.com>
2023-07-19 11:29:53 +0900

    JCA & JCE - 대칭키 & 비대칭키

--- oauth2-resource-server/src/main/java/io/security/oauth2/resourcserver/Oauth2ResourceServerApplication.java
+++ oauth2-resource-server/src/main/java/io/security/oauth2/resourcserver/Oauth2ResourceServerApplication.java
@@ -2,7 +2,9 @@ package io.security.oauth2.resourcserver;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static io.security.oauth2.resourcserver.jca.MacTest.hmac;
 import static io.security.oauth2.resourcserver.jca.MessageDigestTest.messageDigest;
+import static io.security.oauth2.resourcserver.jca.RSATest.rsa;
 import static io.security.oauth2.resourcserver.jca.SignatureTest.signature;
 
 @SpringBootApplication
@@ -10,7 +12,9 @@ public class Oauth2ResourceServerApplication {
 
     public static void main(String[] args) throws Exception {
 //        messageDigest("Spring Security");
-        signature("Spring Security");
+//        signature("Spring Security");
+        hmac("Spring Security");
+        rsa("Spring Security");
 //        SpringApplication.run(Oauth2ResourceServerApplication.class, args);
     }
 
