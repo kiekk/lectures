0c777e2c7d3114e02eba983d851b6f73eb380eba
shyoon <shyoon991@gmail.com>
2023-07-19 14:11:37 +0900

    이전 예제 코드 제거

--- oauth2-resource-server/src/main/java/io/security/oauth2/resourcserver/Oauth2ResourceServerApplication.java
+++ oauth2-resource-server/src/main/java/io/security/oauth2/resourcserver/Oauth2ResourceServerApplication.java
@@ -1,15 +1,13 @@
 package io.security.oauth2.resourcserver;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import static io.security.oauth2.resourcserver.jwk.JWKTest.jwk;
 
 @SpringBootApplication
 public class Oauth2ResourceServerApplication {
 
     public static void main(String[] args) throws Exception {
-        jwk();
-//        SpringApplication.run(Oauth2ResourceServerApplication.class, args);
+        SpringApplication.run(Oauth2ResourceServerApplication.class, args);
     }
 
 }
--- oauth2-resource-server/src/main/java/io/security/oauth2/resourcserver/config/OAuthResourceServerConfig.java
+++ oauth2-resource-server/src/main/java/io/security/oauth2/resourcserver/config/OAuthResourceServerConfig.java
@@ -1,50 +1,21 @@
 package io.security.oauth2.resourcserver.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration(proxyBeanMethods = false)
 public class OAuthResourceServerConfig {
 
-    @Autowired
-    private OAuth2ResourceServerProperties properties;
-
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .authorizeHttpRequests(authz -> {
+                    authz
+                            .requestMatchers("/").permitAll()
+                            .anyRequest().authenticated();
+                })
                 .build();
     }
-
-    @Bean
-//    @Primary
-    JwtDecoder jwtDecoder1() {
-        return JwtDecoders.fromIssuerLocation(properties.getJwt().getIssuerUri());
-    }
-
-    @Bean
-//    @Primary
-    JwtDecoder jwtDecoder2() {
-        return JwtDecoders.fromOidcIssuerLocation(properties.getJwt().getIssuerUri());
-    }
-
-    @Bean
-    @Primary
-    JwtDecoder jwtDecoder3() {
-        return NimbusJwtDecoder.withJwkSetUri(properties.getJwt().getJwkSetUri())
-                .jwsAlgorithm(SignatureAlgorithm.RS512)
-                .build();
-    }
-
 }
