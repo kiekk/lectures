ede2d3a5cf84cf972962985bce567f30a177450a
shyoon <shyoon991@gmail.com>
2023-07-24 14:39:46 +0900

    토큰 검사 및 프로세스 이해

--- oauth2-resource-server/src/main/java/io/security/oauth2/resourcserver/config/OAuthResourceServerConfig.java
+++ oauth2-resource-server/src/main/java/io/security/oauth2/resourcserver/config/OAuthResourceServerConfig.java
@@ -1,18 +1,21 @@
 package io.security.oauth2.resourcserver.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.server.resource.introspection.NimbusOpaqueTokenIntrospector;
 import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
 public class OAuthResourceServerConfig {
+
+    @Autowired
+    private OAuth2ResourceServerProperties properties;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -26,10 +29,8 @@ public class OAuthResourceServerConfig {
     }
 
     @Bean
-    OpaqueTokenIntrospector opaqueTokenIntrospector(OAuth2ResourceServerProperties properties) {
-        OAuth2ResourceServerProperties.Opaquetoken opaquetoken = properties.getOpaquetoken();
-        return new NimbusOpaqueTokenIntrospector(opaquetoken.getIntrospectionUri(), opaquetoken.getClientId(), opaquetoken.getClientSecret());
-
+    OpaqueTokenIntrospector opaqueTokenIntrospector() {
+        return new CustomOpaqueTokenIntrospector(properties);
     }
 
 }
