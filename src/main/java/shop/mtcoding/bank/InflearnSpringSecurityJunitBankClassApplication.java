457482876aceaad0ca006b94e4d630340fdf2f4a
soonho <shyoon991@gmail.com>
2023-09-14 19:09:34 +0900

    Initial Commit

---
+++ src/main/java/shop/mtcoding/bank/InflearnSpringSecurityJunitBankClassApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringSecurityJunitBankClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnSpringSecurityJunitBankClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shop/mtcoding/bank/InflearnSpringSecurityJunitBankClassApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringSecurityJunitBankClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
