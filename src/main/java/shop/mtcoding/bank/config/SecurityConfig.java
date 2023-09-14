c81028ce3c091d61e303279c1ae5eb56f52ca742
soonho <shyoon991@gmail.com>
2023-09-14 20:58:48 +0900

    #8 - Security 기본 설정
    headers, csrf, cors 그리고 sessionManagement, formLogin, httpBasic 설정 추가

---
+++ src/main/java/shop/mtcoding/bank/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package shop.mtcoding.bank.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import shop.mtcoding.bank.domain.user.UserEnum;
+
+@Configuration
+public class SecurityConfig {
+
+    private final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        log.debug("** securityFilterChain called **");
+        return http
+                .headers(headers ->
+                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(configurationSource()))
+                .authorizeHttpRequests(authz ->
+                        authz.requestMatchers("/api/s/**").authenticated()
+                                .requestMatchers("/api/admin/**").hasRole(UserEnum.ADMIN.getValue())
+                                .anyRequest().permitAll()
+                )
+                .sessionManagement(
+                        sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        log.debug("** passwordEncoder called **");
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CorsConfigurationSource configurationSource() {
+        log.debug("** configurationSource called **");
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*"); // GET, POST, PUT, DELETE (Javascript 요청 허용)
+        corsConfiguration.addAllowedOriginPattern("*"); // 모든 IP 주소 허용
+        corsConfiguration.setAllowCredentials(true); // 클라이언트에서 쿠키 요청 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
+}
