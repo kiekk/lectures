ab0947cf89d67b0a781cd5df1be6a2a7edfa8ade
soonho <shyoon991@gmail.com>
2023-09-14 20:23:50 +0900

    #5 - 회원 도메인 추가
    회원 도메인 작성 중 `createdAt`, `updatedAt` 필드에 `@CreatedDate`, `@LastModifiedDate` 를 적용하기 위해
    `@EnableJpaAuditing`, `@EntityListener` 를 같이 적용

---
+++ src/main/java/shop/mtcoding/bank/domain/User.java
@@ -0,0 +1,59 @@
+package shop.mtcoding.bank.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Table(name = "user_tb")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false, length = 60)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String fullname;
+
+    @Enumerated(EnumType.STRING)
+    private UserEnum role; // ADMIN, CUSTOMER
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public User(Long id, String username, String password, String email, String fullname, UserEnum role, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.fullname = fullname;
+        this.role = role;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+}
