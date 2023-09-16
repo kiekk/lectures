b02f87a704b409e9d7e335b9c8a83a7325a96bef
soonho <shyoon991@gmail.com>
2023-09-16 10:13:31 +0900

    #10 - 회원 가입 DTO JoinRequest에 유효성 검사 정규 표현식 적용 및 테스트

--- src/main/java/shop/mtcoding/bank/dto/user/UserRequest.java
+++ src/main/java/shop/mtcoding/bank/dto/user/UserRequest.java
@@ -1,6 +1,7 @@
 package shop.mtcoding.bank.dto.user;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
@@ -13,13 +14,19 @@ public class UserRequest {
     @Getter
     @Setter
     public static class JoinRequest {
+        @Pattern(regexp = "^[a-zA-Z0-9]{2,20}$", message = "영문/숫자 2~20자 이내로 작성해주세요.")
         @NotEmpty(message = "필수 값입니다.")
         private String username;
+
         @NotEmpty(message = "필수 값입니다.")
-        @Length(max = 20, message = "20자 이하여야 합니다.")
+        @Length(min = 4, max = 20, message = "4~20자여야 합니다.")
         private String password;
+
+        @Pattern(regexp = "^[a-zA-Z0-9]{2,10}@[a-zA-Z0-9]{2,6}\\.[a-zA-Z]{2,3}$", message = "이메일 형식으로 작성해주세요.")
         @NotEmpty(message = "필수 값입니다.")
         private String email;
+
+        @Pattern(regexp = "^[a-zA-Z가-힣]{1,20}$", message = "한글/영문 1~20자 이내로 작성해주세요.")
         @NotEmpty(message = "필수 값입니다.")
         private String fullname;
 
