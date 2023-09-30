3f5f5b95fb83ebdb6bf2a4cd53d3d05f514d3e5a
soonho <shyoon991@gmail.com>
2023-09-30 10:14:09 +0900

    #11 - `@DataJpaTest`로 데이터 조회 테스트
    현재 테스트의 경우 데이터는 테스트마다 잘 초기화 & 셋팅 되지만, `auto increment`는 초기화되지 않아 이 부분을 직접 처리해줘야 한다.

--- src/test/java/shop/mtcoding/bank/config/domain/transaction/TransactionRepositoryImplTest.java
+++ src/test/java/shop/mtcoding/bank/config/domain/transaction/TransactionRepositoryImplTest.java
@@ -1,8 +1,11 @@
 package shop.mtcoding.bank.config.domain.transaction;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 import shop.mtcoding.bank.config.dummy.DummyObject;
 import shop.mtcoding.bank.domain.account.Account;
 import shop.mtcoding.bank.domain.account.AccountRepository;
@@ -11,6 +14,9 @@ import shop.mtcoding.bank.domain.transaction.TransactionRepository;
 import shop.mtcoding.bank.domain.user.User;
 import shop.mtcoding.bank.domain.user.UserRepository;
 
+import java.util.List;
+
+@Sql("classpath:db/teardown.sql")
 @DataJpaTest
 public class TransactionRepositoryImplTest extends DummyObject {
 
@@ -26,6 +32,33 @@ public class TransactionRepositoryImplTest extends DummyObject {
     @BeforeEach
     void setUp() {
         dataSetting();
+    }
+
+    @DisplayName("간단한 데이터 조회 테스트 - 따로 검증은 하지 않는다.")
+    @Test
+    public void dataJpa_test1() {
+        List<Transaction> transactionList = transactionRepository.findAll();
+        transactionList.forEach((transaction) -> {
+            System.out.println("id : " + transaction.getId());
+            System.out.println("sender : " + transaction.getSender());
+            System.out.println("receiver : " + transaction.getReceiver());
+            System.out.println("gubun : " + transaction.getGubun());
+            System.out.println("테스트 : ========================");
+        });
+    }
+
+
+    @DisplayName("@BeforeEach 에서 더미 데이터 셋팅이 잘 되었는지, dataJpa_test1과 동일하게 데이터를 조회해본다.")
+    @Test
+    public void dataJpa_test2() {
+        List<Transaction> transactionList = transactionRepository.findAll();
+        transactionList.forEach((transaction) -> {
+            System.out.println("테스트 : " + transaction.getId());
+            System.out.println("테스트 : " + transaction.getSender());
+            System.out.println("테스트 : " + transaction.getReceiver());
+            System.out.println("테스트 : " + transaction.getGubun());
+            System.out.println("테스트 : ========================");
+        });
     }
 
     private void dataSetting() {
