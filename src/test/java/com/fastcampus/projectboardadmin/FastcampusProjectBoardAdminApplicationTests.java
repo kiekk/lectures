8bf54c05caf2d0e6de31c6f052f9d20df55b580e
soonho <shyoon991@gmail.com>
2023-10-12 20:46:12 +0900

    #3 - 테스트 파일 프로필 적용

--- src/test/java/com/fastcampus/projectboardadmin/FastcampusProjectBoardAdminApplicationTests.java
+++ src/test/java/com/fastcampus/projectboardadmin/FastcampusProjectBoardAdminApplicationTests.java
@@ -2,7 +2,9 @@ package com.fastcampus.projectboardadmin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class FastcampusProjectBoardAdminApplicationTests {
 
