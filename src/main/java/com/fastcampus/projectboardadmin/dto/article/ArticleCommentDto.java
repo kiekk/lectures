9d0e94fb0096f4ad1192cea467d883982c16f55f
soonho <shyoon991@gmail.com>
2023-10-16 19:43:24 +0900

    #15 - 댓글 관리 서비스의 비즈니스 로직 및 컨트롤러 테스트 작성

---
+++ src/main/java/com/fastcampus/projectboardadmin/dto/article/ArticleCommentDto.java
@@ -0,0 +1,23 @@
+package com.fastcampus.projectboardadmin.dto.article;
+
+import com.fastcampus.projectboardadmin.dto.UserAccountDto;
+
+import java.time.LocalDateTime;
+
+public record ArticleCommentDto(
+        Long id,
+        Long articleId,
+        UserAccountDto userAccount,
+        Long parentCommentId,
+        String content,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy
+) {
+
+    public static ArticleCommentDto of(Long id, Long articleId, UserAccountDto userAccountDto, Long parentCommentId, String content, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new ArticleCommentDto(id, articleId, userAccountDto, parentCommentId, content, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+}
