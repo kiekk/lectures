70ce59bc70e99ba9b0a8b5109278c42d7dd28228
soonho <shyoon991@gmail.com>
2023-10-26 07:35:10 +0900

    feat: common-module에 bean 작성 및 api module에서 bean 주입 받기 - 1

--- module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
+++ module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
@@ -3,7 +3,8 @@ package dev.be.moduleapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// 1. 커먼 모듈에 있는 빈들을 스캔할 수 있도록 커먼 모듈의 기본 패키지 경로를 추가
+@SpringBootApplication(scanBasePackages = {"dev.be.modulecommon", "dev.be.moduleapi"})
 public class ModuleApiApplication {
 
     public static void main(String[] args) {
