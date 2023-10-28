50961db883cec811f320f458e78a5cae2cd0adb8
soonho <shyoon991@gmail.com>
2023-10-28 15:20:10 +0900

    feat: common-module에 bean 작성 및 api module에서 bean 주입 받기 - 장단점 비교

--- module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
+++ module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
@@ -1,14 +1,17 @@
-//package dev.be.moduleapi;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//// 1. 커먼 모듈에 있는 빈들을 스캔할 수 있도록 커먼 모듈의 기본 패키지 경로를 추가
-//@SpringBootApplication(scanBasePackages = {"dev.be.modulecommon", "dev.be.moduleapi"})
-//public class ModuleApiApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(ModuleApiApplication.class, args);
-//    }
-//
-//}
+package dev.be.moduleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 실제 현업에서는 아래와 같은 방법을 주로 사용
+// 2번 방법을 사용하면 api 모듈이 의존하고 있는 모든 module에서 component scan을 하지만,
+// 명시적으로 component scan 범위를 지정하게 되면 원하는 module에서 component scan을 통해
+// 원하는 bean들만 의존성을 주입 받을 수 있다는 장점이 있습니다.
+@SpringBootApplication(scanBasePackages = {"dev.be.modulecommon", "dev.be.moduleapi"})
+public class ModuleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleApiApplication.class, args);
+    }
+
+}
