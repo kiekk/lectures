ddd6c1a434cbd55bb25766fc757caab0f1c28f65
soonho <shyoon991@gmail.com>
2023-10-29 21:35:09 +0900

    feat: 공통 예외 객체 테스트

--- module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
+++ module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -1,5 +1,6 @@
 package dev.be.moduleapi.service;
 
+import dev.be.moduleapi.exception.CustomException;
 import dev.be.modulecommon.enums.CodeEnum;
 import dev.be.modulecommon.service.CommonDemoService;
 import lombok.RequiredArgsConstructor;
@@ -21,4 +22,10 @@ public class DemoService {
         return "find";
     }
 
+    public String exception() {
+        if (true) {
+            throw new CustomException(CodeEnum.UNKNOWN_ERROR);
+        }
+        return "exception";
+    }
 }
