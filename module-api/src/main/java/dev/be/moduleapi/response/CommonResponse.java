af1b53a6738141237d8191a15b5de8a78754cf48
soonho <shyoon991@gmail.com>
2023-10-29 21:30:04 +0900

    feat: 응답 객체 공통 포맷 설정

---
+++ module-api/src/main/java/dev/be/moduleapi/response/CommonResponse.java
@@ -0,0 +1,32 @@
+package dev.be.moduleapi.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import dev.be.modulecommon.enums.CodeEnum;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@NoArgsConstructor
+public class CommonResponse<T> {
+    private String returnCode;
+    private String returnMessage;
+    private T info;
+
+    public CommonResponse(CodeEnum codeEnum) {
+        this(codeEnum, null);
+    }
+
+    public CommonResponse(T info) {
+        this(CodeEnum.SUCCESS, info);
+    }
+
+    public CommonResponse(CodeEnum codeEnum, T info) {
+        this.returnCode = codeEnum.getCode();
+        this.returnMessage = codeEnum.getMessage();
+        this.info = info;
+    }
+
+}
