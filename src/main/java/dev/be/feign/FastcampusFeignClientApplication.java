cb467d53edb26ad237a6665ae5a16dbf9eb2c588
soonho <shyoon991@gmail.com>
2023-10-30 20:37:33 +0900

    Initial Commit

---
+++ src/main/java/dev/be/feign/FastcampusFeignClientApplication.java
@@ -0,0 +1,13 @@
+package dev.be.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusFeignClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusFeignClientApplication.class, args);
+    }
+
+}
