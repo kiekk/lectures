be3508d1f0feb051a253652e04f2aca414345a48
shyoon <shyoon991@gmail.com>
2023-10-31 08:05:40 +0900

    Initial Commit

---
+++ src/main/java/com/shyoon/wms/SpringBootWmsApplication.java
@@ -0,0 +1,13 @@
+package com.shyoon.wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWmsApplication.class, args);
+    }
+
+}
