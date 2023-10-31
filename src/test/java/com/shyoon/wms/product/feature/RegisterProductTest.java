0a24ef2fa5e99b95d5ba3e8cf72db53615812ed1
shyoon <shyoon991@gmail.com>
2023-10-31 10:46:42 +0900

    create object RegisterProduct

--- src/test/java/com/shyoon/wms/product/feature/RegisterProductTest.java
+++ src/test/java/com/shyoon/wms/product/feature/RegisterProductTest.java
@@ -1,13 +1,23 @@
 package com.shyoon.wms.product.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RegisterProductTest {
 
+    private RegisterProduct registerProduct;
+
+    @BeforeEach
+    void setUp() {
+        registerProduct = new RegisterProduct();
+    }
+
     @Test
     @DisplayName("상품을 등록한다.")
     void registerProduct() {
+    }
 
+    private static class RegisterProduct {
     }
 }
