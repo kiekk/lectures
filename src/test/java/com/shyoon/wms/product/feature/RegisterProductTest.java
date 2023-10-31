3b98343a156779ec73ae7f8e9138f9173ffdb29c
shyoon <shyoon991@gmail.com>
2023-10-31 11:08:49 +0900

    create RegisterProduct

--- src/test/java/com/shyoon/wms/product/feature/RegisterProductTest.java
+++ src/test/java/com/shyoon/wms/product/feature/RegisterProductTest.java
@@ -16,8 +16,80 @@ class RegisterProductTest {
     @Test
     @DisplayName("상품을 등록한다.")
     void registerProduct() {
+
+        // given
+        final Long weightInGrams = 1000L;
+        final Long widthInMillimeters = 100L;
+        final Long heightInMillimeters = 100L;
+        final Long lengthInMillimeters = 100L;
+        final String name = "name";
+        final String code = "code";
+        final String description = "description";
+        final String brand = "brand";
+        final String maker = "maker";
+        final String origin = "origin";
+
+        RegisterProduct.Request request = new RegisterProduct.Request(
+                name,
+                code,
+                description,
+                brand,
+                maker,
+                origin,
+                Category.ELECTRONICS,
+                TemperatureZone.ROOM_TEMPERATURE,
+                weightInGrams, // gram
+                widthInMillimeters, // 너비 mm
+                heightInMillimeters, // 높이 mm
+                lengthInMillimeters // 길이 mm
+        );
+
+        // when
+        registerProduct.request(request);
+
+        // then
+//        assertThat(productRepository().findAll()).hasSize(1);
     }
 
-    private static class RegisterProduct {
+    public static class RegisterProduct {
+        public void request(Request request) {
+
+        }
+
+        public record Request(
+                String name,
+                String code,
+                String description,
+                String brand,
+                String maker,
+                String origin,
+                Category category,
+                TemperatureZone temperatureZone,
+                Long gram,
+                Long width,
+                Long height,
+                Long length
+        ) {
+        }
+    }
+
+    public enum Category {
+        ELECTRONICS("전자 제품");
+
+        private final String description;
+
+        Category(String description) {
+            this.description = description;
+        }
+    }
+
+    public enum TemperatureZone {
+        ROOM_TEMPERATURE("상온");
+
+        private final String description;
+
+        TemperatureZone(String description) {
+            this.description = description;
+        }
     }
 }
