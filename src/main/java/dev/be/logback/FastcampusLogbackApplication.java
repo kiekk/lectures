7aa92b4f7184c26cbd18033778d518da8004ed54
soonho <shyoon991@gmail.com>
2023-11-01 21:22:14 +0900

    Initial Commit

---
+++ src/main/java/dev/be/logback/FastcampusLogbackApplication.java
@@ -0,0 +1,13 @@
+package dev.be.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusLogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusLogbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/be/logback/FastcampusLogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.be.logback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusLogbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
