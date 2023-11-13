ad25598dec97817394f26d5290afd32995ee7bc0
shyoon <shyoon991@gmail.com>
2023-11-14 08:32:15 +0900

    feat: add validation to request (#10)

--- src/test/java/com/shyoon/wms/location/feature/AssignLocationLPNTest.java
+++ src/test/java/com/shyoon/wms/location/feature/AssignLocationLPNTest.java
@@ -3,6 +3,7 @@ package com.shyoon.wms.location.feature;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 class AssignLocationLPNTest {
 
@@ -34,6 +35,11 @@ class AssignLocationLPNTest {
         public record Request(
                 String locationBarcode,
                 String lpnBarcode) {
+
+            public Request {
+                Assert.hasText(locationBarcode, "로케이션 바코드는 필수입니다.");
+                Assert.hasText(lpnBarcode, "LPN 바코드는 필수입니다.");
+            }
         }
     }
 }
