bfdb0fcd547b54fae51a1b4af5f5a4f7642e97a7
shyoon <shyoon991@gmail.com>
2023-11-14 07:10:09 +0900

    feat: create inner class RegisterLocation (#9)

--- src/test/java/com/shyoon/wms/location/feature/RegisterLocationTest.java
+++ src/test/java/com/shyoon/wms/location/feature/RegisterLocationTest.java
@@ -1,12 +1,24 @@
 package com.shyoon.wms.location.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RegisterLocationTest {
+
+    private RegisterLocation registerLocation;
+
+    @BeforeEach
+    void setUp() {
+        registerLocation = new RegisterLocation();
+    }
+
     @Test
     @DisplayName("로케이션을 등록한다.")
     void registerLocation() {
+    }
+
+    private class RegisterLocation {
 
     }
 }
