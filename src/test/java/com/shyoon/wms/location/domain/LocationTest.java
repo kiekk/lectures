99c825824844f43b57e623862f466c67fe83234f
shyoon <shyoon991@gmail.com>
2023-11-14 08:50:33 +0900

    feat: create test class: LocationTest (#10)

---
+++ src/test/java/com/shyoon/wms/location/domain/LocationTest.java
@@ -0,0 +1,14 @@
+package com.shyoon.wms.location.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocationTest {
+
+    @Test
+    @DisplayName("로케이션에 LPN을 할당한다.")
+    void assignLPN() {
+    }
+}
