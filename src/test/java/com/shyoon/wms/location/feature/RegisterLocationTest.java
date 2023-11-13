cc2dfb0e119d7cd214cd455eca3bc8ad79b10606
shyoon <shyoon991@gmail.com>
2023-11-14 07:09:16 +0900

    feat: create test case RegisterLocation (#9)

--- src/test/java/com/shyoon/wms/location/feature/RegisterLocationTest.java
+++ src/test/java/com/shyoon/wms/location/feature/RegisterLocationTest.java
@@ -1,9 +1,11 @@
 package com.shyoon.wms.location.feature;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RegisterLocationTest {
     @Test
+    @DisplayName("로케이션을 등록한다.")
     void registerLocation() {
 
     }
