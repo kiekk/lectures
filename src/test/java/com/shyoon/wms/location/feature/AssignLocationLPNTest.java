2dff12f2f479cc7ae5952d62290b70de514a8ebd
shyoon <shyoon991@gmail.com>
2023-11-14 08:11:49 +0900

    feat: create test case AssignLocationLPN  (#10)

--- src/test/java/com/shyoon/wms/location/feature/AssignLocationLPNTest.java
+++ src/test/java/com/shyoon/wms/location/feature/AssignLocationLPNTest.java
@@ -1,4 +1,12 @@
 package com.shyoon.wms.location.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class AssignLocationLPNTest {
+    @Test
+    @DisplayName("로케이션에 LPN을 할당한다.")
+    void assignLocationLPN() {
+
+    }
 }
