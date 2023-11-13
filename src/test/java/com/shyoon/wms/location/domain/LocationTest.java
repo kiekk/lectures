f32924aba5f038da23af81198d7b2ebddee54830
shyoon <shyoon991@gmail.com>
2023-11-14 08:54:01 +0900

    feat: extract method (#10)

--- src/test/java/com/shyoon/wms/location/domain/LocationTest.java
+++ src/test/java/com/shyoon/wms/location/domain/LocationTest.java
@@ -12,22 +12,30 @@ class LocationTest {
     @Test
     @DisplayName("로케이션에 LPN을 할당한다.")
     void assignLPN() {
-        final String locationBarcode = "A-1-1";
-        final StorageType storageType = StorageType.TOTE;
-        final UsagePurpose usagePurpose = UsagePurpose.MOVE;
-        final Location location = new Location(
-                locationBarcode,
-                storageType,
-                usagePurpose
-        );
+        final Location location = createLocation();
 
+        final LPN lpn = createLPN();
+        location.assignLPN(lpn);
+    }
+
+    private static LPN createLPN() {
         final String lpnBarcode = "LPN-1";
         final LocalDateTime expirationAt = LocalDateTime.now().plusDays(1);
-        final LPN lpn = new LPN(
+        return new LPN(
                 lpnBarcode,
                 expirationAt,
                 InboundItemFixture.anInboundItem().build()
         );
-        location.assignLPN(lpn);
+    }
+
+    private static Location createLocation() {
+        final String locationBarcode = "A-1-1";
+        final StorageType storageType = StorageType.TOTE;
+        final UsagePurpose usagePurpose = UsagePurpose.MOVE;
+        return new Location(
+                locationBarcode,
+                storageType,
+                usagePurpose
+        );
     }
 }
