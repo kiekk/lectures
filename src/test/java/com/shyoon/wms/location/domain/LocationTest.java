21137288fa6f5899650038e0f28b0533b86e8f7e
shyoon <shyoon991@gmail.com>
2023-11-14 09:30:04 +0900

    feat: test case 추가 (#10)

--- src/test/java/com/shyoon/wms/location/domain/LocationTest.java
+++ src/test/java/com/shyoon/wms/location/domain/LocationTest.java
@@ -21,4 +21,17 @@ class LocationTest {
         assertThat(location.getLocationLPNList().get(0).getInventoryQuantity()).isEqualTo(1);
     }
 
+    @Test
+    @DisplayName("로케이션에 LPN을 할당한다. 이미 LPN이 존재하면 생성하지 않고 재고만 증가시킨다.")
+    void already_exists_assignLPN() {
+        final Location location = LocationFixture.aLocation().build();
+        final LPN lpn = LPNFixture.anLPN().build();
+
+        location.assignLPN(lpn);
+        location.assignLPN(lpn); // 로케이션을 생성하지 않고 재고 1 증가
+
+        assertThat(location.getLocationLPNList()).hasSize(1);
+        assertThat(location.getLocationLPNList().get(0).getInventoryQuantity()).isEqualTo(2);
+    }
+
 }
