7098bdb8bc58d12df9ae8df3c43d9e47f08b902e
shyoon <shyoon991@gmail.com>
2023-11-14 09:22:17 +0900

    feat: create fields LocationLPN (#10)

--- src/main/java/com/shyoon/wms/location/domain/LocationLPN.java
+++ src/main/java/com/shyoon/wms/location/domain/LocationLPN.java
@@ -1,4 +1,28 @@
 package com.shyoon.wms.location.domain;
 
+import com.shyoon.wms.inbound.domain.LPN;
+import org.springframework.util.Assert;
+
 public class LocationLPN {
+    private final Location location;
+    private final LPN lpn;
+    private Long inventoryQuantity;
+
+    public LocationLPN(
+            final Location location,
+            final LPN lpn) {
+        Assert.notNull(location, "로케이션은 필수입니다.");
+        Assert.notNull(lpn, "LPN은 필수입니다.");
+        this.location = location;
+        this.lpn = lpn;
+        this.inventoryQuantity = 1L;
+    }
+
+    public void increaseQuantity() {
+        inventoryQuantity++;
+    }
+
+    public LPN getLpn() {
+        return lpn;
+    }
 }
