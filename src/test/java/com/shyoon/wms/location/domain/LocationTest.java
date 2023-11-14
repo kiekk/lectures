35bcf1f2222e72cdddec95b0665727f69e1eb9fe
shyoon <shyoon991@gmail.com>
2023-11-14 09:07:50 +0900

    feat: extract delegate: LocationFixture (#10)

--- src/test/java/com/shyoon/wms/location/domain/LocationTest.java
+++ src/test/java/com/shyoon/wms/location/domain/LocationTest.java
@@ -10,21 +10,10 @@ class LocationTest {
     @Test
     @DisplayName("로케이션에 LPN을 할당한다.")
     void assignLPN() {
-        final Location location = createLocation();
-
+        final Location location = LocationFixture.aLocation().build();
         final LPN lpn = LPNFixture.anLPN().build();
 
         location.assignLPN(lpn);
     }
 
-    private Location createLocation() {
-        final String locationBarcode = "A-1-1";
-        final StorageType storageType = StorageType.TOTE;
-        final UsagePurpose usagePurpose = UsagePurpose.MOVE;
-        return new Location(
-                locationBarcode,
-                storageType,
-                usagePurpose
-        );
-    }
 }
