42fd3793d017fa3015c1f4995da7e3dafd883d89
shyoon <shyoon991@gmail.com>
2023-11-15 08:20:34 +0900

    feat: change signature: Request (#13)

--- src/test/java/com/shyoon/wms/outbound/feature/RegisterPackagingMaterialTest.java
+++ src/test/java/com/shyoon/wms/outbound/feature/RegisterPackagingMaterialTest.java
@@ -16,7 +16,30 @@ class RegisterPackagingMaterialTest {
     @Test
     @DisplayName("포장재를 등록한다.")
     void registerPackagingMaterial() {
-        final RegistetPackagingMaterial.Request request = new RegistetPackagingMaterial.Request();
+        final String name = "name";
+        final String code = "code";
+        final Long innerWidthInMillimeters = 1000L;
+        final Long innerHeightInMillimeters = 1000L;
+        final Long innerLengthInMillimeters = 1000L;
+        final Long outerWidthInMillimeters = 1000L;
+        final Long outerHeightInMillimeters = 1000L;
+        final Long outerLengthInMillimeters = 1000L;
+        final Long weightInGrams = 100L;
+        final Long maxWeightInGrams = 10000L;
+
+        final RegistetPackagingMaterial.Request request = new RegistetPackagingMaterial.Request(
+                name,
+                code,
+                innerWidthInMillimeters,
+                innerHeightInMillimeters,
+                innerLengthInMillimeters,
+                outerWidthInMillimeters,
+                outerHeightInMillimeters,
+                outerLengthInMillimeters,
+                weightInGrams,
+                maxWeightInGrams,
+                MaterialType.CORRUGATED_BOX
+        );
         registetPackagingMaterial.request(request);
     }
 
@@ -25,7 +48,28 @@ class RegisterPackagingMaterialTest {
 
         }
 
-        public record Request() {
+        public record Request(
+                String name,
+                String code,
+                Long innerWidthInMillimeters,
+                Long innerHeightInMillimeters,
+                Long innerLengthInMillimeters,
+                Long outerWidthInMillimeters,
+                Long outerHeightInMillimeters,
+                Long outerLengthInMillimeters,
+                Long weightInGrams,
+                Long maxWeightInGrams,
+                MaterialType materialType) {
+        }
+    }
+
+    enum MaterialType {
+        CORRUGATED_BOX("완충재가 있는 골판지 상자");
+
+        private final String description;
+
+        MaterialType(String description) {
+            this.description = description;
         }
     }
 }
