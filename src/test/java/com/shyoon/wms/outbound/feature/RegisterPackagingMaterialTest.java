8ac7a03fea4e1cdd001bd7e849a82ba67e2724ef
shyoon <shyoon991@gmail.com>
2023-11-15 08:12:50 +0900

    feat: create inner class: RegisterPackagingMaterial (#13)

--- src/test/java/com/shyoon/wms/outbound/feature/RegisterPackagingMaterialTest.java
+++ src/test/java/com/shyoon/wms/outbound/feature/RegisterPackagingMaterialTest.java
@@ -1,13 +1,23 @@
 package com.shyoon.wms.outbound.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RegisterPackagingMaterialTest {
 
+    private RegistetPackagingMaterial registetPackagingMaterial;
+
+    @BeforeEach
+    void setUp() {
+        registetPackagingMaterial = new RegistetPackagingMaterial();
+    }
+
     @Test
     @DisplayName("포장재를 등록한다.")
     void registerPackagingMaterial() {
+    }
 
+    private class RegistetPackagingMaterial {
     }
 }
