67c4608c363752251b10396a476d107a3201e4e9
shyoon <shyoon991@redutec.co.kr>
2023-11-16 08:27:25 +0900

    feat: add service
    * UserService
    * PostService

---
+++ src/main/java/com/example/inflearn/service/post/PostService.java
@@ -0,0 +1,42 @@
+package com.example.inflearn.service.post;
+
+import com.example.inflearn.exception.ResourceNotFoundException;
+import com.example.inflearn.model.dto.post.PostCreateDto;
+import com.example.inflearn.model.dto.post.PostUpdateDto;
+import com.example.inflearn.repository.post.PostEntity;
+import com.example.inflearn.repository.post.PostRepository;
+import com.example.inflearn.repository.user.UserEntity;
+import com.example.inflearn.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Clock;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+    private final UserService userService;
+
+    public PostEntity getPostById(long id) {
+        return postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Posts", id));
+    }
+
+    public PostEntity createPost(PostCreateDto postCreateDto) {
+        UserEntity userEntity = userService.getByIdOrElseThrow(postCreateDto.getWriterId());
+        PostEntity postEntity = new PostEntity();
+        postEntity.setWriter(userEntity);
+        postEntity.setContent(postCreateDto.getContent());
+        postEntity.setCreatedAt(Clock.systemUTC().millis());
+        return postRepository.save(postEntity);
+    }
+
+    public PostEntity updatePost(Long id, PostUpdateDto postUpdateDto) {
+        PostEntity postEntity = getPostById(id);
+        postEntity.setContent(postUpdateDto.getContent());
+        postEntity.setModifiedAt(Clock.systemUTC().millis());
+        return postRepository.save(postEntity);
+    }
+
+}
