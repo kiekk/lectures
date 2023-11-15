3f86d4a8d501403013f285c7f5a5451e18972ea9
soonho <shyoon991@gmail.com>
2023-11-15 20:08:04 +0900

    Initial Commit

---
+++ src/main/java/com/example/inflearn/InflearnJavaSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.inflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnJavaSpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnJavaSpringTestApplication.class, args);
+    }
+
+}
