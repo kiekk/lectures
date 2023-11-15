cb08383256555cb953b48ad9028d09ec3389421b
shyoon <shyoon991@redutec.co.kr>
2023-11-16 08:40:37 +0900

    feat: add controllers
    * UserCreateController
    * PostCreateController

---
+++ src/main/java/com/example/inflearn/controller/post/PostCreateController.java
@@ -0,0 +1,24 @@
+package com.example.inflearn.controller.post;
+
+import com.example.inflearn.model.dto.post.PostCreateDto;
+import com.example.inflearn.model.dto.post.PostResponse;
+import com.example.inflearn.service.post.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/posts")
+@RequiredArgsConstructor
+public class PostCreateController {
+
+    private final PostService postService;
+    private final PostController postController;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PostResponse createPost(@RequestBody PostCreateDto postCreateDto) {
+        return postController.toResponse(postService.createPost(postCreateDto));
+    }
+
+}
---
+++ src/main/java/com/example/inflearn/controller/user/UserCreateController.java
@@ -0,0 +1,26 @@
+package com.example.inflearn.controller.user;
+
+import com.example.inflearn.model.dto.user.UserCreateDto;
+import com.example.inflearn.model.dto.user.UserResponse;
+import com.example.inflearn.repository.user.UserEntity;
+import com.example.inflearn.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserCreateController {
+
+    private final UserController userController;
+    private final UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponse createUser(@RequestBody UserCreateDto userCreateDto) {
+        UserEntity userEntity = userService.createUser(userCreateDto);
+        return userController.toResponse(userEntity);
+    }
+
+}
