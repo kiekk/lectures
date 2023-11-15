043dbf2e19b65b2189b63dc7e3fdc0ec84ece600
shyoon <shyoon991@gmail.com>
2023-11-15 09:06:10 +0900

    feat: 시나리오 테스트로 전환 (#13)

--- src/test/java/com/shyoon/wms/outbound/feature/RegisterPackagingMaterialTest.java
+++ src/test/java/com/shyoon/wms/outbound/feature/RegisterPackagingMaterialTest.java
@@ -1,14 +1,11 @@
 package com.shyoon.wms.outbound.feature;
 
 import com.shyoon.wms.common.ApiTest;
-import com.shyoon.wms.outbound.domain.MaterialType;
+import com.shyoon.wms.common.Scenario;
 import com.shyoon.wms.outbound.domain.PackagingMaterialRepository;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,38 +17,8 @@ class RegisterPackagingMaterialTest extends ApiTest {
     @Test
     @DisplayName("포장재를 등록한다.")
     void registerPackagingMaterial() {
-        final String name = "name";
-        final String code = "code";
-        final Long innerWidthInMillimeters = 1000L;
-        final Long innerHeightInMillimeters = 1000L;
-        final Long innerLengthInMillimeters = 1000L;
-        final Long outerWidthInMillimeters = 1000L;
-        final Long outerHeightInMillimeters = 1000L;
-        final Long outerLengthInMillimeters = 1000L;
-        final Long weightInGrams = 100L;
-        final Long maxWeightInGrams = 10000L;
-
-        final RegisterPackagingMaterial.Request request = new RegisterPackagingMaterial.Request(
-                name,
-                code,
-                innerWidthInMillimeters,
-                innerHeightInMillimeters,
-                innerLengthInMillimeters,
-                outerWidthInMillimeters,
-                outerHeightInMillimeters,
-                outerLengthInMillimeters,
-                weightInGrams,
-                maxWeightInGrams,
-                MaterialType.CORRUGATED_BOX
-        );
-
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(request)
-                .when()
-                .post("/packaging-materials")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value());
+        Scenario
+                .registerPackagingMaterial().request();
 
         assertThat(packagingMaterialRepository.findAll()).hasSize(1);
     }
