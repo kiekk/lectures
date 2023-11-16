c7068a2b5b3a16424bf36ff84f505988f0936593
soonho <shyoon991@gmail.com>
2023-11-16 19:05:35 +0900

    rename: long -> Long

--- src/main/java/com/example/inflearn/repository/user/UserRepository.java
+++ src/main/java/com/example/inflearn/repository/user/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    Optional<UserEntity> findByIdAndStatus(long id, UserStatus userStatus);
+    Optional<UserEntity> findByIdAndStatus(Long id, UserStatus userStatus);
 
     Optional<UserEntity> findByEmailAndStatus(String email, UserStatus userStatus);
 }
