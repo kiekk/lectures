3dd92c14cb7ce7f25df2876b39cbb8940d73e0ad
shyoon <shyoon991@gmail.com>
2023-11-17 13:17:35 +0900

    feat: PostController, PostCreateController 테스트

--- src/test/java/com/example/inflearn/controller/post/PostCreateControllerTest.java
+++ src/test/java/com/example/inflearn/controller/post/PostCreateControllerTest.java
@@ -1,7 +1,55 @@
 package com.example.inflearn.controller.post;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.inflearn.model.dto.post.PostCreateDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("Test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(value = "/sql/post-create-controller-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
 class PostCreateControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void 사용자는_게시물을_작성할_수_있다() throws Exception {
+        // given
+        final PostCreateDto postCreateDto = PostCreateDto.builder()
+                .writerId(1L)
+                .content("helloworld")
+                .build();
+
+        // when
+        // then
+        mockMvc.perform(
+                        post("/api/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(postCreateDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.content").value("helloworld"))
+                .andExpect(jsonPath("$.writer.id").isNumber())
+                .andExpect(jsonPath("$.writer.email").value("shyoon991@gmail.com"))
+                .andExpect(jsonPath("$.writer.nickname").value("soono"));
+    }
 }
