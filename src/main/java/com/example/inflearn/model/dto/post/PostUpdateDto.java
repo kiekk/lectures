64fd23f4a96426ff91e20fe26f5f175ef5a4d97f
shyoon <shyoon991@gmail.com>
2023-11-17 13:17:35 +0900

    feat: PostController, PostCreateController 테스트

--- src/main/java/com/example/inflearn/model/dto/post/PostUpdateDto.java
+++ src/main/java/com/example/inflearn/model/dto/post/PostUpdateDto.java
@@ -3,14 +3,19 @@ package com.example.inflearn.model.dto.post;
 import lombok.Builder;
 import lombok.Getter;
 
+/*
+파라미터가 한 개 일경우 serialize 에러가 발생한다...
+ */
 @Getter
 public class PostUpdateDto {
 
     private final String content;
+    private final String test; // dummy
 
     @Builder
-    public PostUpdateDto(String content) {
+    public PostUpdateDto(String content, String test) {
         this.content = content;
+        this.test = test;
     }
 
 }
---
+++ src/test/resources/sql/post-create-controller-test-data.sql
@@ -0,0 +1,2 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'shyoon991@gmail.com', 'soono', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
