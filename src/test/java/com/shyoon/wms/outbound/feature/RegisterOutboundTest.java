00ba35e37540c96744c18b3915374066713f74fd
shyoon <shyoon991@gmail.com>
2023-11-21 07:16:50 +0900

    feat: create method: request (#15)

---
+++ src/test/java/com/shyoon/wms/outbound/feature/RegisterOutboundTest.java
@@ -0,0 +1,28 @@
+package com.shyoon.wms.outbound.feature;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RegisterOutboundTest {
+
+    private RegisterOutbound registerOutbound;
+
+    @BeforeEach
+    void setUp() {
+        registerOutbound = new RegisterOutbound();
+    }
+
+    @Test
+    @DisplayName("출고를 등록한다.")
+    void registerOutboundTest() {
+        registerOutbound.request();
+    }
+
+    private class RegisterOutbound {
+
+        public void request() {
+
+        }
+    }
+}
