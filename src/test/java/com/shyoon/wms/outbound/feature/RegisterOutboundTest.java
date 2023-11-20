d8c43c5c3f7cf2ff4aef34bad7414987f6faef31
shyoon <shyoon991@gmail.com>
2023-11-21 07:20:21 +0900

    feat: create signature (#15)

--- src/test/java/com/shyoon/wms/outbound/feature/RegisterOutboundTest.java
+++ src/test/java/com/shyoon/wms/outbound/feature/RegisterOutboundTest.java
@@ -3,6 +3,9 @@ package com.shyoon.wms.outbound.feature;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import java.time.LocalDate;
 
 class RegisterOutboundTest {
 
@@ -16,17 +19,40 @@ class RegisterOutboundTest {
     @Test
     @DisplayName("출고를 등록한다.")
     void registerOutboundTest() {
-        RegisterOutbound.Request request = new RegisterOutbound.Request();
+        final Long orderNo = 1L;
+        final Boolean isPriorityDelivery = false;
+        final LocalDate desiredDeliveryAt = LocalDate.now();
+        final RegisterOutbound.Request request = new RegisterOutbound.Request(
+                orderNo,
+                isPriorityDelivery,
+                desiredDeliveryAt
+        );
         registerOutbound.request(request);
     }
 
     private class RegisterOutbound {
 
-        public void request(Request request) {
+        public void request(final Request request) {
 
         }
 
         public static class Request {
+            private final Long orderNo;
+            private final Boolean isPriorityDelivery;
+            private final LocalDate desiredDeliveryAt;
+
+            public Request(
+                    final Long orderNo,
+                    final Boolean isPriorityDelivery,
+                    final LocalDate desiredDeliveryAt) {
+                Assert.notNull(orderNo, "주문번호는 필수입니다.");
+                Assert.notNull(isPriorityDelivery, "우선출고여부는 필수입니다.");
+                Assert.notNull(desiredDeliveryAt, "희망출고일은 필수입니다.");
+
+                this.orderNo = orderNo;
+                this.isPriorityDelivery = isPriorityDelivery;
+                this.desiredDeliveryAt = desiredDeliveryAt;
+            }
         }
     }
 }
