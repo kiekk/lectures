b32d2b84422b8fd4a6b26782792e35c3eb9c5205
shyoon <shyoon991@gmail.com>
2023-11-23 14:41:23 +0900

    feat: create class OutboundProduct (#15)

--- src/test/java/com/shyoon/wms/outbound/feature/RegisterOutboundTest.java
+++ src/test/java/com/shyoon/wms/outbound/feature/RegisterOutboundTest.java
@@ -49,9 +49,12 @@ class RegisterOutboundTest {
             // 출고에 사용할 포장재를 선택
 
             // 출고 생성
-            for (OrderProduct orderProduct : order.orderProducts) {
-
-            }
+            final List<OutboundProduct> outboundProducts = order.orderProducts.stream()
+                    .map(orderProduct -> new OutboundProduct(
+                            orderProduct.product,
+                            orderProduct.orderQuantity,
+                            orderProduct.unitPrice))
+                    .toList();
 
             // 출고 등록
         }
@@ -164,6 +167,36 @@ class RegisterOutboundTest {
             this.product = product;
             this.orderQuantity = orderQuantity;
             this.unitPrice = unitPrice;
+        }
+    }
+
+    private class OutboundProduct {
+        private final Product product;
+        private final Long orderQuantity;
+        private final Long unitPrice;
+
+        public OutboundProduct(
+                final Product product,
+                final Long orderQuantity,
+                final Long unitPrice) {
+            validateConstructor(product, orderQuantity, unitPrice);
+
+            this.product = product;
+            this.orderQuantity = orderQuantity;
+            this.unitPrice = unitPrice;
+        }
+
+        private void validateConstructor(Product product, Long orderQuantity, Long unitPrice) {
+            Assert.notNull(product, "상품은 필수입니다.");
+            Assert.notNull(product, "주문 수량은 필수입니다.");
+            if (orderQuantity < 1) {
+                throw new IllegalArgumentException("주문 수량은 1개 이상이어야 합니다.");
+            }
+
+            Assert.notNull(unitPrice, "단가는 필수입니다.");
+            if (unitPrice < 1) {
+                throw new IllegalArgumentException("단가는 1원 이상이어야 합니다.");
+            }
         }
     }
 }
