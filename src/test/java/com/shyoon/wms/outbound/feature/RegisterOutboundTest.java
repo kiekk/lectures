647098db215322a00c40b98ba13ed096511a55aa
shyoon <shyoon991@gmail.com>
2023-11-23 14:45:03 +0900

    feat: create class Outbound (#15)

--- src/test/java/com/shyoon/wms/outbound/feature/RegisterOutboundTest.java
+++ src/test/java/com/shyoon/wms/outbound/feature/RegisterOutboundTest.java
@@ -56,6 +56,15 @@ class RegisterOutboundTest {
                             orderProduct.unitPrice))
                     .toList();
 
+            final Outbound outbound = new Outbound(
+                    order.orderNo,
+                    order.orderCustomer,
+                    order.deliveryRequirements,
+                    outboundProducts,
+                    request.isPriorityDelivery,
+                    request.desiredDeliveryAt
+            );
+
             // 출고 등록
         }
 
@@ -197,6 +206,41 @@ class RegisterOutboundTest {
             if (unitPrice < 1) {
                 throw new IllegalArgumentException("단가는 1원 이상이어야 합니다.");
             }
+        }
+    }
+
+    private class Outbound {
+        private final Long orderNo;
+        private final OrderCustomer orderCustomer;
+        private final String deliveryRequirements;
+        private final List<OutboundProduct> outboundProducts;
+        private final Boolean isPriorityDelivery;
+        private final LocalDate desiredDeliveryAt;
+
+        public Outbound(
+                final Long orderNo,
+                final OrderCustomer orderCustomer,
+                final String deliveryRequirements,
+                final List<OutboundProduct> outboundProducts,
+                final Boolean isPriorityDelivery,
+                final LocalDate desiredDeliveryAt) {
+            validateConstructor(orderNo, orderCustomer, deliveryRequirements, outboundProducts, isPriorityDelivery, desiredDeliveryAt);
+
+            this.orderNo = orderNo;
+            this.orderCustomer = orderCustomer;
+            this.deliveryRequirements = deliveryRequirements;
+            this.outboundProducts = outboundProducts;
+            this.isPriorityDelivery = isPriorityDelivery;
+            this.desiredDeliveryAt = desiredDeliveryAt;
+        }
+
+        private void validateConstructor(Long orderNo, OrderCustomer orderCustomer, String deliveryRequirements, List<OutboundProduct> outboundProducts, Boolean isPriorityDelivery, LocalDate desiredDeliveryAt) {
+            Assert.notNull(orderNo, "주문번호는 필수입니다.");
+            Assert.notNull(orderCustomer, "주문고객은 필수입니다.");
+            Assert.notNull(deliveryRequirements, "배송요구사항은 필수입니다.");
+            Assert.notEmpty(outboundProducts, "출고상품은 필수입니다.");
+            Assert.notNull(isPriorityDelivery, "우선출고여부는 필수입니다.");
+            Assert.notNull(desiredDeliveryAt, "희망출고일은 필수입니다.");
         }
     }
 }
