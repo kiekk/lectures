4acaa0f850b7090794773417c629b38b2cbd0a07
shyoon <shyoon991@gmail.com>
2023-12-06 13:48:27 +0900

    feat: request validation (#21)

--- src/test/java/com/shyoon/wms/outbound/feature/SplitOutboundTest.java
+++ src/test/java/com/shyoon/wms/outbound/feature/SplitOutboundTest.java
@@ -3,6 +3,7 @@ package com.shyoon.wms.outbound.feature;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 import java.util.List;
 
@@ -22,8 +23,8 @@ class SplitOutboundTest {
         final Long productNo = 1L;
         final Long quantity = 1L;
         final SplitOutbound.Request.Product product = new SplitOutbound.Request.Product(
-              productNo,
-              quantity
+                productNo,
+                quantity
         );
         final List<SplitOutbound.Request.Product> products = List.of(product);
         final SplitOutbound.Request request = new SplitOutbound.Request(
@@ -40,9 +41,22 @@ class SplitOutboundTest {
         public record Request(
                 Long outboundNo,
                 List<Product> products) {
+
+            public Request {
+                Assert.notNull(outboundNo, "출고번호는 필수입니다.");
+                Assert.notEmpty(products, "상품은 필수입니다.");
+            }
+
             public record Product(
                     Long productNo,
                     Long quantity) {
+                public Product {
+                    Assert.notNull(productNo, "상품번호는 필수입니다.");
+                    Assert.notNull(quantity, "수량은 필수입니다.");
+                    if (quantity < 1) {
+                        throw new IllegalArgumentException("수량은 1 이상이어야 합니다.");
+                    }
+                }
             }
         }
     }
