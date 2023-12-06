70fa6494b406f63c871e5f925cafa9f104e94c11
shyoon <shyoon991@gmail.com>
2023-12-06 13:45:52 +0900

    feat: create inner record: product (#21)

--- src/test/java/com/shyoon/wms/outbound/feature/SplitOutboundTest.java
+++ src/test/java/com/shyoon/wms/outbound/feature/SplitOutboundTest.java
@@ -1,13 +1,41 @@
 package com.shyoon.wms.outbound.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class SplitOutboundTest {
 
+    private SplitOutbound splitOutbound;
+
+    @BeforeEach
+    void setUp() {
+        splitOutbound = new SplitOutbound();
+    }
+
     @Test
     @DisplayName("출고를 분할한다.")
     void splitOutbound() {
+        final Long outboundNo = 1L;
+        final Long productNo = 1L;
+        final Long quantity = 1L;
+        final SplitOutbound.Request.Product product = new SplitOutbound.Request.Product(
+              productNo,
+              quantity
+        );
+        final SplitOutbound.Request request = new SplitOutbound.Request();
+        splitOutbound.request(request);
+    }
 
+    private class SplitOutbound {
+        public void request(final Request request) {
+        }
+
+        public record Request() {
+            public record Product(
+                    Long productNo,
+                    Long quantity) {
+            }
+        }
     }
 }
