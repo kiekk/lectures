ae173b00ea7dda4b777445a3a2b48989192675a4
shyoon <shyoon991@gmail.com>
2023-12-08 11:40:46 +0900

    feat: change signature (#25)

--- src/test/java/com/shyoon/wms/outbound/feature/AllocatePickingTest.java
+++ src/test/java/com/shyoon/wms/outbound/feature/AllocatePickingTest.java
@@ -1,10 +1,28 @@
 package com.shyoon.wms.outbound.feature;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AllocatePickingTest {
 
+    private AllocatePicking allocatePicking;
+
+    @BeforeEach
+    void setUp() {
+        allocatePicking = new AllocatePicking();
+    }
+
     @Test
-    void name() {
+    @DisplayName("출고 상품에 대한 집품 목록을 할당한다.")
+    void allocatePicking() {
+        final Long outboundNo = 1L;
+        allocatePicking.request(outboundNo);
+    }
+
+    private class AllocatePicking {
+
+        public void request(final Long outboundNo) {
+        }
     }
 }
