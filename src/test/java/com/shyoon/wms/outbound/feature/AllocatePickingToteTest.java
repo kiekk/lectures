76463545777bfd095ea0fc0823eb0bda50fdb0c7
shyoon <shyoon991@gmail.com>
2023-12-08 09:54:25 +0900

    feat: add test case (#23)

--- src/test/java/com/shyoon/wms/outbound/feature/AllocatePickingToteTest.java
+++ src/test/java/com/shyoon/wms/outbound/feature/AllocatePickingToteTest.java
@@ -1,10 +1,12 @@
 package com.shyoon.wms.outbound.feature;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AllocatePickingToteTest {
     @Test
-    void name() {
+    @DisplayName("출고 상품을 집품할 토트에 할당한다.")
+    void allocatePickingTote() {
 
     }
 }
