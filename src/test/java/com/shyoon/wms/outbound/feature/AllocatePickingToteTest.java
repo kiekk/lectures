de143d097abaab4792a910888ce0642ef9f63fa3
shyoon <shyoon991@gmail.com>
2023-12-08 09:58:46 +0900

    feat: change signature (#23)

--- src/test/java/com/shyoon/wms/outbound/feature/AllocatePickingToteTest.java
+++ src/test/java/com/shyoon/wms/outbound/feature/AllocatePickingToteTest.java
@@ -3,6 +3,7 @@ package com.shyoon.wms.outbound.feature;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 class AllocatePickingToteTest {
 
@@ -16,7 +17,12 @@ class AllocatePickingToteTest {
     @Test
     @DisplayName("출고 상품을 집품할 토트에 할당한다.")
     void allocatePickingTote() {
-        final AllocatePickingTote.Request request = new AllocatePickingTote.Request();
+        final Long outboundNo = 1L;
+        final String toteBarcode = "A-1-1";
+        final AllocatePickingTote.Request request = new AllocatePickingTote.Request(
+                outboundNo,
+                toteBarcode
+        );
         allocatePickingTote.request(request);
     }
 
@@ -25,7 +31,13 @@ class AllocatePickingToteTest {
 
         }
 
-        public record Request() {
+        public record Request(
+                Long outboundNo,
+                String toteBarcode) {
+            public Request {
+                Assert.notNull(outboundNo, "출고 번호는 필수입니다.");
+                Assert.hasText(toteBarcode, "토트 바코드는 필수입니다.");
+            }
         }
     }
 }
