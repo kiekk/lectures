bc8f09279b04ca895d219687adaba10208d71850
shyoon <shyoon991@gmail.com>
2023-12-08 11:27:24 +0900

    feat: 토트 할당 api 테스트 변경 (#23)

--- src/main/java/com/shyoon/wms/outbound/feature/AllocatePickingTote.java
+++ src/main/java/com/shyoon/wms/outbound/feature/AllocatePickingTote.java
@@ -4,31 +4,37 @@ import com.shyoon.wms.location.domain.Location;
 import com.shyoon.wms.location.domain.LocationRepository;
 import com.shyoon.wms.outbound.domain.Outbound;
 import com.shyoon.wms.outbound.domain.OutboundRepository;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
 @RequiredArgsConstructor
-class AllocatePickingTote {
+public class AllocatePickingTote {
 
     private final OutboundRepository outboundRepository;
     private final LocationRepository locationRepository;
 
+    @PostMapping("/outbounds/allocate-picking-tote")
     @Transactional
-    public void request(final Request request) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public void request(@RequestBody @Valid final Request request) {
         final Outbound outbound = outboundRepository.getBy(request.outboundNo);
         final Location tote = locationRepository.getByLocationBarcode(request.toteBarcode);
         outbound.allocatePickingTote(tote);
     }
 
     public record Request(
+            @NotNull(message = "출고 번호는 필수입니다.")
             Long outboundNo,
+            @NotBlank(message = "토트 바코드는 필수입니다.")
             String toteBarcode) {
-        public Request {
-            Assert.notNull(outboundNo, "출고 번호는 필수입니다.");
-            Assert.hasText(toteBarcode, "토트 바코드는 필수입니다.");
-        }
     }
 }
--- src/test/java/com/shyoon/wms/outbound/feature/AllocatePickingToteTest.java
+++ src/test/java/com/shyoon/wms/outbound/feature/AllocatePickingToteTest.java
@@ -2,15 +2,19 @@ package com.shyoon.wms.outbound.feature;
 
 import com.shyoon.wms.common.ApiTest;
 import com.shyoon.wms.common.Scenario;
+import com.shyoon.wms.outbound.domain.Outbound;
+import com.shyoon.wms.outbound.domain.OutboundRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class AllocatePickingToteTest extends ApiTest {
 
     @Autowired
-    private AllocatePickingTote allocatePickingTote;
+    private OutboundRepository outboundRepository;
 
     @BeforeEach
     void setUpAllocatePickingTote() {
@@ -29,15 +33,13 @@ class AllocatePickingToteTest extends ApiTest {
     @Test
     @DisplayName("출고 상품을 집품할 토트에 할당한다.")
     void allocatePickingTote() {
-        final Long outboundNo = 1L;
-        final String toteBarcode = "TOTE-1";
-        final AllocatePickingTote.Request request = new AllocatePickingTote.Request(
-                outboundNo,
-                toteBarcode
-        );
-        allocatePickingTote.request(request);
+        Scenario
+                .allocatePickingTote().request();
 
         // 실제로 outbound 에 토트 바구니가 할당되었는지 확인
+        final Outbound outbound = outboundRepository.getBy(1L);
+
+        assertThat(outbound.getPikingTote()).isNotNull();
     }
 
 }
