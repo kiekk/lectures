ce21af3caf7c8213c346d9569871610dae0d8319
shyoon <shyoon991@gmail.com>
2023-12-08 09:55:12 +0900

    feat: add class AllocatePickingTote (#23)

--- src/test/java/com/shyoon/wms/outbound/feature/AllocatePickingToteTest.java
+++ src/test/java/com/shyoon/wms/outbound/feature/AllocatePickingToteTest.java
@@ -1,12 +1,24 @@
 package com.shyoon.wms.outbound.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AllocatePickingToteTest {
+
+    private AllocatePickingTote allocatePickingTote;
+
+    @BeforeEach
+    void setUp() {
+        allocatePickingTote = new AllocatePickingTote();
+    }
+
     @Test
     @DisplayName("출고 상품을 집품할 토트에 할당한다.")
     void allocatePickingTote() {
 
+    }
+
+    private class AllocatePickingTote {
     }
 }
