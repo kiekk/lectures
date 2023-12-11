bca334a5d1c2d90e2b17f4983264e35cfc02c6c3
shyoon <shyoon991@gmail.com>
2023-12-11 10:47:56 +0900

    feat: 멀티 쓰레드 환경에서 집품 할당 동시성 이슈 처리 - 1 (#27)
    * 집품 할당 메서드에 `synchronized` 키워드 추가

--- src/main/java/com/shyoon/wms/outbound/feature/AllocatePicking.java
+++ src/main/java/com/shyoon/wms/outbound/feature/AllocatePicking.java
@@ -20,7 +20,7 @@ public class AllocatePicking {
 
     @PostMapping("/outbounds/{outboundNo}/allocate-picking")
     @Transactional
-    public void request(@PathVariable final Long outboundNo) {
+    public synchronized void request(@PathVariable final Long outboundNo) {
         final Outbound outbound = outboundRepository.getBy(outboundNo);
         final Inventories inventories = inventoryRepository.inventoriesBy(outbound.getProductNos());
 
