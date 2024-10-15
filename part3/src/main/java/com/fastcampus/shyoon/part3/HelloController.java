007f18c0af206e42b5ec991d9ba02be3fae0a8c8
shyoon <shyoon991@gmail.com>
2024-10-15 21:45:27 +0900

    feat: Part3: use StringRedisTemplate

--- part3/src/main/java/com/fastcampus/shyoon/part3/HelloController.java
+++ part3/src/main/java/com/fastcampus/shyoon/part3/HelloController.java
@@ -1,13 +1,33 @@
 package com.fastcampus.shyoon.part3;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/setFruit")
+    public String setFruit(@RequestParam String name) {
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        ops.set("fruit", name);
+        return "saved";
+    }
+
+    @GetMapping("/getFruit")
+    public String getFruit() {
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        return ops.get("fruit");
     }
 }
