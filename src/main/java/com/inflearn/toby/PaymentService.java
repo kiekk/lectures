da0e83340b2da6399866ba8e4cb96e4b6d57a341
shyoon <kiekk@i-commtech.com>
2025-03-19 14:57:21 +0900

    관심사의 분리
    * 메서드를 분리하여 해당 작업에 이름을 부여하여 어떤 작업을 하는지 직관적으로 파악할 수 있도록 함

--- src/main/java/com/inflearn/toby/PaymentService.java
+++ src/main/java/com/inflearn/toby/PaymentService.java
@@ -13,7 +13,19 @@ import java.util.stream.Collectors;
 
 public class PaymentService {
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        // 환율 가져오기
+        BigDecimal exRate = getExRate(currency);
+
+        // 금액 계산
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+
+        // 유효 시간 계산
+        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
+
+        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+    }
+
+    // 환율 정보 가져오기
+    private BigDecimal getExRate(String currency) throws IOException {
         // https://open.er-api/com/v6/latest/
         URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -24,15 +36,7 @@ public class PaymentService {
 
         ObjectMapper mapper = new ObjectMapper();
         ExRateData exRateData = mapper.readValue(response, ExRateData.class);
-        BigDecimal exRate = exRateData.rates().get(currency);
-
-        // 금액 계산
-        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
-
-        // 유효 시간 계산
-        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
-
-        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+        return exRateData.rates().get(currency);
     }
 
     public static void main(String[] args) throws IOException {
