d1d636cefeb6382562e2300929cfc7f529c6f5ad
shyoon <kiekk@i-commtech.com>
2025-03-19 11:42:59 +0900

    PaymentService 개발 (2) - prepare 메서드 내부 구현
    * 환율 정보를 조회한 후 ObjectMapper를 사용하여 ExRateData 객체에 매핑
    * 이 때 response로 받아온 데이터의 포맷이 ExRateData 객체와 다를 경우 에러가 발생하는데 @JsonIgnoreProperties(ignoreUnknown = true)로 해당하는 필드만 매핑하여 해결할 수 있다

--- src/main/java/com/inflearn/toby/Payment.java
+++ src/main/java/com/inflearn/toby/Payment.java
@@ -1,4 +1,60 @@
 package com.inflearn.toby;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Payment {
+    private Long orderId;
+    private String currency;
+    // Tip: Double 타입을 사용할 경우 부동소수점으로 인해 오차가 발생할 수 있음
+    private BigDecimal foreignCurrencyAmount;
+    private BigDecimal exRate;
+    private BigDecimal convertedAmount;
+    private LocalDateTime validUntil;
+
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate,
+                   BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exRate = exRate;
+        this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getForeignCurrencyAmount() {
+        return foreignCurrencyAmount;
+    }
+
+    public BigDecimal getExRate() {
+        return exRate;
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public LocalDateTime getValidUntil() {
+        return validUntil;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "orderId=" + orderId +
+                ", currency='" + currency + '\'' +
+                ", foreignCurrencyAmount=" + foreignCurrencyAmount +
+                ", exRate=" + exRate +
+                ", convertedAmount=" + convertedAmount +
+                ", validUntil=" + validUntil +
+                '}';
+    }
 }
