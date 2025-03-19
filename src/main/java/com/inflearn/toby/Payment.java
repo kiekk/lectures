09ec045dbb3958debd96d2a457f50a860a8d38c1
shyoon <kiekk@i-commtech.com>
2025-03-19 10:01:49 +0900

    PaymentService 개발 (1) - PaymentService, Payment 객체 생성
    * 간단하게 PaymentService, Payment 객체를 확인하기 위해 PaymentService.main 메서드로 확인

---
+++ src/main/java/com/inflearn/toby/Payment.java
@@ -0,0 +1,4 @@
+package com.inflearn.toby;
+
+public class Payment {
+}
---
+++ src/main/java/com/inflearn/toby/PaymentService.java
@@ -0,0 +1,13 @@
+package com.inflearn.toby;
+
+public class PaymentService {
+    public Payment prepare() {
+        return new Payment();
+    }
+
+    public static void main(String[] args) {
+        PaymentService paymentService = new PaymentService();
+        Payment payment = paymentService.prepare();
+        System.out.println(payment);
+    }
+}
