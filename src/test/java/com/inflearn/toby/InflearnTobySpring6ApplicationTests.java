b72f86b0cf8a69d126caa412fa0890bcce2adc83
shyoon <kiekk@i-commtech.com>
2025-03-19 09:43:38 +0900

    Initial Commit

---
+++ src/test/java/com/inflearn/toby/InflearnTobySpring6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inflearn.toby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnTobySpring6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
