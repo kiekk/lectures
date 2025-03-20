7cfbb6d71df5b2cd9d8853ee9416fdf5d8e6ace5
shyoon <shyoon991@gmail.com>
2025-03-20 14:04:29 +0900

    스프링 컨테이너와 의존관계 주입
    * 앞서서 고민했던 내용들을 Spring의 BeanFactory를 이용하여 쉽게 구현할 수 있다.
    * 단순히 Spring의 기능을 사용하는 것에서 끝나는게 아니라 어떤 니즈를 해결하려고 했는지 그리고 내부는 어떻게 구현이 되어 있는지 왜 그렇게 되어 있는지
    파악하기 위해서 강의에서 이렇게 진행했던 것 같다.

--- src/main/java/com/inflearn/toby/Client.java
+++ src/main/java/com/inflearn/toby/Client.java
@@ -1,12 +1,15 @@
 package com.inflearn.toby;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.io.IOException;
 import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        ObjectFactory objectFactory = new ObjectFactory();
-        PaymentService paymentService = objectFactory.paymentService();
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext();
+        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
