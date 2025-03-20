8e7189aebd32cdefd280857cde12461ca2931e50
shyoon <shyoon991@gmail.com>
2025-03-20 17:08:25 +0900

    DI와 디자인 패턴 (2) - Decorator Pattern
    * Decorator Pattern을 사용하여 캐싱 구현
    * Decorator Pattern의 장점은 ExRateProvider의 구현체는 건드리지 않으면서 새로운 기능을 추가할 수 있다는 점이다.
    * 캐싱을 구현할 때는 데이터는 변경이 된다는 사실을 염두하여 캐시가 갱신되는 로직도 함께 고려해야 한다.

---
+++ src/main/java/com/inflearn/toby/CachedExRateProvider.java
@@ -0,0 +1,27 @@
+package com.inflearn.toby;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class CachedExRateProvider implements ExRateProvider {
+
+    private final ExRateProvider target;
+    private BigDecimal cachedExRate; // 캐시된 환율
+    private LocalDateTime cacheExpiryTime; // 캐시 만료 시간
+
+    public CachedExRateProvider(ExRateProvider target) {
+        this.target = target;
+    }
+
+    @Override
+    public BigDecimal getExRate(String currency) throws IOException {
+        if (cachedExRate == null || cacheExpiryTime.isBefore(LocalDateTime.now())) {
+            cachedExRate = target.getExRate(currency);
+            cacheExpiryTime = LocalDateTime.now().plusSeconds(3);
+
+            System.out.println("Cache Updated");
+        }
+        return cachedExRate;
+    }
+}
--- src/main/java/com/inflearn/toby/ObjectFactory.java
+++ src/main/java/com/inflearn/toby/ObjectFactory.java
@@ -1,19 +1,22 @@
 package com.inflearn.toby;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
 public class ObjectFactory {
-//    @Bean
-//    public PaymentService paymentService() {
-//        return new PaymentService(exRateProvider());
-//    }
-//
-//    @Bean
-//    public ExRateProvider exRateProvider() {
-//        return new SimpleExRateProvider();
-//    }
+    @Bean
+    public PaymentService paymentService() {
+        return new PaymentService(cachedExRateProvider());
+    }
+
+    @Bean
+    public ExRateProvider exRateProvider() {
+        return new WebApiExRateProvider();
+    }
+
+    @Bean
+    public ExRateProvider cachedExRateProvider() {
+        return new CachedExRateProvider(exRateProvider());
+    }
 }
