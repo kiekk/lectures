7e62a31888bae973eede5265dc085e121615bf33
shyoon <shyoon991@gmail.com>
2025-03-25 16:49:55 +0900

    테스트와 DI (1)
    * Stub을 이용하여 외부 환경으로부터 격리, 원하는 기능만 테스트
    * 메서드로 분리하여 여러 파라미터에 따른 테스트가 가능하도록 처리. 이 방식은 @ParameterizedTest로 대체 가능

--- src/test/java/com/inflearn/toby/payment/PaymentServiceTest.java
+++ src/test/java/com/inflearn/toby/payment/PaymentServiceTest.java
@@ -1,32 +1,29 @@
 package com.inflearn.toby.payment;
 
-import com.inflearn.toby.exrate.provider.WebApiExRateProvider;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
+import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PaymentServiceTest {
 
     @Test
     @DisplayName("prepare 메서드의 요구사항 3가지를 잘 충족했는지 검증, 1.적용환율 2.원화 환산 금액 3.원화 환산 금액 유효시간")
-    void prepare() throws IOException {
-        // given
-        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
+    void convertedAmount() throws IOException {
+        testAmount(valueOf(500), valueOf(5_000));
+        testAmount(valueOf(1_00), valueOf(10_000));
+        testAmount(valueOf(3_00), valueOf(30_000));
+    }
 
-        // when
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.TEN);
 
-        // then
-        // 적용환율
-        assertThat(payment.getExRate()).isNotNull();
-        // 원화 환산 금액
-        assertThat(payment.getConvertedAmount()).isEqualTo(payment.getExRate().multiply(payment.getForeignCurrencyAmount()));
-        // 원호 환산 금액 유효시간
-        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+        assertThat(payment.getExRate()).isEqualTo(exRate);
+        assertThat(payment.getConvertedAmount()).isEqualTo(convertedAmount);
     }
 }
