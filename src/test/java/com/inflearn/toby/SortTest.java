c02e7c396f8456ec31a7a666faf7bffe152f109f
shyoon <shyoon991@gmail.com>
2025-03-25 11:26:23 +0900

    Junit 테스트 작성
    * @BeforeEach를 사용하여 테스트들의 공통 로직을 분리할 수도 있다.
    * @BeforeEach 메서드의 네이밍은 setUp을 관례로 사용했지만 다른 이름을 사용해도 좋다. 요즘은 @BeforeEach와 동일한 이름으로 beforeEach를 사용하기도 한다.

--- src/test/java/com/inflearn/toby/SortTest.java
+++ src/test/java/com/inflearn/toby/SortTest.java
@@ -1,5 +1,6 @@
 package com.inflearn.toby;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,12 +11,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SortTest {
 
+    private Sort sort;
+
+    @BeforeEach
+    void beforeEach() {
+        sort = new Sort();
+    }
+
     @Test
     @DisplayName("문자열 목록을 길이 순으로 정렬, 2개의 문자열로 테스트")
     void sort() {
-        // given
-        Sort sort = new Sort();
-
         // when
         List<String> list = sort.sortByLength(Arrays.asList("aa", "b"));
 
@@ -26,9 +31,6 @@ public class SortTest {
     @Test
     @DisplayName("문자열 목록을 길이 순으로 정렬, 3개의 문자열로 테스트")
     void sort3Items() {
-        // given
-        Sort sort = new Sort();
-
         // when
         List<String> list = sort.sortByLength(Arrays.asList("aa", "ccc", "b"));
 
@@ -39,9 +41,6 @@ public class SortTest {
     @Test
     @DisplayName("문자열 목록을 길이 순으로 정렬, 이미 정렬이 된 목록으로 테스트")
     void sortAlreadySorted() {
-        // given
-        Sort sort = new Sort();
-
         // when
         List<String> list = sort.sortByLength(Arrays.asList("b", "aa", "ccc"));
 
