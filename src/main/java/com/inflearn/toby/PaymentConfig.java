abe033ddf8b9a0bbe7d7898592abf8f9a3097c53
shyoon <shyoon991@gmail.com>
2025-03-26 10:53:01 +0900

    Clock을 이용한 시간 테스트
    * 시간 데이터의 경우 이를 정확하게 비교하는 것이 어렵기 때문에 Clock을 사용하여 테스트의 경우에는 고정된 시간을 사용하여 테스트를 정확히 할 수 있도록 한다.

--- src/main/java/com/inflearn/toby/PaymentConfig.java
+++ src/main/java/com/inflearn/toby/PaymentConfig.java
@@ -6,15 +6,22 @@ import com.inflearn.toby.payment.PaymentService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Clock;
+
 @Configuration
-public class ObjectFactory {
+public class PaymentConfig {
     @Bean
     public PaymentService paymentService() {
-        return new PaymentService(exRateProvider());
+        return new PaymentService(exRateProvider(), clock());
     }
 
     @Bean
     public ExRateProvider exRateProvider() {
         return new WebApiExRateProvider();
+    }
+
+    @Bean
+    public Clock clock() {
+        return Clock.systemDefaultZone();
     }
 }
--- src/test/java/com/inflearn/toby/payment/PaymentServiceTest.java
+++ src/test/java/com/inflearn/toby/payment/PaymentServiceTest.java
@@ -1,26 +1,50 @@
 package com.inflearn.toby.payment;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PaymentServiceTest {
 
+    private Clock clock;
+
+    @BeforeEach
+    void beforeEach() {
+        clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+    }
+
     @Test
     @DisplayName("prepare 메서드의 요구사항 3가지를 잘 충족했는지 검증, 1.적용환율 2.원화 환산 금액 3.원화 환산 금액 유효시간")
     void convertedAmount() throws IOException {
-        testAmount(valueOf(500), valueOf(5_000));
-        testAmount(valueOf(1_00), valueOf(10_000));
-        testAmount(valueOf(3_00), valueOf(30_000));
+        testAmount(valueOf(500), valueOf(5_000), clock);
+        testAmount(valueOf(1_00), valueOf(10_000), clock);
+        testAmount(valueOf(3_00), valueOf(30_000), clock);
     }
 
-    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
-        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
+    @Test
+    @DisplayName("Payment.validUntil은 현재 시간으로부터 30분 이후의 시간이어야 한다.")
+    void validUntil() throws IOException {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(valueOf(1_000)), clock);
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.TEN);
+
+        // validUntil은 현재 시간으로부터 30분 이후의 시간이어야 한다.
+        LocalDateTime now = LocalDateTime.now(clock);
+        LocalDateTime expectedValidUntil = now.plusMinutes(30);
+        assertThat(payment.getValidUntil()).isEqualTo(expectedValidUntil);
+    }
+
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount, Clock clock) throws IOException {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate), clock);
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.TEN);
 
         assertThat(payment.getExRate()).isEqualByComparingTo(exRate);
