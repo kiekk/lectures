7a4ee0e281c20d5a3faef2fcfe48b8026b381cbd
shyoon <shyoon991@gmail.com>
2025-03-26 14:10:38 +0900

    WebApiExRateProvider 리팩터링
    * Deprecated 된 API 교체 (java.net.URL -> java.net.URI)
    * interface에서 구현체 로직과 관련된 예외는 throw 하는 것이 좋지 않다. 최대한 try~catch로 잡고 필요하다면 RuntimeException을 반환하는 것이 좋다.

--- src/main/java/com/inflearn/toby/exrate/provider/WebApiExRateProvider.java
+++ src/main/java/com/inflearn/toby/exrate/provider/WebApiExRateProvider.java
@@ -1,5 +1,6 @@
 package com.inflearn.toby.exrate.provider;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.inflearn.toby.exrate.ExRateData;
 import com.inflearn.toby.payment.ExRateProvider;
@@ -9,23 +10,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.stream.Collectors;
 
 public class WebApiExRateProvider implements ExRateProvider {
 
     @Override
-    public BigDecimal getExRate(String currency) throws IOException {
-        // https://open.er-api/com/v6/latest/
-        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+    public BigDecimal getExRate(String currency) {
+        String urlString = "https://open.er-api.com/v6/latest/" + currency;
+        URI uri;
+        try {
+            uri = new URI(urlString);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
 
-        String response = br.lines().collect(Collectors.joining());
-        br.close();
+        String response;
+        try {
+            HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
 
-        ObjectMapper mapper = new ObjectMapper();
-        ExRateData exRateData = mapper.readValue(response, ExRateData.class);
-        return exRateData.rates().get(currency);
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                response = br.lines().collect(Collectors.joining());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            ExRateData exRateData = mapper.readValue(response, ExRateData.class);
+            return exRateData.rates().get(currency);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/test/java/com/inflearn/toby/payment/PaymentServiceTest.java
+++ src/test/java/com/inflearn/toby/payment/PaymentServiceTest.java
@@ -25,15 +25,15 @@ class PaymentServiceTest {
 
     @Test
     @DisplayName("prepare 메서드의 요구사항 3가지를 잘 충족했는지 검증, 1.적용환율 2.원화 환산 금액 3.원화 환산 금액 유효시간")
-    void convertedAmount() throws IOException {
+    void convertedAmount() {
         testAmount(valueOf(500), valueOf(5_000), clock);
-        testAmount(valueOf(1_00), valueOf(10_000), clock);
-        testAmount(valueOf(3_00), valueOf(30_000), clock);
+        testAmount(valueOf(1_00), valueOf(1_000), clock);
+        testAmount(valueOf(3_00), valueOf(3_000), clock);
     }
 
     @Test
     @DisplayName("Payment.validUntil은 현재 시간으로부터 30분 이후의 시간이어야 한다.")
-    void validUntil() throws IOException {
+    void validUntil() {
         PaymentService paymentService = new PaymentService(new ExRateProviderStub(valueOf(1_000)), clock);
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.TEN);
 
@@ -43,7 +43,7 @@ class PaymentServiceTest {
         assertThat(payment.getValidUntil()).isEqualTo(expectedValidUntil);
     }
 
-    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount, Clock clock) throws IOException {
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount, Clock clock) {
         PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate), clock);
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.TEN);
 
