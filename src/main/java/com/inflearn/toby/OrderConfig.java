b9c45dc1b39eed0a816af38c594ec0a4eb7b00c9
shyoon <shyoon991@gmail.com>
2025-03-28 15:34:47 +0900

    @Transactional과 AOP
    * @Transactional이 붙은 클래스의 메소드가 트랜잭션 안에서 실행되도록 프록시를 만들어 줌
    * Test시 OrderService를 확인해보면 실제 구현 객체가 아니라 Proxy 객체로 되어 있음을 확인할 수 있다.

--- src/main/java/com/inflearn/toby/OrderConfig.java
+++ src/main/java/com/inflearn/toby/OrderConfig.java
@@ -4,21 +4,21 @@ import com.inflearn.toby.data.JdbcOrderRepository;
 import com.inflearn.toby.order.OrderRepository;
 import com.inflearn.toby.order.OrderService;
 import com.inflearn.toby.order.OrderServiceImpl;
-import com.inflearn.toby.order.OrderServiceTxProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
 @Import(DataConfig.class)
+@EnableTransactionManagement
 public class OrderConfig {
 
     @Bean
-    public OrderService orderService(PlatformTransactionManager transactionManager, OrderRepository orderRepository) {
-        return new OrderServiceTxProxy(new OrderServiceImpl(orderRepository), transactionManager);
+    public OrderService orderService(OrderRepository orderRepository) {
+        return new OrderServiceImpl(orderRepository);
     }
 
     @Bean
