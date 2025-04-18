aa497733eb00e0f962054f5ec0af05c6c02ee62b
shyoon <shyoon991@gmail.com>
2025-04-18 16:24:26 +0900

    섹션 14. 회원가입 / PasswordEncoder
    * modelmapper, jpa 의존성 추가
    * 로그인 페이지 디자인 & controller & permitAll 추가
    * passwordEncoder 빈 등록
    * Account entity & dto 추가
    * User controller, service, repository 추

--- section14/회원_인증_시스템/security/src/main/java/com/inflearn/security/config/security/config/SecurityConfig.java
+++ section14/회원_인증_시스템/security/src/main/java/com/inflearn/security/config/security/config/SecurityConfig.java
@@ -8,19 +8,30 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private static final List<String> PERMIT_ALL_URLS = List.of(
+            "/",
+            "/signup"
+    );
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
                         // static resources
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .requestMatchers("/").permitAll()
+                        .requestMatchers(PERMIT_ALL_URLS.toArray(new String[0])).permitAll()
                         .anyRequest().authenticated())
                 .formLogin(form ->
                         form.loginPage("/login").permitAll()
@@ -32,5 +43,11 @@ public class SecurityConfig {
     public UserDetailsService userDetailsService() {
         UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
         return new InMemoryUserDetailsManager(user);
+    }
+
+    public PasswordEncoder passwordEncoder() {
+        // 해당 메서드는 deprecated 되었으므로 사용하지 말 것
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ section14/회원_인증_시스템/security/src/main/java/com/inflearn/security/users/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.inflearn.security.users.repository;
+
+import com.inflearn.security.domain.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Account, Long> {
+
+}
