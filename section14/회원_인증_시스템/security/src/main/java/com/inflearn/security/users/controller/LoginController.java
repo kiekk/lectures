5e4e7ee53e0be6dfc2341d8096fe18a8d0b44f2f
shyoon <shyoon991@gmail.com>
2025-04-18 17:55:48 +0900

    섹션 14. 커스텀 로그아웃
    * get 방식으로도 로그아웃이 가능하도록 별도의 endpoint를 만들어 로그아웃 처리

--- section14/회원_인증_시스템/security/src/main/java/com/inflearn/security/users/controller/LoginController.java
+++ section14/회원_인증_시스템/security/src/main/java/com/inflearn/security/users/controller/LoginController.java
@@ -1,5 +1,10 @@
 package com.inflearn.security.users.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,8 +15,18 @@ public class LoginController {
         return "login/login";
     }
 
-    @GetMapping(value="/signup")
+    @GetMapping(value = "/signup")
     public String signup() {
         return "login/signup";
+    }
+
+    @GetMapping(value = "/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "redirect:/login";
     }
 }
