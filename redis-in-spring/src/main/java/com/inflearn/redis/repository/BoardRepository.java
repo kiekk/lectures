ea208df9ece4a33dc9164738f71e1d1683cd75bc
shyoon <shyoon991@gmail.com>
2025-04-23 11:30:17 +0900

    섹션 5. Board entity & controller & service & repository 작성

---
+++ redis-in-spring/src/main/java/com/inflearn/redis/repository/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.inflearn.redis.repository;
+
+import com.inflearn.redis.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    Page<Board> findAllByOrderByCreatedAtDesc(Pageable pageable);
+}
---
+++ redis-in-spring/src/main/java/com/inflearn/redis/service/BoardService.java
@@ -0,0 +1,26 @@
+package com.inflearn.redis.service;
+
+import com.inflearn.redis.entity.Board;
+import com.inflearn.redis.repository.BoardRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    public List<Board> getBoards(int page, int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        Page<Board> pageOfBoards = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
+        return pageOfBoards.getContent();
+    }
+}
