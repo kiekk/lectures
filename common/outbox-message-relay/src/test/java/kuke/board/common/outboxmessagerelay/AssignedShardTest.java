7404f60cf2c1f58ab38f3767664e32cb1f98a6d3
shyoon <shyoon991@gmail.com>
2025-05-07 18:04:41 +0900

    섹션 6. Transactional Outbox 모듈 구현

---
+++ common/outbox-message-relay/src/test/java/kuke/board/common/outboxmessagerelay/AssignedShardTest.java
@@ -0,0 +1,38 @@
+package kuke.board.common.outboxmessagerelay;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AssignedShardTest {
+
+    @Test
+    void ofTest() {
+        // given
+        Long shardCount = 64L;
+        List<String> appIds = List.of("appId1", "appId2", "appId3");
+
+        // when
+        AssignedShard assignedShard1 = AssignedShard.of(appIds.getFirst(), appIds, shardCount);
+        AssignedShard assignedShard2 = AssignedShard.of(appIds.get(1), appIds, shardCount);
+        AssignedShard assignedShard3 = AssignedShard.of(appIds.get(2), appIds, shardCount);
+        AssignedShard assignedShard4 = AssignedShard.of("invalid", appIds, shardCount);
+
+        // then
+        List<Long> result = Stream.of(assignedShard1.getShards(), assignedShard2.getShards(), assignedShard3.getShards(), assignedShard4.getShards())
+                .flatMap(List::stream)
+                .toList();
+
+        assertThat(result).hasSize(shardCount.intValue());
+
+        for (int i = 0; i < 64; i++) {
+            assertThat(result.get(i)).isEqualTo(i);
+        }
+
+        assertThat(assignedShard4.getShards()).isEmpty();
+    }
+
+}
