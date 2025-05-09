c149031f39ed8ebf0506f3f2d718184e5088d60d
shyoon <shyoon991@gmail.com>
2025-05-09 09:57:00 +0900

    섹션 7. 게시글 조회 서비스 설정 & Client & QueryModel

---
+++ service/article-read/src/main/java/soono/board/articleread/client/CommentClient.java
@@ -0,0 +1,36 @@
+package soono.board.articleread.client;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CommentClient {
+    private RestClient restClient;
+
+    @Value("${endpoints.soono-board-comment-service.url}")
+    private String commentServiceUrl;
+
+    @PostConstruct
+    public void initRestClient() {
+        this.restClient = RestClient.create(commentServiceUrl);
+    }
+
+    public Long count(Long articleId) {
+        try {
+            return restClient.get()
+                    .uri("/v2/comments/articles/{articleId}/count", articleId)
+                    .retrieve()
+                    .body(Long.class);
+        } catch (Exception e) {
+            log.error("[CommentClient.read] articleId={}", articleId, e);
+            return 0L;
+        }
+    }
+
+}
