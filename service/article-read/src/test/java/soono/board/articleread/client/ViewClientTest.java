953be10c063a5f98ee62ecfe58819fb7195d1c5b
shyoon <shyoon991@gmail.com>
2025-05-09 14:34:28 +0900

    섹션 7. 캐시 최적화 전략 설계 - 기존 캐시 전략의 제약
    * 동시 요청이 올 경우 원본 데이터 서버에 여러 번 요청된다.

--- service/article-read/src/test/java/soono/board/articleread/client/ViewClientTest.java
+++ service/article-read/src/test/java/soono/board/articleread/client/ViewClientTest.java
@@ -1,11 +1,16 @@
 package soono.board.articleread.client;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @SpringBootTest
 class ViewClientTest {
     @Autowired
@@ -19,5 +24,25 @@ class ViewClientTest {
 
         TimeUnit.SECONDS.sleep(3);
         viewClient.count(1L); // 로그 출력 (cache miss)
+    }
+
+    @Test
+    void readCacheableMultiThreadTest() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        viewClient.count(1L); // init cache
+
+        for (int i = 0; i < 5; i++) {
+            CountDownLatch latch = new CountDownLatch(5);
+            for (int j = 0; j < 5; j++) {
+                executorService.submit(() -> {
+                    viewClient.count(1L); // cache hit
+                    latch.countDown();
+                });
+            }
+            latch.await();
+            TimeUnit.SECONDS.sleep(3);
+            log.info("--cache expired--");
+        }
     }
 }
