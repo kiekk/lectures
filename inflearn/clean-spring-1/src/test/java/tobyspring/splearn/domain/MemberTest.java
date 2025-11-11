9465c691b7264766df9ba95f7b409c1f84605df9
shyoon <shyoon991@gmail.com>
2025-11-11 19:49:48 +0900

    토비의 클린 스프링 - Member 엔티티 생성

---
+++ inflearn/clean-spring-1/src/test/java/tobyspring/splearn/domain/MemberTest.java
@@ -0,0 +1,14 @@
+package tobyspring.splearn.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemberTest {
+    @Test
+    void createMember() {
+        var member = new Member("soono@splearn.app", "soono", "secret");
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+    }
+
+}
