624f5bc3922f582443b5644528f61cf6eb0f0fb1
shyoon <shyoon991@gmail.com>
2025-11-15 08:26:14 +0900

    토비의 클린 스프링 - 엔티티의 자연키 지정

--- inflearn/clean-spring-1/src/test/java/tobyspring/splearn/application/required/MemberRepositoryTest.java
+++ inflearn/clean-spring-1/src/test/java/tobyspring/splearn/application/required/MemberRepositoryTest.java
@@ -4,9 +4,11 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import tobyspring.splearn.domain.Member;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static tobyspring.splearn.domain.MemberFixture.createMemberRegisterRequest;
 import static tobyspring.splearn.domain.MemberFixture.createPasswordEncoder;
 
@@ -30,6 +32,16 @@ class MemberRepositoryTest {
         assertThat(member.getId()).isNotNull();
 
         entityManager.flush();
+    }
+
+    @Test
+    void duplicateEmailFail() {
+        var member = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+        memberRepository.save(member);
+
+        var member2 = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+        assertThatThrownBy(() -> memberRepository.save(member2))
+                .isInstanceOf(DataIntegrityViolationException.class);
     }
 
 }
